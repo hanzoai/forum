hanzoai/forum

commit 33bbd4256fdefbb2af1a57760b555f0a018fc174

Remove use of `rescue nil`.
* `rescue nil` is a really bad pattern to use in our code base.
  We should rescue errors that we expect the code to throw and
  not rescue everything because we're unsure of what errors the
  code would throw. This would reduce the amount of pain we face
  when debugging why something isn't working as expexted. I've
  been bitten countless of times by errors being swallowed as a
  result during debugging sessions.

app/services/handle_chunk_upload.rb
@@ -42,8 +42,8 @@ class HandleChunkUpload
     tmp_directory   = @params[:tmp_directory]
 
     # delete destination files
-    File.delete(upload_path) rescue nil
-    File.delete(tmp_upload_path) rescue nil
+    File.delete(upload_path)
+    File.delete(tmp_upload_path)
 
     # merge all the chunks
     File.open(tmp_upload_path, "a") do |file|
@@ -59,7 +59,7 @@ class HandleChunkUpload
     FileUtils.mv(tmp_upload_path, upload_path, force: true)
 
     # remove tmp directory
-    FileUtils.rm_rf(tmp_directory) rescue nil
+    FileUtils.rm_rf(tmp_directory)
   end
 
 end

app/controllers/admin/users_controller.rb
@@ -390,7 +390,13 @@ class Admin::UsersController < Admin::AdminController
     ip = params[:ip]
 
     # should we cache results in redis?
-    location = Excon.get("https://ipinfo.io/#{ip}/json", read_timeout: 10, connect_timeout: 10).body rescue nil
+    begin
+      location = Excon.get(
+        "https://ipinfo.io/#{ip}/json",
+        read_timeout: 10, connect_timeout: 10
+      )&.body
+    rescue Excon::Error
+    end
 
     render json: location
   end
@@ -424,7 +430,7 @@ class Admin::UsersController < Admin::AdminController
     }
 
     AdminUserIndexQuery.new(params).find_users(50).each do |user|
-      user_destroyer.destroy(user, options) rescue nil
+      user_destroyer.destroy(user, options)
     end
 
     render json: success_json

app/controllers/embed_controller.rb
@@ -103,9 +103,12 @@ class EmbedController < ApplicationController
     end
 
     def ensure_embeddable
+      if !(Rails.env.development? && current_user&.admin?)
+        referer = request.referer
 
-      if !(Rails.env.development? && current_user.try(:admin?))
-        raise Discourse::InvalidAccess.new('invalid referer host') unless EmbeddableHost.url_allowed?(request.referer)
+        unless referer && EmbeddableHost.url_allowed?(referer)
+          raise Discourse::InvalidAccess.new('invalid referer host')
+        end
       end
 
       response.headers['X-Frame-Options'] = "ALLOWALL"

app/controllers/stylesheets_controller.rb
@@ -38,7 +38,13 @@ class StylesheetsController < ApplicationController
     end
 
     cache_time = request.env["HTTP_IF_MODIFIED_SINCE"]
-    cache_time = Time.rfc2822(cache_time) rescue nil if cache_time
+
+    if cache_time
+      begin
+        cache_time = Time.rfc2822(cache_time)
+      rescue ArgumentError
+      end
+    end
 
     query = StylesheetCache.where(target: target)
     if digest
@@ -82,12 +88,21 @@ class StylesheetsController < ApplicationController
   def handle_missing_cache(location, name, digest)
     location = location.sub(".css.map", ".css")
     source_map_location = location + ".map"
+    existing = read_file(location)
 
-    existing = File.read(location) rescue nil
     if existing && digest
-      source_map = File.read(source_map_location) rescue nil
+      source_map = read_file(source_map_location)
       StylesheetCache.add(name, digest, existing, source_map)
     end
   end
+
+  private
+
+    def read_file(location)
+      begin
+        File.read(location)
+      rescue Errno::ENOENT
+      end
+    end
 
 end

app/controllers/uploads_controller.rb
@@ -127,7 +127,7 @@ class UploadsController < ApplicationController
 
     upload.errors.empty? ? upload : { errors: upload.errors.values.flatten }
   ensure
-    tempfile&.close! rescue nil
+    tempfile&.close!
   end
 
 end

app/controllers/user_badges_controller.rb
@@ -58,7 +58,11 @@ class UserBadgesController < ApplicationController
     post_id = nil
 
     if params[:reason].present?
-      path = URI.parse(params[:reason]).path rescue nil
+      path = begin
+        URI.parse(params[:reason]).path
+      rescue URI::InvalidURIError
+      end
+
       route = Rails.application.routes.recognize_path(path) if path
       if route
         topic_id = route[:topic_id].to_i

app/controllers/users/omniauth_callbacks_controller.rb
@@ -48,7 +48,11 @@ class Users::OmniauthCallbacksController < ApplicationController
     end
 
     if origin.present?
-      parsed = URI.parse(origin) rescue nil
+      parsed = begin
+        URI.parse(origin)
+      rescue URI::InvalidURIError
+      end
+
       if parsed
         @origin = "#{parsed.path}?#{parsed.query}"
       end

app/controllers/webhooks_controller.rb
@@ -77,8 +77,8 @@ class WebhooksController < ActionController::Base
   def mandrill
     events = params["mandrill_events"]
     events.each do |event|
-      message_id = event["msg"]["metadata"]["message_id"] rescue nil
-      to_address = event["msg"]["email"] rescue nil
+      message_id = event.dig("msg", "metadata", "message_id")
+      to_address = event.dig("msg", "email")
 
       case event["event"]
       when "hard_bounce"
@@ -94,12 +94,12 @@ class WebhooksController < ActionController::Base
   def sparkpost
     events = params["_json"] || [params]
     events.each do |event|
-      message_event = event["msys"]["message_event"] rescue nil
+      message_event = event.dig("msys", "message_event")
       next unless message_event
 
-      message_id   = message_event["rcpt_meta"]["message_id"] rescue nil
-      to_address   = message_event["rcpt_to"]                 rescue nil
-      bounce_class = message_event["bounce_class"]            rescue nil
+      message_id   = message_event.dig("rcpt_meta", "message_id")
+      to_address   = message_event["rcpt_to"]
+      bounce_class = message_event["bounce_class"]
       next unless bounce_class
 
       bounce_class = bounce_class.to_i

commit 45c903ab0d84898521cb10d04485a3b0abeb6d12

FIX: Can't upload backup.
* Regression introduced in https://github.com/discourse/discourse/commit/33bbd4256fdefbb2af1a57760b555f0a018fc174

app/services/handle_chunk_upload.rb
@@ -42,8 +42,11 @@ class HandleChunkUpload
     tmp_directory   = @params[:tmp_directory]
 
     # delete destination files
-    File.delete(upload_path)
-    File.delete(tmp_upload_path)
+    begin
+      File.delete(upload_path)
+      File.delete(tmp_upload_path)
+    rescue Errno::ENOENT
+    end
 
     # merge all the chunks
     File.open(tmp_upload_path, "a") do |file|
@@ -59,7 +62,10 @@ class HandleChunkUpload
     FileUtils.mv(tmp_upload_path, upload_path, force: true)
 
     # remove tmp directory
-    FileUtils.rm_rf(tmp_directory)
+    begin
+      FileUtils.rm_rf(tmp_directory)
+    rescue Errno::ENOENT
+    end
   end
 
 end
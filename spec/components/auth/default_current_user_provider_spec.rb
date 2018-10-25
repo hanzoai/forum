hanzoai/forum

commit e7a78976400fe1fc6257a32d68b15f84118474df

FIX: Do not update `last seen` time for suspended users

lib/auth/default_current_user_provider.rb
@@ -75,14 +75,6 @@ class Auth::DefaultCurrentUserProvider
       @env[BAD_TOKEN] = true
     end
 
-    if current_user && should_update_last_seen?
-      u = current_user
-      Scheduler::Defer.later "Updating Last Seen" do
-        u.update_last_seen!
-        u.update_ip_address!(request.ip)
-      end
-    end
-
     # possible we have an api call, impersonate
     if api_key
       current_user = lookup_api_user(api_key, request)
@@ -125,6 +117,14 @@ class Auth::DefaultCurrentUserProvider
     # under no conditions to suspended or inactive accounts get current_user
     if current_user && (current_user.suspended? || !current_user.active)
       current_user = nil
+    end
+
+    if current_user && should_update_last_seen?
+      u = current_user
+      Scheduler::Defer.later "Updating Last Seen" do
+        u.update_last_seen!
+        u.update_ip_address!(request.ip)
+      end
     end
 
     @env[CURRENT_USER_KEY] = current_user

spec/components/auth/default_current_user_provider_spec.rb
@@ -156,6 +156,39 @@ describe Auth::DefaultCurrentUserProvider do
           ).should_update_last_seen?).to eq(false)
   end
 
+  it "should not update last seen for suspended users" do
+    user = Fabricate(:user)
+    provider = provider('/')
+    cookies = {}
+    provider.log_on_user(user, {}, cookies)
+    unhashed_token = cookies["_t"][:value]
+
+    freeze_time
+    Sidekiq::Testing.inline! do
+      # Need to clear this key from redis, otherwise
+      # this test could fail if run twice in 1 minute
+      $redis.del("user:#{user.id}:#{Time.now.to_date}")
+      provider2 = provider("/", "HTTP_COOKIE" => "_t=#{unhashed_token}")
+      u = provider2.current_user
+      u.reload
+      expect(u.last_seen_at).to eq(Time.now)
+
+      freeze_time 20.minutes.from_now
+
+      u.last_seen_at = nil
+      u.suspended_till = 1.year.from_now
+      u.save!
+
+      $redis.del("user:#{user.id}:#{Time.now.to_date}")
+      provider2 = provider("/", "HTTP_COOKIE" => "_t=#{unhashed_token}")
+      expect(provider2.current_user).to eq(nil)
+
+      u.reload
+      expect(u.last_seen_at).to eq(nil)
+    end
+
+  end
+
   it "should update ajax reqs with discourse visible" do
     expect(provider("/topic/anything/goes",
                     :method => "POST",

commit 32dc90ea8314889266b8d1ebf8c2a9c01469bf4d

FIX: Do not update `last_seen` for API access
This regressed in e7a7897. I have now added tests for the behavior.

lib/auth/default_current_user_provider.rb
@@ -240,6 +240,8 @@ class Auth::DefaultCurrentUserProvider
   def should_update_last_seen?
     if @request.xhr?
       @env["HTTP_DISCOURSE_VISIBLE".freeze] == "true".freeze
+    elsif !!(@env[API_KEY_ENV]) || !!(@env[USER_API_KEY_ENV])
+      false
     else
       true
     end

spec/components/auth/default_current_user_provider_spec.rb
@@ -32,7 +32,11 @@ describe Auth::DefaultCurrentUserProvider do
     it "finds a user for a correct per-user api key" do
       user = Fabricate(:user)
       ApiKey.create!(key: "hello", user_id: user.id, created_by_id: -1)
-      expect(provider("/?api_key=hello").current_user.id).to eq(user.id)
+      good_provider = provider("/?api_key=hello")
+      expect(good_provider.current_user.id).to eq(user.id)
+      expect(good_provider.is_api?).to eq(true)
+      expect(good_provider.is_user_api?).to eq(false)
+      expect(good_provider.should_update_last_seen?).to eq(false)
 
       user.update_columns(active: false)
 
@@ -399,6 +403,7 @@ describe Auth::DefaultCurrentUserProvider do
       expect(good_provider.current_user.id).to eq(user.id)
       expect(good_provider.is_api?).to eq(false)
       expect(good_provider.is_user_api?).to eq(true)
+      expect(good_provider.should_update_last_seen?).to eq(false)
 
       expect {
         provider("/", params.merge("REQUEST_METHOD" => "POST")).current_user
hanzoai/forum

commit c6004631f0c5b441788cad699b778e5d89cb06fe

FIX: Don't rescue `PG::UniqueViolation` within a transaction.
Also acquire a transaction per link instead of failing when
any of the links can't be processed.

This prevents ActiveRecord from rolling back the transaction
and the next SQL statement sent to PG will fail. This is
however hard to test as it only happens when there are
two competing process trying to process this method at the
same time.

app/models/topic_link.rb
@@ -112,24 +112,23 @@ SQL
     return if post.blank? || post.whisper?
 
     added_urls = []
-    TopicLink.transaction do
+    reflected_ids = []
 
-      added_urls = []
-      reflected_ids = []
-
-      PrettyText
-        .extract_links(post.cooked)
-        .map do |u|
-          uri = begin
-            URI.parse(u.url)
-          rescue URI::Error
-          end
-
-          [u, uri]
+    PrettyText
+      .extract_links(post.cooked)
+      .map do |u|
+        uri = begin
+          URI.parse(u.url)
+        rescue URI::Error
         end
-        .reject { |_, p| p.nil? || "mailto".freeze == p.scheme }
-        .uniq { |_, p| p }
-        .each do |link, parsed|
+
+        [u, uri]
+      end
+      .reject { |_, p| p.nil? || "mailto".freeze == p.scheme }
+      .uniq { |_, p| p }
+      .each do |link, parsed|
+
+      TopicLink.transaction do
         begin
           url = link.url
           internal = false
@@ -185,7 +184,7 @@ SQL
                                 link_post_id: reflected_post.try(:id),
                                 quote: link.is_quote,
                                 extension: file_extension)
-            rescue ActiveRecord::RecordNotUnique, PG::UniqueViolation
+            rescue ActiveRecord::RecordNotUnique
               # it's fine
             end
           end

commit ebde1eae8d2d8dfe1ceca24ba1e56e50236d190e

FIX: Do not reset link counts when post is rebaked
This was an indentation mistake introduced in c600463. Pretty understandable, considering we are indented 8 levels deep in this method. Will follow-up with a refactor to improve this.

app/models/topic_link.rb
@@ -223,34 +223,35 @@ SQL
           # If we can't find the route, no big deal
         end
       end
+    end
 
-      # Remove links that aren't there anymore
-      if added_urls.present?
+    # Remove links that aren't there anymore
+    if added_urls.present?
+      TopicLink.where(
+        "(url not in (:urls)) AND (post_id = :post_id AND NOT reflection)",
+        urls: added_urls, post_id: post.id
+      ).delete_all
+
+      reflected_ids.compact!
+      if reflected_ids.present?
         TopicLink.where(
-          "(url not in (:urls)) AND (post_id = :post_id AND NOT reflection)",
-          urls: added_urls, post_id: post.id
+          "(id not in (:reflected_ids)) AND (link_post_id = :post_id AND reflection)",
+          reflected_ids: reflected_ids, post_id: post.id
         ).delete_all
-
-        reflected_ids.compact!
-        if reflected_ids.present?
-          TopicLink.where(
-            "(id not in (:reflected_ids)) AND (link_post_id = :post_id AND reflection)",
-            reflected_ids: reflected_ids, post_id: post.id
-          ).delete_all
-        else
-          TopicLink
-            .where("link_post_id = :post_id AND reflection", post_id: post.id)
-            .delete_all
-        end
       else
         TopicLink
-          .where(
-            "(post_id = :post_id AND NOT reflection) OR (link_post_id = :post_id AND reflection)",
-            post_id: post.id
-          )
+          .where("link_post_id = :post_id AND reflection", post_id: post.id)
           .delete_all
       end
+    else
+      TopicLink
+        .where(
+          "(post_id = :post_id AND NOT reflection) OR (link_post_id = :post_id AND reflection)",
+          post_id: post.id
+        )
+        .delete_all
     end
+
   end
 
   # Crawl a link's title after it's saved

spec/models/topic_link_spec.rb
@@ -26,15 +26,17 @@ describe TopicLink do
   end
 
   describe 'external links' do
-    before do
-      post = Fabricate(:post, raw: <<~RAW, user: user, topic: topic)
+    let(:post2) do
+      Fabricate(:post, raw: <<~RAW, user: user, topic: topic)
         http://a.com/
         https://b.com/b
         http://#{'a' * 200}.com/invalid
         //b.com/#{'a' * 500}
       RAW
+    end
 
-      TopicLink.extract_from(post)
+    before do
+      TopicLink.extract_from(post2)
     end
 
     it 'works' do
@@ -43,6 +45,16 @@ describe TopicLink do
         "https://b.com/b",
         "//b.com/#{'a' * 500}"[0...TopicLink.max_url_length]
       )
+    end
+
+    it "doesn't reset them when rebaking" do
+      old_ids = topic.topic_links.pluck(:id)
+
+      TopicLink.extract_from(post2)
+
+      new_ids = topic.topic_links.pluck(:id)
+
+      expect(new_ids).to contain_exactly(*old_ids)
     end
 
   end
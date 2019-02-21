hanzoai/forum

commit ffa5e42f1abb9dccb0f8eab6eac55501c9c39f06

FEATURE: autocomplete usernames early in topic based on participation
Following this change when a user hits `@` and is replying to a topic they
will see usernames of people who were last seen and participated in the topic

This is somewhat experimental, we may tweak this, or make it optional.

Also, a regression in ff9f4147 where hitting TAB would eat a post you were writing:

Eg this would eat a post:

``` text
@hello, testing 123 <tab>
```

app/assets/javascripts/discourse/lib/user-search.js.es6
@@ -23,7 +23,11 @@ function performSearch(
     resultsFn(cached);
     return;
   }
-  if (term === "") {
+
+  // I am not strongly against unconditionally returning
+  // however this allows us to return a list of probable
+  // users we want to mention, early on a topic
+  if (term === "" && !topicId) {
     return [];
   }
 
@@ -108,6 +112,18 @@ function organizeResults(r, options) {
   return results;
 }
 
+// all punctuations except for . which is allowed in usernames
+// note: these are valid in names, but will end up tripping search anyway so just skip
+// this means searching for `sam saffron` is OK but if my name is `sam$ saffron` autocomplete
+// will not find me, which is a reasonable compromise
+//
+// we also ignore if we notice a double space or a string that is only a space
+const ignoreRegex = /([\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-\/:;<=>?@\[\]^_`{|}~])|\s\s|^\s$/;
+
+function skipSearch(term) {
+  return !!term.match(ignoreRegex);
+}
+
 export default function userSearch(options) {
   if (options.term && options.term.length > 0 && options.term[0] === "@") {
     options.term = options.term.substring(1);
@@ -120,10 +136,6 @@ export default function userSearch(options) {
     allowedUsers = options.allowedUsers,
     topicId = options.topicId,
     group = options.group;
-
-  if (/[^\w.-]/.test(term)) {
-    term = "";
-  }
 
   if (oldSearch) {
     oldSearch.abort();
@@ -142,6 +154,11 @@ export default function userSearch(options) {
     var clearPromise = setTimeout(function() {
       resolve(CANCELLED_STATUS);
     }, 5000);
+
+    if (skipSearch(term)) {
+      resolve([]);
+      return;
+    }
 
     debouncedSearch(
       term,

test/javascripts/lib/user-search-test.js.es6
@@ -1,5 +1,4 @@
 import userSearch from "discourse/lib/user-search";
-import { CANCELLED_STATUS } from "discourse/lib/autocomplete";
 
 QUnit.module("lib:user-search", {
   beforeEach() {
@@ -73,7 +72,29 @@ QUnit.test("it strips @ from the beginning", async assert => {
   assert.equal(results[results.length - 1]["name"], "team");
 });
 
-QUnit.test("it does not search for invalid usernames", async assert => {
-  let results = await userSearch({ term: "foo, " });
-  assert.equal(results, CANCELLED_STATUS);
+QUnit.test("it skips a search depending on punctuations", async assert => {
+  let skippedTerms = [
+    "@sam  s", // double space is not allowed
+    "@sam;",
+    "@sam,",
+    "@sam:"
+  ];
+
+  skippedTerms.forEach(async term => {
+    let results = await userSearch({ term });
+    assert.equal(results.length, 0);
+  });
+
+  let allowedTerms = [
+    "@sam sam", // double space is not allowed
+    "@sam.sam",
+    "@"
+  ];
+
+  let topicId = 100;
+
+  allowedTerms.forEach(async term => {
+    let results = await userSearch({ term, topicId });
+    assert.equal(results.length, 6);
+  });
 });

app/models/user_search.rb
@@ -80,7 +80,13 @@ class UserSearch
 
     # 2. in topic
     if @topic_id
-      filtered_by_term_users.where('users.id IN (SELECT p.user_id FROM posts p WHERE topic_id = ?)', @topic_id)
+      in_topic = filtered_by_term_users.where('users.id IN (SELECT p.user_id FROM posts p WHERE topic_id = ?)', @topic_id)
+
+      if @searching_user.present?
+        in_topic = in_topic.where('users.id <> ?', @searching_user.id)
+      end
+
+      in_topic
         .order('last_seen_at DESC')
         .limit(@limit - users.length)
         .pluck(:id)
@@ -90,10 +96,12 @@ class UserSearch
     return users.to_a if users.length >= @limit
 
     # 3. global matches
-    filtered_by_term_users.order('last_seen_at DESC')
-      .limit(@limit - users.length)
-      .pluck(:id)
-      .each { |id| users << id }
+    if !@topic_id || @term.present?
+      filtered_by_term_users.order('last_seen_at DESC')
+        .limit(@limit - users.length)
+        .pluck(:id)
+        .each { |id| users << id }
+    end
 
     users.to_a
   end

spec/models/user_search_spec.rb
@@ -137,6 +137,11 @@ describe UserSearch do
 
     results = search_for(staged.username, include_staged_users: true)
     expect(results.first.username).to eq(staged.username)
+
+    results = search_for("", topic_id: topic.id, searching_user: user1)
+
+    # mrb is omitted, mrb is current user
+    expect(results.map(&:username)).to eq(["mrpink", "mrorange"])
   end
 
 end

commit 6799acd4a7160b37fc32ad86ad29116f7a91cbad

REFACTOR: remove disallowEmails option from user-selector
Negative option was leading to a fair amount of confusion, going forward
if we want to allow selection of emails from user selector it must be
supplied with `allowEmails=true`

This corrects a regression in ffa5e42f which broke invite by emails and
start PM to email

app/assets/javascripts/discourse/components/user-selector.js.es6
@@ -34,7 +34,7 @@ export default TextField.extend({
       single = bool("single"),
       allowAny = bool("allowAny"),
       disabled = bool("disabled"),
-      disallowEmails = bool("disallowEmails"),
+      allowEmails = bool("allowEmails"),
       fullWidthWrap = bool("fullWidthWrap");
 
     function excludedUsernames() {
@@ -67,7 +67,7 @@ export default TextField.extend({
             includeMentionableGroups,
             includeMessageableGroups,
             group: self.get("group"),
-            disallowEmails
+            allowEmails
           });
           return results;
         },

app/assets/javascripts/discourse/lib/user-search.js.es6
@@ -85,7 +85,7 @@ function organizeResults(r, options) {
     });
   }
 
-  if (!options.disallowEmails && emailValid(options.term)) {
+  if (options.allowEmails && emailValid(options.term)) {
     let e = { username: options.term };
     emails = [e];
     results.push(e);
@@ -118,9 +118,13 @@ function organizeResults(r, options) {
 // will not find me, which is a reasonable compromise
 //
 // we also ignore if we notice a double space or a string that is only a space
-const ignoreRegex = /([\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-\/:;<=>?@\[\]^_`{|}~])|\s\s|^\s$/;
+const ignoreRegex = /([\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-\/:;<=>?\[\]^_`{|}~])|\s\s|^\s$/;
 
-function skipSearch(term) {
+function skipSearch(term, allowEmails) {
+  if (term.indexOf("@") > -1 && !allowEmails) {
+    return true;
+  }
+
   return !!term.match(ignoreRegex);
 }
 
@@ -155,7 +159,7 @@ export default function userSearch(options) {
       resolve(CANCELLED_STATUS);
     }, 5000);
 
-    if (skipSearch(term)) {
+    if (skipSearch(term, options.allowEmails)) {
       resolve([]);
       return;
     }

app/assets/javascripts/discourse/templates/components/composer-user-selector.hbs
@@ -7,6 +7,7 @@
                   tabindex="1"
                   usernames=usernames
                   hasGroups=hasGroups
+                  allowEmails='true'
                   autocomplete="discourse"}}
 {{else}}
   <div class='ac-wrap composer-user-selector-limited' {{action "toggleSelector"}}>

app/assets/javascripts/discourse/templates/components/invite-panel.hbs
@@ -29,6 +29,7 @@
           hasGroups=hasGroups
           usernames=emailOrUsername
           placeholderKey=placeholderKey
+          allowEmails=true
           class="invite-user-input"
           autocomplete="discourse"}}
       {{else}}

app/assets/javascripts/discourse/templates/modal/group-add-members.hbs
@@ -9,8 +9,7 @@
           class="input-xxlarge"
           usernames=model.usernames
           placeholderKey="groups.selector_placeholder"
-          id="group-add-members-user-selector"
-          disallowEmails=true}}
+          id="group-add-members-user-selector"}}
     </div>
 
     {{#if currentUser.admin}}

test/javascripts/lib/user-search-test.js.es6
@@ -73,6 +73,7 @@ QUnit.test("it strips @ from the beginning", async assert => {
 });
 
 QUnit.test("it skips a search depending on punctuations", async assert => {
+  let results;
   let skippedTerms = [
     "@sam  s", // double space is not allowed
     "@sam;",
@@ -81,7 +82,7 @@ QUnit.test("it skips a search depending on punctuations", async assert => {
   ];
 
   for (let term of skippedTerms) {
-    let results = await userSearch({ term });
+    results = await userSearch({ term });
     assert.equal(results.length, 0);
   }
 
@@ -94,7 +95,14 @@ QUnit.test("it skips a search depending on punctuations", async assert => {
   let topicId = 100;
 
   for (let term of allowedTerms) {
-    let results = await userSearch({ term, topicId });
+    results = await userSearch({ term, topicId });
     assert.equal(results.length, 6);
   }
+
+  results = await userSearch({ term: "sam@sam.com", allowEmails: true });
+  // 6 + email
+  assert.equal(results.length, 7);
+
+  results = await userSearch({ term: "sam@sam.com" });
+  assert.equal(results.length, 0);
 });
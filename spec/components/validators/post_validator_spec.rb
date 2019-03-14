hanzoai/forum

commit f5dc5af78afa7ef6f379bef77de3ae5c30405bd0

Revert "REFACTOR: remove unnecessary parentheses attempt 2 (follow-up on 71395590)"
This reverts commit 2e479356762a3054172f5d923e7edaf91ef4109e.

Commit breaks post edits for regular users.

app/models/post.rb
@@ -215,7 +215,8 @@ class Post < ActiveRecord::Base
   end
 
   def matches_recent_post?
-    $redis.get(unique_post_key)&.to_i != id
+    post_id = $redis.get(unique_post_key)
+    post_id != (nil) && post_id.to_i != (id)
   end
 
   def raw_hash

commit b1138dd81058ca529949a9836f763ec95d43ce5e

DEV: Improve specs for validating a unique post.
Follow up to f5dc5af78afa7ef6f379bef77de3ae5c30405bd0 which caused post
edits to fail.

spec/components/validators/post_validator_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 require_dependency 'validators/post_validator'
 
 describe Validators::PostValidator do
-  let(:post) { build(:post, topic: Fabricate(:topic)) }
+  let(:topic) { Fabricate(:topic) }
+  let(:post) { build(:post, topic: topic) }
   let(:validator) { Validators::PostValidator.new({}) }
 
   context "#post_body_validator" do
@@ -186,35 +187,51 @@ describe Validators::PostValidator do
   end
 
   describe "unique_post_validator" do
+    let(:user) { Fabricate(:user, id: 999) }
+    let(:post) { Fabricate(:post, user: user, topic: topic) }
+
     before do
       SiteSetting.unique_posts_mins = 5
+      post.store_unique_post_key
+      @key = post.unique_post_key
+    end
+
+    after do
+      $redis.del(@key)
     end
 
     context "post is unique" do
-      before do
-        post.stubs(:matches_recent_post?).returns(false)
+      let(:new_post) do
+        Fabricate.build(:post, user: user, raw: "unique content", topic: topic)
       end
 
       it "should not add an error" do
+        validator.unique_post_validator(new_post)
+        expect(new_post.errors.count).to eq(0)
+      end
+
+      it 'should not add an error when changing an existing post' do
+        post.raw = "changing raw"
+
         validator.unique_post_validator(post)
         expect(post.errors.count).to eq(0)
       end
     end
 
     context "post is not unique" do
-      before do
-        post.stubs(:matches_recent_post?).returns(true)
+      let(:new_post) do
+        Fabricate.build(:post, user: user, raw: post.raw, topic: topic)
       end
 
       it "should add an error" do
-        validator.unique_post_validator(post)
-        expect(post.errors.count).to be > 0
+        validator.unique_post_validator(new_post)
+        expect(new_post.errors.keys).to contain_exactly(:raw)
       end
 
       it "should not add an error if post.skip_unique_check is true" do
-        post.skip_unique_check = true
-        validator.unique_post_validator(post)
-        expect(post.errors.count).to eq(0)
+        new_post.skip_unique_check = true
+        validator.unique_post_validator(new_post)
+        expect(new_post.errors.count).to eq(0)
       end
     end
   end
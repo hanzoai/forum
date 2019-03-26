hanzoai/forum

commit 110526bab6fc8e79734d70d79ae84b59b2641e40

FIX: Post blurb incorrect when search contains a phrase match.
If the blurb generated is not around the search term, we will not be
able to highlight it on the client side.

app/assets/javascripts/discourse/lib/highlight-text.js.es6
@@ -1,7 +1,13 @@
+import { PHRASE_MATCH_REGEXP_PATTERN } from "discourse/lib/concerns/search-constants";
+
 export default function($elem, term) {
   if (!_.isEmpty(term)) {
     // special case ignore "l" which is used for magic sorting
-    let words = _.reject(term.match(/"[^"]+"|[^\s]+/g), t => t === "l");
+    let words = _.reject(
+      term.match(new RegExp(`${PHRASE_MATCH_REGEXP_PATTERN}|[^\s]+`, "g")),
+      t => t === "l"
+    );
+
     words = words.map(w => w.replace(/^"(.*)"$/, "$1"));
     $elem.highlight(words, { className: "search-highlight", wordsOnly: true });
   }

app/assets/javascripts/discourse/lib/concerns/search-constants.js.es6.erb
@@ -1,0 +1,1 @@
+export const PHRASE_MATCH_REGEXP_PATTERN = '<%= Search::PHRASE_MATCH_REGEXP_PATTERN %>';

lib/search.rb
@@ -727,6 +727,8 @@ class Search
     end
   end
 
+  PHRASE_MATCH_REGEXP_PATTERN = '"([^"]+)"'
+
   def posts_query(limit, opts = nil)
     opts ||= {}
 
@@ -770,7 +772,7 @@ class Search
         # D is for cooked
         weights = @in_title ? 'A' : (SiteSetting.tagging_enabled ? 'ABCD' : 'ABD')
         posts = posts.where("post_search_data.search_data @@ #{ts_query(weight_filter: weights)}")
-        exact_terms = @term.scan(/"([^"]+)"/).flatten
+        exact_terms = @term.scan(Regexp.new(PHRASE_MATCH_REGEXP_PATTERN)).flatten
 
         exact_terms.each do |exact|
           posts = posts.where("posts.raw ilike :exact OR topics.title ilike :exact", exact: "%#{exact}%")

lib/search/grouped_search_results.rb
@@ -70,7 +70,16 @@ class Search
 
       if term
         terms = term.split(/\s+/)
-        blurb = TextHelper.excerpt(cooked, terms.first, radius: blurb_length / 2, seperator: " ")
+        phrase = terms.first
+
+        if phrase =~ Regexp.new(Search::PHRASE_MATCH_REGEXP_PATTERN)
+          phrase = Regexp.last_match[1]
+        end
+
+        blurb = TextHelper.excerpt(cooked, phrase,
+          radius: blurb_length / 2,
+          seperator: " "
+        )
       end
 
       blurb = TextHelper.truncate(cooked, length: blurb_length, seperator: " ") if blurb.blank?

spec/components/search_spec.rb
@@ -304,16 +304,35 @@ describe Search do
     end
 
     context 'searching for a post' do
-      let!(:reply) { Fabricate(:basic_reply, topic: topic, user: topic.user) }
-      let(:result) { Search.execute('quotes', type_filter: 'topic', include_blurbs: true) }
+      let!(:reply) do
+        Fabricate(:post_with_long_raw_content,
+          topic: topic,
+          user: topic.user,
+        ).tap { |post| post.update!(raw: "#{post.raw} elephant") }
+      end
+
+      let(:expected_blurb) do
+        "...to satisfy any test conditions that require content longer than the typical test post raw content. elephant"
+      end
 
       it 'returns the post' do
-        expect(result).to be_present
-        expect(result.posts.length).to eq(1)
-        p = result.posts[0]
-        expect(p.topic.id).to eq(topic.id)
-        expect(p.id).to eq(reply.id)
-        expect(result.blurb(p)).to eq("this reply has no quotes")
+        result = Search.execute('elephant',
+          type_filter: 'topic',
+          include_blurbs: true
+        )
+
+        expect(result.posts).to contain_exactly(reply)
+        expect(result.blurb(reply)).to eq(expected_blurb)
+      end
+
+      it 'returns the right post and blurb for searches with phrase' do
+        result = Search.execute('"elephant"',
+          type_filter: 'topic',
+          include_blurbs: true
+        )
+
+        expect(result.posts).to contain_exactly(reply)
+        expect(result.blurb(reply)).to eq(expected_blurb)
       end
     end
 

commit 9ee4f2dc42ad2a2361b86b48491f62799c07310c

Fix regression introduced in 110526bab6fc8e79734d70d79ae84b59b2641e40.
Add tests so it doesn't bite me again.

app/assets/javascripts/discourse/lib/highlight-text.js.es6
@@ -1,14 +1,16 @@
 import { PHRASE_MATCH_REGEXP_PATTERN } from "discourse/lib/concerns/search-constants";
 
+export const CLASS_NAME = "search-highlight";
+
 export default function($elem, term) {
   if (!_.isEmpty(term)) {
     // special case ignore "l" which is used for magic sorting
     let words = _.reject(
-      term.match(new RegExp(`${PHRASE_MATCH_REGEXP_PATTERN}|[^\s]+`, "g")),
+      term.match(new RegExp(`${PHRASE_MATCH_REGEXP_PATTERN}|[^\\s]+`, "g")),
       t => t === "l"
     );
 
     words = words.map(w => w.replace(/^"(.*)"$/, "$1"));
-    $elem.highlight(words, { className: "search-highlight", wordsOnly: true });
+    $elem.highlight(words, { className: CLASS_NAME, wordsOnly: true });
   }
 }

test/javascripts/lib/highlight-text-test.js.es6
@@ -1,0 +1,28 @@
+import {
+  default as highlightText,
+  CLASS_NAME
+} from "discourse/lib/highlight-text";
+
+QUnit.module("lib:highlight-text");
+
+QUnit.test("highlighting text", assert => {
+  fixture().html(
+    `
+    <p>This is some text to highlight</p>
+    `
+  );
+
+  highlightText(fixture(), "some text");
+
+  const terms = [];
+
+  fixture(`.${CLASS_NAME}`).each((_, elem) => {
+    terms.push(elem.textContent);
+  });
+
+  assert.equal(
+    terms.join(" "),
+    "some text",
+    "it should highlight the terms correctly"
+  );
+});
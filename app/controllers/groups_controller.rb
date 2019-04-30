hanzoai/forum

commit 20b4c8f9c869ec5f84830c87e97893ffcbcc2e5b

UX: new layout for groups page, allow changes to automatic group bio (#7350)

app/assets/javascripts/discourse/templates/groups/index.hbs
@@ -25,69 +25,61 @@
     {{#conditional-loading-spinner condition=model.loading}}
       {{#load-more selector=".groups-table .groups-table-row" action=(action "loadMore")}}
         <div class='container'>
-          <table class="groups-table">
-            <thead>
-              <tr>
-                {{directory-toggle field="name" labelKey="groups.group_name" order=order asc=asc}}
-                {{directory-toggle field="user_count" labelKey="groups.user_count" order=order asc=asc}}
-                <th>{{i18n "groups.index.group_type"}}</th>
-                <th>{{i18n "groups.membership"}}</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {{#each model as |group|}}
-                <tr class="groups-table-row">
-                  <td class="groups-info">
-                    {{#link-to "group.members" group.name}}
-                      {{#if group.flair_url}}
-                        <span class='group-avatar-flair'>
-                          {{avatar-flair
-                            flairURL=group.flair_url
-                            flairBgColor=group.flair_bg_color
-                            flairColor=group.flair_color
-                            groupName=group.name}}
-                        </span>
-                      {{/if}}
-
-                      {{groups-info group=group}}
-                    {{/link-to}}
-                  </td>
-
-                  <td class="groups-user-count">{{d-icon "group"}}{{group.user_count}}</td>
-
-                  <td class="groups-table-type">
-                    {{#if group.public_admission}}
-                      {{i18n 'groups.index.public'}}
-                    {{else if group.isPrivate}}
-                      {{d-icon "far-eye-slash"}}
-                      {{i18n 'groups.index.private'}}
-                    {{else}}
-                      {{#if group.automatic}}
-                        {{i18n 'groups.index.automatic'}}
-                      {{else}}
-                        {{i18n 'groups.index.closed'}}
-                      {{/if}}
+          <div class="groups-boxes">
+            {{#each model as |group|}}
+            {{#link-to "group.members" group.name class="group-box"}}
+                <div class="group-box-inner">
+                  <div class="group-info-wrapper">
+                    {{#if group.flair_url}}
+                      <span class='group-avatar-flair'>
+                        {{avatar-flair
+                          flairURL=group.flair_url
+                          flairBgColor=group.flair_bg_color
+                          flairColor=group.flair_color
+                          groupName=group.name}}
+                      </span>
                     {{/if}}
-                  </td>
+                    <span class="group-info">
+                    {{groups-info group=group}}
 
-                  <td class="groups-table-membership">
-                    {{#if group.is_group_owner}}
-                      <span>
+                    <div class="group-user-count">{{d-icon "user"}}{{group.user_count}}</div>
+                  </span>
+                </div>
+
+
+                  <div class="group-description">{{{group.bio_excerpt}}}</div>
+
+                  <div class="group-membership">
+                    {{#group-membership-button tagName='' model=group showLogin=(route-action "showLogin")}}
+                      {{#if group.is_group_owner}}
+                      <span class="is-group-owner">
+                        {{d-icon "shield"}}
                         {{i18n "groups.index.is_group_owner"}}
                       </span>
-                    {{else if group.is_group_user}}
-                      <span>
+                      {{else if group.is_group_user}}
+                      <span class="is-group-member">
+                        {{d-icon "check"}}
                         {{i18n "groups.index.is_group_user"}}
                       </span>
-                    {{/if}}
-
-                    {{group-membership-button tagName='' model=group showLogin=(route-action "showLogin")}}
-                  </td>
-                </tr>
-              {{/each}}
-            </tbody>
-          </table>
+                      {{else if group.public_admission}}
+                        {{i18n 'groups.index.public'}}
+                      {{else if group.isPrivate}}
+                        {{d-icon "far-eye-slash"}}
+                        {{i18n 'groups.index.private'}}
+                      {{else}}
+                        {{#if group.automatic}}
+                          {{i18n 'groups.index.automatic'}}
+                        {{else}}
+                          {{d-icon "ban"}}
+                          {{i18n 'groups.index.closed'}}
+                        {{/if}}
+                      {{/if}}
+                    {{/group-membership-button}}
+                  </div>
+                </div>
+                {{/link-to}}
+            {{/each}}
+          </div>
         </div>
       {{/load-more}}
 

app/controllers/groups_controller.rb
@@ -507,6 +507,7 @@ class GroupsController < ApplicationController
           mentionable_level
           messageable_level
           default_notification_level
+          bio_raw
         }
       else
         default_params = %i{

app/assets/javascripts/discourse/components/groups-form-profile-fields.js.es6
@@ -21,6 +21,8 @@ export default Ember.Component.extend({
     }
   },
 
+  canEdit: Ember.computed.not("model.automatic"),
+
   @computed("basicNameValidation", "uniqueNameValidation")
   nameValidation(basicNameValidation, uniqueNameValidation) {
     return uniqueNameValidation ? uniqueNameValidation : basicNameValidation;

app/assets/javascripts/discourse/controllers/group-manage.js.es6
@@ -6,19 +6,16 @@ export default Ember.Controller.extend({
   @computed("model.automatic")
   tabs(automatic) {
     const defaultTabs = [
+      { route: "group.manage.profile", title: "groups.manage.profile.title" },
       {
         route: "group.manage.interaction",
         title: "groups.manage.interaction.title"
       },
+
       { route: "group.manage.logs", title: "groups.manage.logs.title" }
     ];
 
     if (!automatic) {
-      defaultTabs.splice(0, 0, {
-        route: "group.manage.profile",
-        title: "groups.manage.profile.title"
-      });
-
       defaultTabs.splice(1, 0, {
         route: "group.manage.membership",
         title: "groups.manage.membership.title"

app/assets/javascripts/discourse/routes/group-manage-profile.js.es6
@@ -3,11 +3,5 @@ export default Discourse.Route.extend({
 
   titleToken() {
     return I18n.t("groups.manage.profile.title");
-  },
-
-  afterModel(group) {
-    if (group.get("automatic")) {
-      this.replaceWith("group.manage.interaction", group);
-    }
   }
 });

app/assets/javascripts/discourse/templates/components/group-membership-button.hbs
@@ -16,4 +16,6 @@
       disabled=loading
       icon="user-plus"
       label="groups.request"}}
+{{else}}
+  {{yield}}
 {{/if}}

app/assets/javascripts/discourse/templates/components/groups-form-profile-fields.hbs
@@ -1,36 +1,38 @@
-{{#if this.currentUser.admin}}
-  <div class="control-group">
-    <label class="control-label" for="name">{{i18n 'groups.name'}}</label>
+{{#if canEdit}}
+  {{#if this.currentUser.admin}}
+    <div class="control-group">
+      <label class="control-label" for="name">{{i18n 'groups.name'}}</label>
 
-    {{text-field name="name"
-        class="input-xxlarge group-form-name"
-        value=nameInput
-        placeholderKey="admin.groups.name_placeholder"}}
+      {{text-field name="name"
+          class="input-xxlarge group-form-name"
+          value=nameInput
+          placeholderKey="admin.groups.name_placeholder"}}
 
-    {{input-tip validation=nameValidation}}
-  </div>
-{{/if}}
-
-<div class="control-group">
-  <label class="control-label" for='full_name'>{{i18n 'groups.manage.full_name'}}</label>
-
-  {{text-field name='full_name'
-      class="input-xxlarge group-form-full-name"
-      value=model.full_name}}
-</div>
-
-{{#if this.currentUser.admin}}
-  <div class="control-group">
-    <label class="control-label" for="title">
-      {{i18n 'admin.groups.default_title'}}
-    </label>
-
-    {{input value=model.title name="title" class="input-xxlarge"}}
-
-    <div class="control-instructions">
-      {{i18n 'admin.groups.default_title_description'}}
+      {{input-tip validation=nameValidation}}
     </div>
+  {{/if}}
+
+  <div class="control-group">
+    <label class="control-label" for='full_name'>{{i18n 'groups.manage.full_name'}}</label>
+
+    {{text-field name='full_name'
+        class="input-xxlarge group-form-full-name"
+        value=model.full_name}}
   </div>
+
+  {{#if this.currentUser.admin}}
+    <div class="control-group">
+      <label class="control-label" for="title">
+        {{i18n 'admin.groups.default_title'}}
+      </label>
+
+      {{input value=model.title name="title" class="input-xxlarge"}}
+
+      <div class="control-instructions">
+        {{i18n 'admin.groups.default_title_description'}}
+      </div>
+    </div>
+  {{/if}}
 {{/if}}
 
 <div class="control-group">
@@ -38,10 +40,12 @@
   {{d-editor value=model.bio_raw class="group-form-bio input-xxlarge"}}
 </div>
 
-{{yield}}
+{{#if canEdit}}
+  {{yield}}
 
-<div class="control-group">
-  {{group-flair-inputs model=model}}
-</div>
+  <div class="control-group">
+    {{group-flair-inputs model=model}}
+  </div>
 
-{{plugin-outlet name="group-edit" args=(hash group=model)}}
+  {{plugin-outlet name="group-edit" args=(hash group=model)}}
+{{/if}}

app/assets/javascripts/discourse/templates/components/groups-info.hbs
@@ -1,11 +1,5 @@
-<span class='groups-info-name'>{{group.displayName}}</span>
-
 {{#if showFullName}}
-  <span class='groups-info-full-name'>{{group.full_name}}</span>
-{{/if}}
-
-{{#if group.title}}
-  <div>
-    <span class='groups-info-title'>{{group.title}}</span>
-  </div>
+  <span class='groups-info-name'>{{group.full_name}}</span>
+{{else}}
+  <span class='groups-info-name'>{{group.displayName}}</span>
 {{/if}}

commit 997c2f5b80411641a2860ac4c9f5dbd1be217d50

FIX: Loading more broken on groups page.
Regressed in 20b4c8f9c869ec5f84830c87e97893ffcbcc2e5b.

app/assets/javascripts/discourse/templates/groups/index.hbs
@@ -23,11 +23,11 @@
 
   {{#if model}}
     {{#conditional-loading-spinner condition=model.loading}}
-      {{#load-more selector=".groups-table .groups-table-row" action=(action "loadMore")}}
+      {{#load-more selector=".groups-boxes .group-box" action=(action "loadMore")}}
         <div class='container'>
           <div class="groups-boxes">
             {{#each model as |group|}}
-            {{#link-to "group.members" group.name class="group-box"}}
+              {{#link-to "group.members" group.name class="group-box"}}
                 <div class="group-box-inner">
                   <div class="group-info-wrapper">
                     {{#if group.flair_url}}
@@ -39,13 +39,13 @@
                           groupName=group.name}}
                       </span>
                     {{/if}}
+
                     <span class="group-info">
-                    {{groups-info group=group}}
+                      {{groups-info group=group}}
 
-                    <div class="group-user-count">{{d-icon "user"}}{{group.user_count}}</div>
-                  </span>
-                </div>
-
+                      <div class="group-user-count">{{d-icon "user"}}{{group.user_count}}</div>
+                    </span>
+                  </div>
 
                   <div class="group-description">{{{group.bio_excerpt}}}</div>
 
@@ -77,7 +77,7 @@
                     {{/group-membership-button}}
                   </div>
                 </div>
-                {{/link-to}}
+              {{/link-to}}
             {{/each}}
           </div>
         </div>

app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  include ApplicationHelper
+
   requires_login only: [
     :set_notifications,
     :mentionable,
@@ -42,7 +44,7 @@ class GroupsController < ApplicationController
       raise Discourse::InvalidAccess.new(:enable_group_directory)
     end
 
-    page_size = 30
+    page_size = mobile_device? ? 15 : 36
     page = params[:page]&.to_i || 0
     order = %w{name user_count}.delete(params[:order])
     dir = params[:asc] ? 'ASC' : 'DESC'
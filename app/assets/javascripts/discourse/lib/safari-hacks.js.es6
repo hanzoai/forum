hanzoai/forum

commit 548f2e17ca7b192d75a5f3460d993c2e190a8a8d

FIX: do not unpin reply box in iOS when selecting Emoji

app/assets/javascripts/discourse/lib/safari-hacks.js.es6
@@ -117,6 +117,7 @@ function positioningWorkaround($fixedElement) {
     if (
       lastTouchedElement &&
       ($(lastTouchedElement).hasClass("select-kit-header") ||
+        $(lastTouchedElement).closest(".autocomplete") ||
         ["span", "svg", "button"].includes(
           lastTouchedElement.nodeName.toLowerCase()
         ))

commit b42658ef46fd966fef9236397b7a95a74ec0b7c5

UX: fixes regression with iOS composer
Fixes a bug introduced in 548f2e17ca7b192d75a5f3460d993c2e190a8a8d where dismissing the keyboard in iOS would not minimize the composer.

app/assets/javascripts/discourse/lib/safari-hacks.js.es6
@@ -117,7 +117,7 @@ function positioningWorkaround($fixedElement) {
     if (
       lastTouchedElement &&
       ($(lastTouchedElement).hasClass("select-kit-header") ||
-        $(lastTouchedElement).closest(".autocomplete") ||
+        $(lastTouchedElement).closest(".autocomplete").length ||
         ["span", "svg", "button"].includes(
           lastTouchedElement.nodeName.toLowerCase()
         ))
hanzoai/forum

commit 4e5b537c9dd2fe8628159b09f8384ed8489d054e

FIX: Make FooterNav work with PWAs on iPadOS

app/assets/javascripts/discourse/lib/utilities.js.es6
@@ -367,9 +367,10 @@ export function areCookiesEnabled() {
 }
 
 export function isiOSPWA() {
+  const caps = Discourse.__container__.lookup("capabilities:main");
   return (
     window.matchMedia("(display-mode: standalone)").matches &&
-    navigator.userAgent.match(/(iPad|iPhone|iPod)/g)
+    caps.isIOS
   );
 }
 

commit fca3a4702744912c9799665c07fadf7d8c1b363a

DEV: Fix lint error introduced in 4e5b537

app/assets/javascripts/discourse/lib/utilities.js.es6
@@ -368,10 +368,7 @@ export function areCookiesEnabled() {
 
 export function isiOSPWA() {
   const caps = Discourse.__container__.lookup("capabilities:main");
-  return (
-    window.matchMedia("(display-mode: standalone)").matches &&
-    caps.isIOS
-  );
+  return window.matchMedia("(display-mode: standalone)").matches && caps.isIOS;
 }
 
 export function isAppWebview() {
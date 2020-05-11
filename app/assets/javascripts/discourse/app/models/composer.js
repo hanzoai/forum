hanzoai/forum

commit cbe6eb4e0e372d2c129e1b40418c426476ee0d24

FIX: default to blank title on open
In production `title` was set to undefined causing a
discrepancy between originalTitle and title

This attempts to work around the issue in the production bundle

app/assets/javascripts/discourse/app/models/composer.js
@@ -785,7 +785,10 @@ const Composer = RestModel.extend({
       });
     }
 
-    this.set("originalTitle", "");
+    if (this.get("canEditTitle")) {
+      this.set("originalTitle", "");
+      this.set("title", "");
+    }
 
     if (opts.title) {
       this.set("title", opts.title);

commit 214a45593304e21c568dbe2d5ecab4b7d634f661

FIX: stop clearing title by mistake
cbe6eb4e0e372d caused a short lived regression.

This code is very deliberate about ensuring originalTitle is
set correctly

app/assets/javascripts/discourse/app/models/composer.js
@@ -785,18 +785,16 @@ const Composer = RestModel.extend({
       });
     }
 
-    if (this.get("canEditTitle")) {
-      this.set("originalTitle", "");
-      this.set("title", "");
-    }
-
     if (opts.title) {
       this.set("title", opts.title);
     }
 
     this.set("originalText", opts.draft ? "" : this.reply);
 
-    if (this.editingFirstPost) {
+    if (this.canEditTitle) {
+      if (isEmpty(this.title) && this.title !== "") {
+        this.set("title", "");
+      }
       this.set("originalTitle", this.title);
     }
 
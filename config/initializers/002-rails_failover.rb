hanzoai/forum

commit 8f4272302e9d25b618d3185766d50d9d6776bfc5

DEV: Only failover the entire cluster when the default db goes down.

config/initializers/002-rails_failover.rb
@@ -31,9 +31,11 @@ if defined?(RailsFailover::ActiveRecord)
   end
 
   RailsFailover::ActiveRecord.on_failover do
-    RailsMultisite::ConnectionManagement.each_connection do
-      Discourse.enable_readonly_mode(Discourse::PG_READONLY_MODE_KEY)
-      Sidekiq.pause!("pg_failover") if !Sidekiq.paused?
+    if RailsMultisite::ConnectionManagement.current_db = RailsMultisite::ConnectionManagement::DEFAULT
+      RailsMultisite::ConnectionManagement.each_connection do
+        Discourse.enable_readonly_mode(Discourse::PG_READONLY_MODE_KEY)
+        Sidekiq.pause!("pg_failover") if !Sidekiq.paused?
+      end
     end
   end
 

commit 700421073ab7525e04da09fd2533711ba19ab31f

DEV: Fix error introduced in 8f4272302.

config/initializers/002-rails_failover.rb
@@ -31,7 +31,7 @@ if defined?(RailsFailover::ActiveRecord)
   end
 
   RailsFailover::ActiveRecord.on_failover do
-    if RailsMultisite::ConnectionManagement.current_db = RailsMultisite::ConnectionManagement::DEFAULT
+    if RailsMultisite::ConnectionManagement.current_db == RailsMultisite::ConnectionManagement::DEFAULT
       RailsMultisite::ConnectionManagement.each_connection do
         Discourse.enable_readonly_mode(Discourse::PG_READONLY_MODE_KEY)
         Sidekiq.pause!("pg_failover") if !Sidekiq.paused?
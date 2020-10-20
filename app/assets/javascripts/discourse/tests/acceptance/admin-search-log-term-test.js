alebertacco/discourse

REFACTOR: New format for acceptance tests
This gets us closer to how newer Ember versions want to do things, but
with a bit of Discourse flair.

`acceptance` now takes a function as a parameter, and tests need to be
declared in that new function context.

A new helper, `needs`, is passed as a parameter. You can use it to set
up the test the way you want.

app/assets/javascripts/discourse/tests/acceptance/admin-search-log-term-test.js
@@ -1,12 +1,15 @@
 import { visit } from "@ember/test-helpers";
-import { skip } from "qunit";
+import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
-acceptance("Admin - Search Log Term", { loggedIn: true });
 
-skip("show search log term details", async (assert) => {
-  await visit("/admin/logs/search_logs/term?term=ruby");
+acceptance("Admin - Search Log Term", function (needs) {
+  needs.authenticatedUser();
 
-  assert.ok($("div.search-logs-filter").length, "has the search type filter");
-  assert.ok(exists("canvas.chartjs-render-monitor"), "has graph canvas");
-  assert.ok(exists("div.header-search-results"), "has header search results");
+  test("show search log term details", async (assert) => {
+    await visit("/admin/logs/search_logs/term?term=ruby");
+
+    assert.ok($("div.search-logs-filter").length, "has the search type filter");
+    assert.ok(exists("canvas.chartjs-render-monitor"), "has graph canvas");
+    assert.ok(exists("div.header-search-results"), "has header search results");
+  });
 });

app/assets/javascripts/discourse/tests/acceptance/admin-search-logs-test.js
@@ -1,22 +1,25 @@
 import { visit } from "@ember/test-helpers";
-import { skip } from "qunit";
+import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
-acceptance("Admin - Search Logs", { loggedIn: true });
 
-skip("show search logs", async (assert) => {
-  await visit("/admin/logs/search_logs");
+acceptance("Admin - Search Logs", function (needs) {
+  needs.authenticatedUser();
 
-  assert.ok($("table.search-logs-list.grid").length, "has the div class");
+  test("show search logs", async (assert) => {
+    await visit("/admin/logs/search_logs");
 
-  assert.ok(
-    exists(".search-logs-list .admin-list-item .col"),
-    "has a list of search logs"
-  );
+    assert.ok($("table.search-logs-list.grid").length, "has the div class");
 
-  await click(".term a");
+    assert.ok(
+      exists(".search-logs-list .admin-list-item .col"),
+      "has a list of search logs"
+    );
 
-  assert.ok(
-    $("div.search-logs-filter").length,
-    "it should show the search log term page"
-  );
+    await click(".term a");
+
+    assert.ok(
+      $("div.search-logs-filter").length,
+      "it should show the search log term page"
+    );
+  });
 });

app/assets/javascripts/discourse/tests/acceptance/admin-user-index-test.js
@@ -2,12 +2,11 @@ import { visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import selectKit from "discourse/tests/helpers/select-kit-helper";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
-import pretender from "discourse/tests/helpers/create-pretender";
 
-acceptance("Admin - User Index", {
-  loggedIn: true,
-  pretend(pretenderServer, helper) {
-    pretenderServer.get("/groups/search.json", () => {
+acceptance("Admin - User Index", function (needs) {
+  needs.authenticatedUser();
+  needs.pretender((server, helper) => {
+    server.get("/groups/search.json", () => {
       return helper.response([
         {
           id: 42,
@@ -33,60 +32,56 @@ acceptance("Admin - User Index", {
         },
       ]);
     });
-  },
-});
 
-test("can edit username", async (assert) => {
-  pretender.put("/users/sam/preferences/username", () => [
-    200,
-    {
-      "Content-Type": "application/json",
-    },
-    { id: 2, username: "new-sam" },
-  ]);
+    server.put("/users/sam/preferences/username", () => {
+      return helper.response({ id: 2, username: "new-sam" });
+    });
+  });
 
-  await visit("/admin/users/2/sam");
+  test("can edit username", async (assert) => {
+    await visit("/admin/users/2/sam");
 
-  assert.equal(find(".display-row.username .value").text().trim(), "sam");
+    assert.equal(find(".display-row.username .value").text().trim(), "sam");
 
-  // Trying cancel.
-  await click(".display-row.username button");
-  await fillIn(".display-row.username .value input", "new-sam");
-  await click(".display-row.username a");
-  assert.equal(find(".display-row.username .value").text().trim(), "sam");
+    // Trying cancel.
+    await click(".display-row.username button");
+    await fillIn(".display-row.username .value input", "new-sam");
+    await click(".display-row.username a");
+    assert.equal(find(".display-row.username .value").text().trim(), "sam");
 
-  // Doing edit.
-  await click(".display-row.username button");
-  await fillIn(".display-row.username .value input", "new-sam");
-  await click(".display-row.username button");
-  assert.equal(find(".display-row.username .value").text().trim(), "new-sam");
-});
+    // Doing edit.
+    await click(".display-row.username button");
+    await fillIn(".display-row.username .value input", "new-sam");
+    await click(".display-row.username button");
+    assert.equal(find(".display-row.username .value").text().trim(), "new-sam");
+  });
 
-test("will clear unsaved groups when switching user", async (assert) => {
-  await visit("/admin/users/2/sam");
+  test("will clear unsaved groups when switching user", async (assert) => {
+    await visit("/admin/users/2/sam");
 
-  assert.equal(
-    find(".display-row.username .value").text().trim(),
-    "sam",
-    "the name should be correct"
-  );
+    assert.equal(
+      find(".display-row.username .value").text().trim(),
+      "sam",
+      "the name should be correct"
+    );
 
-  const groupChooser = selectKit(".group-chooser");
-  await groupChooser.expand();
-  await groupChooser.selectRowByValue(42);
-  assert.equal(groupChooser.header().value(), 42, "group should be set");
+    const groupChooser = selectKit(".group-chooser");
+    await groupChooser.expand();
+    await groupChooser.selectRowByValue(42);
+    assert.equal(groupChooser.header().value(), 42, "group should be set");
 
-  await visit("/admin/users/1/eviltrout");
+    await visit("/admin/users/1/eviltrout");
 
-  assert.equal(
-    find(".display-row.username .value").text().trim(),
-    "eviltrout",
-    "the name should be correct"
-  );
+    assert.equal(
+      find(".display-row.username .value").text().trim(),
+      "eviltrout",
+      "the name should be correct"
+    );
 
-  assert.equal(
-    find('.group-chooser span[title="Macdonald"]').length,
-    0,
-    "group should not be set"
-  );
+    assert.equal(
+      find('.group-chooser span[title="Macdonald"]').length,
+      0,
+      "group should not be set"
+    );
+  });
 });

app/assets/javascripts/discourse/tests/acceptance/admin-watched-words-test.js
@@ -1,75 +1,78 @@
 import { visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
-acceptance("Admin - Watched Words", { loggedIn: true });
 
-test("list words in groups", async (assert) => {
-  await visit("/admin/logs/watched_words/action/block");
+acceptance("Admin - Watched Words", function (needs) {
+  needs.authenticatedUser();
 
-  assert.ok(exists(".watched-words-list"));
-  assert.ok(
-    !exists(".watched-words-list .watched-word"),
-    "Don't show bad words by default."
-  );
+  test("list words in groups", async (assert) => {
+    await visit("/admin/logs/watched_words/action/block");
 
-  await fillIn(".admin-controls .controls input[type=text]", "li");
+    assert.ok(exists(".watched-words-list"));
+    assert.ok(
+      !exists(".watched-words-list .watched-word"),
+      "Don't show bad words by default."
+    );
 
-  assert.equal(
-    find(".watched-words-list .watched-word").length,
-    1,
-    "When filtering, show words even if checkbox is unchecked."
-  );
+    await fillIn(".admin-controls .controls input[type=text]", "li");
 
-  await fillIn(".admin-controls .controls input[type=text]", "");
+    assert.equal(
+      find(".watched-words-list .watched-word").length,
+      1,
+      "When filtering, show words even if checkbox is unchecked."
+    );
 
-  assert.ok(
-    !exists(".watched-words-list .watched-word"),
-    "Clearing the filter hides words again."
-  );
+    await fillIn(".admin-controls .controls input[type=text]", "");
 
-  await click(".show-words-checkbox");
+    assert.ok(
+      !exists(".watched-words-list .watched-word"),
+      "Clearing the filter hides words again."
+    );
 
-  assert.ok(
-    exists(".watched-words-list .watched-word"),
-    "Always show the words when checkbox is checked."
-  );
+    await click(".show-words-checkbox");
 
-  await click(".nav-stacked .censor a");
+    assert.ok(
+      exists(".watched-words-list .watched-word"),
+      "Always show the words when checkbox is checked."
+    );
 
-  assert.ok(exists(".watched-words-list"));
-  assert.ok(!exists(".watched-words-list .watched-word"), "Empty word list.");
-});
+    await click(".nav-stacked .censor a");
 
-test("add words", async (assert) => {
-  await visit("/admin/logs/watched_words/action/block");
-
-  click(".show-words-checkbox");
-  fillIn(".watched-word-form input", "poutine");
-
-  await click(".watched-word-form button");
-
-  let found = [];
-  $.each(find(".watched-words-list .watched-word"), (index, elem) => {
-    if ($(elem).text().trim() === "poutine") {
-      found.push(true);
-    }
-  });
-  assert.equal(found.length, 1);
-});
-
-test("remove words", async (assert) => {
-  await visit("/admin/logs/watched_words/action/block");
-  await click(".show-words-checkbox");
-
-  let word = null;
-
-  $.each(find(".watched-words-list .watched-word"), (index, elem) => {
-    if ($(elem).text().trim() === "anise") {
-      word = elem;
-    }
+    assert.ok(exists(".watched-words-list"));
+    assert.ok(!exists(".watched-words-list .watched-word"), "Empty word list.");
   });
 
-  await click("#" + $(word).attr("id"));
+  test("add words", async (assert) => {
+    await visit("/admin/logs/watched_words/action/block");
 
-  assert.equal(find(".watched-words-list .watched-word").length, 2);
+    click(".show-words-checkbox");
+    fillIn(".watched-word-form input", "poutine");
+
+    await click(".watched-word-form button");
+
+    let found = [];
+    $.each(find(".watched-words-list .watched-word"), (index, elem) => {
+      if ($(elem).text().trim() === "poutine") {
+        found.push(true);
+      }
+    });
+    assert.equal(found.length, 1);
+  });
+
+  test("remove words", async (assert) => {
+    await visit("/admin/logs/watched_words/action/block");
+    await click(".show-words-checkbox");
+
+    let word = null;
+
+    $.each(find(".watched-words-list .watched-word"), (index, elem) => {
+      if ($(elem).text().trim() === "anise") {
+        word = elem;
+      }
+    });
+
+    await click("#" + $(word).attr("id"));
+
+    assert.equal(find(".watched-words-list .watched-word").length, 2);
+  });
 });

app/assets/javascripts/discourse/tests/acceptance/category-banner-test.js
@@ -3,8 +3,9 @@ import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import DiscoveryFixtures from "discourse/tests/fixtures/discovery-fixtures";
 
-acceptance("Category Banners", {
-  pretend(server, helper) {
+acceptance("Category Banners", function (needs) {
+  needs.authenticatedUser();
+  needs.pretender((server, helper) => {
     server.get("/c/test-read-only-without-banner/5/l/latest.json", () => {
       return helper.response(
         DiscoveryFixtures["/latest_can_create_topic.json"]
@@ -15,9 +16,8 @@ acceptance("Category Banners", {
         DiscoveryFixtures["/latest_can_create_topic.json"]
       );
     });
-  },
-  loggedIn: true,
-  site: {
+  });
+  needs.site({
     categories: [
       {
         id: 5,
@@ -34,45 +34,44 @@ acceptance("Category Banners", {
           "You need to video yourself <div class='inner'>doing</div> the secret handshake to post here",
       },
     ],
-  },
+  });
+
+  test("Does not display category banners when not set", async (assert) => {
+    await visit("/c/test-read-only-without-banner");
+
+    await click("#create-topic");
+    assert.ok(!visible(".bootbox.modal"), "it does not pop up a modal");
+    assert.ok(
+      !visible(".category-read-only-banner"),
+      "it does not show a banner"
+    );
+  });
+
+  test("Displays category banners when set", async (assert) => {
+    await visit("/c/test-read-only-with-banner");
+
+    await click("#create-topic");
+    assert.ok(visible(".bootbox.modal"), "it pops up a modal");
+
+    await click(".modal-footer>.btn-primary");
+    assert.ok(!visible(".bootbox.modal"), "it closes the modal");
+    assert.ok(visible(".category-read-only-banner"), "it shows a banner");
+    assert.ok(
+      find(".category-read-only-banner .inner").length === 1,
+      "it allows staff to embed html in the message"
+    );
+  });
 });
 
-test("Does not display category banners when not set", async (assert) => {
-  await visit("/c/test-read-only-without-banner");
-
-  await click("#create-topic");
-  assert.ok(!visible(".bootbox.modal"), "it does not pop up a modal");
-  assert.ok(
-    !visible(".category-read-only-banner"),
-    "it does not show a banner"
-  );
-});
-
-test("Displays category banners when set", async (assert) => {
-  await visit("/c/test-read-only-with-banner");
-
-  await click("#create-topic");
-  assert.ok(visible(".bootbox.modal"), "it pops up a modal");
-
-  await click(".modal-footer>.btn-primary");
-  assert.ok(!visible(".bootbox.modal"), "it closes the modal");
-  assert.ok(visible(".category-read-only-banner"), "it shows a banner");
-  assert.ok(
-    find(".category-read-only-banner .inner").length === 1,
-    "it allows staff to embed html in the message"
-  );
-});
-
-acceptance("Anonymous Category Banners", {
-  pretend(server, helper) {
+acceptance("Anonymous Category Banners", function (needs) {
+  needs.pretender((server, helper) => {
     server.get("/c/test-read-only-with-banner/6/l/latest.json", () => {
       return helper.response(
         DiscoveryFixtures["/latest_can_create_topic.json"]
       );
     });
-  },
-  loggedIn: false,
-  site: {
+  });
+  needs.site({
     categories: [
       {
         id: 6,
@@ -83,13 +82,13 @@ acceptance("Anonymous Category Banners", {
           "You need to video yourself doing the secret handshake to post here",
       },
     ],
-  },
-});
+  });
 
-test("Does not display category banners when set", async (assert) => {
-  await visit("/c/test-read-only-with-banner");
-  assert.ok(
-    !visible(".category-read-only-banner"),
-    "it does not show a banner"
-  );
+  test("Does not display category banners when set", async (assert) => {
+    await visit("/c/test-read-only-with-banner");
+    assert.ok(
+      !visible(".category-read-only-banner"),
+      "it does not show a banner"
+    );
+  });
 });

hanzoai/forum

app/assets/javascripts/discourse/tests/acceptance/admin-search-log-term-test.js
@@ -1,12 +1,15 @@
 import { visit } from "@ember/test-helpers";
-import { skip } from "qunit";
+import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
-acceptance("Admin - Search Log Term", { loggedIn: true });
 
-skip("show search log term details", async (assert) => {
-  await visit("/admin/logs/search_logs/term?term=ruby");
+acceptance("Admin - Search Log Term", function (needs) {
+  needs.user();
 
-  assert.ok($("div.search-logs-filter").length, "has the search type filter");
-  assert.ok(exists("canvas.chartjs-render-monitor"), "has graph canvas");
-  assert.ok(exists("div.header-search-results"), "has header search results");
+  test("show search log term details", async (assert) => {
+    await visit("/admin/logs/search_logs/term?term=ruby");
+
+    assert.ok($("div.search-logs-filter").length, "has the search type filter");
+    assert.ok(exists("canvas.chartjs-render-monitor"), "has graph canvas");
+    assert.ok(exists("div.header-search-results"), "has header search results");
+  });
 });

app/assets/javascripts/discourse/tests/acceptance/admin-search-logs-test.js
@@ -1,22 +1,25 @@
 import { visit } from "@ember/test-helpers";
-import { skip } from "qunit";
+import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
-acceptance("Admin - Search Logs", { loggedIn: true });
 
-skip("show search logs", async (assert) => {
-  await visit("/admin/logs/search_logs");
+acceptance("Admin - Search Logs", function (needs) {
+  needs.user();
 
-  assert.ok($("table.search-logs-list.grid").length, "has the div class");
+  test("show search logs", async (assert) => {
+    await visit("/admin/logs/search_logs");
 
-  assert.ok(
-    exists(".search-logs-list .admin-list-item .col"),
-    "has a list of search logs"
-  );
+    assert.ok($("table.search-logs-list.grid").length, "has the div class");
 
-  await click(".term a");
+    assert.ok(
+      exists(".search-logs-list .admin-list-item .col"),
+      "has a list of search logs"
+    );
 
-  assert.ok(
-    $("div.search-logs-filter").length,
-    "it should show the search log term page"
-  );
+    await click(".term a");
+
+    assert.ok(
+      $("div.search-logs-filter").length,
+      "it should show the search log term page"
+    );
+  });
 });

app/assets/javascripts/discourse/tests/acceptance/admin-user-index-test.js
@@ -2,12 +2,11 @@ import { visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import selectKit from "discourse/tests/helpers/select-kit-helper";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
-import pretender from "discourse/tests/helpers/create-pretender";
 
-acceptance("Admin - User Index", {
-  loggedIn: true,
-  pretend(pretenderServer, helper) {
-    pretenderServer.get("/groups/search.json", () => {
+acceptance("Admin - User Index", function (needs) {
+  needs.user();
+  needs.pretender((server, helper) => {
+    server.get("/groups/search.json", () => {
       return helper.response([
         {
           id: 42,
@@ -33,60 +32,56 @@ acceptance("Admin - User Index", {
         },
       ]);
     });
-  },
-});
 
-test("can edit username", async (assert) => {
-  pretender.put("/users/sam/preferences/username", () => [
-    200,
-    {
-      "Content-Type": "application/json",
-    },
-    { id: 2, username: "new-sam" },
-  ]);
+    server.put("/users/sam/preferences/username", () => {
+      return helper.response({ id: 2, username: "new-sam" });
+    });
+  });
 
-  await visit("/admin/users/2/sam");
+  test("can edit username", async (assert) => {
+    await visit("/admin/users/2/sam");
 
-  assert.equal(find(".display-row.username .value").text().trim(), "sam");
+    assert.equal(find(".display-row.username .value").text().trim(), "sam");
 
-  // Trying cancel.
-  await click(".display-row.username button");
-  await fillIn(".display-row.username .value input", "new-sam");
-  await click(".display-row.username a");
-  assert.equal(find(".display-row.username .value").text().trim(), "sam");
+    // Trying cancel.
+    await click(".display-row.username button");
+    await fillIn(".display-row.username .value input", "new-sam");
+    await click(".display-row.username a");
+    assert.equal(find(".display-row.username .value").text().trim(), "sam");
 
-  // Doing edit.
-  await click(".display-row.username button");
-  await fillIn(".display-row.username .value input", "new-sam");
-  await click(".display-row.username button");
-  assert.equal(find(".display-row.username .value").text().trim(), "new-sam");
-});
+    // Doing edit.
+    await click(".display-row.username button");
+    await fillIn(".display-row.username .value input", "new-sam");
+    await click(".display-row.username button");
+    assert.equal(find(".display-row.username .value").text().trim(), "new-sam");
+  });
 
-test("will clear unsaved groups when switching user", async (assert) => {
-  await visit("/admin/users/2/sam");
+  test("will clear unsaved groups when switching user", async (assert) => {
+    await visit("/admin/users/2/sam");
 
-  assert.equal(
-    find(".display-row.username .value").text().trim(),
-    "sam",
-    "the name should be correct"
-  );
+    assert.equal(
+      find(".display-row.username .value").text().trim(),
+      "sam",
+      "the name should be correct"
+    );
 
-  const groupChooser = selectKit(".group-chooser");
-  await groupChooser.expand();
-  await groupChooser.selectRowByValue(42);
-  assert.equal(groupChooser.header().value(), 42, "group should be set");
+    const groupChooser = selectKit(".group-chooser");
+    await groupChooser.expand();
+    await groupChooser.selectRowByValue(42);
+    assert.equal(groupChooser.header().value(), 42, "group should be set");
 
-  await visit("/admin/users/1/eviltrout");
+    await visit("/admin/users/1/eviltrout");
 
-  assert.equal(
-    find(".display-row.username .value").text().trim(),
-    "eviltrout",
-    "the name should be correct"
-  );
+    assert.equal(
+      find(".display-row.username .value").text().trim(),
+      "eviltrout",
+      "the name should be correct"
+    );
 
-  assert.equal(
-    find('.group-chooser span[title="Macdonald"]').length,
-    0,
-    "group should not be set"
-  );
+    assert.equal(
+      find('.group-chooser span[title="Macdonald"]').length,
+      0,
+      "group should not be set"
+    );
+  });
 });

app/assets/javascripts/discourse/tests/acceptance/admin-watched-words-test.js
@@ -1,75 +1,78 @@
 import { visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
-acceptance("Admin - Watched Words", { loggedIn: true });
 
-test("list words in groups", async (assert) => {
-  await visit("/admin/logs/watched_words/action/block");
+acceptance("Admin - Watched Words", function (needs) {
+  needs.user();
 
-  assert.ok(exists(".watched-words-list"));
-  assert.ok(
-    !exists(".watched-words-list .watched-word"),
-    "Don't show bad words by default."
-  );
+  test("list words in groups", async (assert) => {
+    await visit("/admin/logs/watched_words/action/block");
 
-  await fillIn(".admin-controls .controls input[type=text]", "li");
+    assert.ok(exists(".watched-words-list"));
+    assert.ok(
+      !exists(".watched-words-list .watched-word"),
+      "Don't show bad words by default."
+    );
 
-  assert.equal(
-    find(".watched-words-list .watched-word").length,
-    1,
-    "When filtering, show words even if checkbox is unchecked."
-  );
+    await fillIn(".admin-controls .controls input[type=text]", "li");
 
-  await fillIn(".admin-controls .controls input[type=text]", "");
+    assert.equal(
+      find(".watched-words-list .watched-word").length,
+      1,
+      "When filtering, show words even if checkbox is unchecked."
+    );
 
-  assert.ok(
-    !exists(".watched-words-list .watched-word"),
-    "Clearing the filter hides words again."
-  );
+    await fillIn(".admin-controls .controls input[type=text]", "");
 
-  await click(".show-words-checkbox");
+    assert.ok(
+      !exists(".watched-words-list .watched-word"),
+      "Clearing the filter hides words again."
+    );
 
-  assert.ok(
-    exists(".watched-words-list .watched-word"),
-    "Always show the words when checkbox is checked."
-  );
+    await click(".show-words-checkbox");
 
-  await click(".nav-stacked .censor a");
+    assert.ok(
+      exists(".watched-words-list .watched-word"),
+      "Always show the words when checkbox is checked."
+    );
 
-  assert.ok(exists(".watched-words-list"));
-  assert.ok(!exists(".watched-words-list .watched-word"), "Empty word list.");
-});
+    await click(".nav-stacked .censor a");
 
-test("add words", async (assert) => {
-  await visit("/admin/logs/watched_words/action/block");
-
-  click(".show-words-checkbox");
-  fillIn(".watched-word-form input", "poutine");
-
-  await click(".watched-word-form button");
-
-  let found = [];
-  $.each(find(".watched-words-list .watched-word"), (index, elem) => {
-    if ($(elem).text().trim() === "poutine") {
-      found.push(true);
-    }
-  });
-  assert.equal(found.length, 1);
-});
-
-test("remove words", async (assert) => {
-  await visit("/admin/logs/watched_words/action/block");
-  await click(".show-words-checkbox");
-
-  let word = null;
-
-  $.each(find(".watched-words-list .watched-word"), (index, elem) => {
-    if ($(elem).text().trim() === "anise") {
-      word = elem;
-    }
+    assert.ok(exists(".watched-words-list"));
+    assert.ok(!exists(".watched-words-list .watched-word"), "Empty word list.");
   });
 
-  await click("#" + $(word).attr("id"));
+  test("add words", async (assert) => {
+    await visit("/admin/logs/watched_words/action/block");
 
-  assert.equal(find(".watched-words-list .watched-word").length, 2);
+    click(".show-words-checkbox");
+    fillIn(".watched-word-form input", "poutine");
+
+    await click(".watched-word-form button");
+
+    let found = [];
+    $.each(find(".watched-words-list .watched-word"), (index, elem) => {
+      if ($(elem).text().trim() === "poutine") {
+        found.push(true);
+      }
+    });
+    assert.equal(found.length, 1);
+  });
+
+  test("remove words", async (assert) => {
+    await visit("/admin/logs/watched_words/action/block");
+    await click(".show-words-checkbox");
+
+    let word = null;
+
+    $.each(find(".watched-words-list .watched-word"), (index, elem) => {
+      if ($(elem).text().trim() === "anise") {
+        word = elem;
+      }
+    });
+
+    await click("#" + $(word).attr("id"));
+
+    assert.equal(find(".watched-words-list .watched-word").length, 2);
+  });
 });

app/assets/javascripts/discourse/tests/acceptance/category-banner-test.js
@@ -3,8 +3,9 @@ import { test } from "qunit";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import DiscoveryFixtures from "discourse/tests/fixtures/discovery-fixtures";
 
-acceptance("Category Banners", {
-  pretend(server, helper) {
+acceptance("Category Banners", function (needs) {
+  needs.user();
+  needs.pretender((server, helper) => {
     server.get("/c/test-read-only-without-banner/5/l/latest.json", () => {
       return helper.response(
         DiscoveryFixtures["/latest_can_create_topic.json"]
@@ -15,9 +16,8 @@ acceptance("Category Banners", {
         DiscoveryFixtures["/latest_can_create_topic.json"]
       );
     });
-  },
-  loggedIn: true,
-  site: {
+  });
+  needs.site({
     categories: [
       {
         id: 5,
@@ -34,45 +34,44 @@ acceptance("Category Banners", {
           "You need to video yourself <div class='inner'>doing</div> the secret handshake to post here",
       },
     ],
-  },
+  });
+
+  test("Does not display category banners when not set", async (assert) => {
+    await visit("/c/test-read-only-without-banner");
+
+    await click("#create-topic");
+    assert.ok(!visible(".bootbox.modal"), "it does not pop up a modal");
+    assert.ok(
+      !visible(".category-read-only-banner"),
+      "it does not show a banner"
+    );
+  });
+
+  test("Displays category banners when set", async (assert) => {
+    await visit("/c/test-read-only-with-banner");
+
+    await click("#create-topic");
+    assert.ok(visible(".bootbox.modal"), "it pops up a modal");
+
+    await click(".modal-footer>.btn-primary");
+    assert.ok(!visible(".bootbox.modal"), "it closes the modal");
+    assert.ok(visible(".category-read-only-banner"), "it shows a banner");
+    assert.ok(
+      find(".category-read-only-banner .inner").length === 1,
+      "it allows staff to embed html in the message"
+    );
+  });
 });
 
-test("Does not display category banners when not set", async (assert) => {
-  await visit("/c/test-read-only-without-banner");
-
-  await click("#create-topic");
-  assert.ok(!visible(".bootbox.modal"), "it does not pop up a modal");
-  assert.ok(
-    !visible(".category-read-only-banner"),
-    "it does not show a banner"
-  );
-});
-
-test("Displays category banners when set", async (assert) => {
-  await visit("/c/test-read-only-with-banner");
-
-  await click("#create-topic");
-  assert.ok(visible(".bootbox.modal"), "it pops up a modal");
-
-  await click(".modal-footer>.btn-primary");
-  assert.ok(!visible(".bootbox.modal"), "it closes the modal");
-  assert.ok(visible(".category-read-only-banner"), "it shows a banner");
-  assert.ok(
-    find(".category-read-only-banner .inner").length === 1,
-    "it allows staff to embed html in the message"
-  );
-});
-
-acceptance("Anonymous Category Banners", {
-  pretend(server, helper) {
+acceptance("Anonymous Category Banners", function (needs) {
+  needs.pretender((server, helper) => {
     server.get("/c/test-read-only-with-banner/6/l/latest.json", () => {
       return helper.response(
         DiscoveryFixtures["/latest_can_create_topic.json"]
       );
     });
-  },
-  loggedIn: false,
-  site: {
+  });
+  needs.site({
     categories: [
       {
         id: 6,
@@ -83,13 +82,13 @@ acceptance("Anonymous Category Banners", {
           "You need to video yourself doing the secret handshake to post here",
       },
     ],
-  },
-});
+  });
 
-test("Does not display category banners when set", async (assert) => {
-  await visit("/c/test-read-only-with-banner");
-  assert.ok(
-    !visible(".category-read-only-banner"),
-    "it does not show a banner"
-  );
+  test("Does not display category banners when set", async (assert) => {
+    await visit("/c/test-read-only-with-banner");
+    assert.ok(
+      !visible(".category-read-only-banner"),
+      "it does not show a banner"
+    );
+  });
 });
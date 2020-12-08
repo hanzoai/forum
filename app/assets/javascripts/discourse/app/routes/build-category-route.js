hanzoai/forum

commit 7fcb2940c52afe08513fb3fc92fec5b9405684da

FIX: Stop category 404s from breaking the router (#11424)
The propagated promise failure from model() caused the router to reject future route transitions, even though it correctly routed to the last-resort 404 page.

Co-authored-by: Jeff Wong <awole20@gmail.com>

app/assets/javascripts/discourse/app/routes/build-category-route.js
@@ -66,12 +66,17 @@ export default (filterArg, params) => {
           parts.pop();
         }
 
-        return Category.reloadBySlugPath(parts.join("/")).then((result) => {
-          const record = this.store.createRecord("category", result.category);
-          record.setupGroupsAndPermissions();
-          this.site.updateCategory(record);
-          return { category: record, modelParams };
-        });
+        return Category.reloadBySlugPath(parts.join("/"))
+          .then((result) => {
+            const record = this.store.createRecord("category", result.category);
+            record.setupGroupsAndPermissions();
+            this.site.updateCategory(record);
+            return { category: record, modelParams };
+          })
+          .catch(() => {
+            // afterModel will call replaceWith(/404)
+            return null;
+          });
       }
 
       if (category) {

app/assets/javascripts/discourse/tests/acceptance/unknown-test.js
@@ -1,6 +1,28 @@
 import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
-import { currentURL, visit } from "@ember/test-helpers";
+import { click, currentURL, visit } from "@ember/test-helpers";
 import { test } from "qunit";
+
+acceptance("Category 404", function (needs) {
+  needs.pretender((server, helper) => {
+    server.get("/c/category-does-not-exist/find_by_slug.json", () => {
+      return helper.response(404, {
+        errors: ["The requested URL or resource could not be found."],
+        error_type: "not_found",
+        extras: { html: "<div class='page-not-found'>not found</div>" },
+      });
+    });
+  });
+  test("Navigating to a bad category link does not break the router", async function (assert) {
+    await visit("/t/internationalization-localization/280");
+
+    await click('[data-for-test="category-404"]');
+    assert.equal(currentURL(), "/404");
+
+    // See that we can navigate away
+    await click("#site-logo");
+    assert.equal(currentURL(), "/");
+  });
+});
 
 acceptance("Unknown", function (needs) {
   const urls = {

app/assets/javascripts/discourse/tests/fixtures/topic.js
@@ -374,7 +374,7 @@ export default {
           uploaded_avatar_id: 40181,
           created_at: "2013-02-05T21:32:47.649Z",
           cooked:
-            '<p>The application strings <a href="https://github.com/discourse/discourse/blob/master/config/locales/en.yml" rel="nofollow">are externalized</a>, so localization should be entirely possible with enough translation effort.</p>',
+            '<p>The application strings <a href="https://github.com/discourse/discourse/blob/master/config/locales/en.yml" rel="nofollow">are externalized</a>, so localization should be entirely possible with enough translation effort.</p><p>Link for unknown-test: <a href="/c/category-does-not-exist/99999" data-for-test="category-404">link</a></p>',
           post_number: 2,
           post_type: 1,
           updated_at: "2013-02-06T10:15:27.965Z",

commit 115aca872358553e2be6c8732e7c3b2536a6e69f

FIX: Remove custom 'error' action on build-category-route/discovery (#11425)
The root cause of the issue was that the route was overriding the 'error' action from the correctly implemented handler in routes/application.js.

Remove the custom handler, and the duplicated template logic for displaying the errorHtml.

Fixes: 7fcb2940c52afe08513fb3fc92fec5b9405684da

app/assets/javascripts/discourse/app/routes/build-category-route.js
@@ -66,17 +66,12 @@ export default (filterArg, params) => {
           parts.pop();
         }
 
-        return Category.reloadBySlugPath(parts.join("/"))
-          .then((result) => {
-            const record = this.store.createRecord("category", result.category);
-            record.setupGroupsAndPermissions();
-            this.site.updateCategory(record);
-            return { category: record, modelParams };
-          })
-          .catch(() => {
-            // afterModel will call replaceWith(/404)
-            return null;
-          });
+        return Category.reloadBySlugPath(parts.join("/")).then((result) => {
+          const record = this.store.createRecord("category", result.category);
+          record.setupGroupsAndPermissions();
+          this.site.updateCategory(record);
+          return { category: record, modelParams };
+        });
       }
 
       if (category) {
@@ -244,18 +239,6 @@ export default (filterArg, params) => {
     },
 
     actions: {
-      error(err) {
-        const json = err.jqXHR.responseJSON;
-        if (json && json.extras && json.extras.html) {
-          this.controllerFor("discovery").set(
-            "errorHtml",
-            err.jqXHR.responseJSON.extras.html
-          );
-        } else {
-          this.replaceWith("exception");
-        }
-      },
-
       setNotification(notification_level) {
         this.currentModel.setNotification(notification_level);
       },

app/assets/javascripts/discourse/app/templates/discovery.hbs
@@ -1,41 +1,37 @@
-{{#if errorHtml}}
-  {{html-safe errorHtml}}
-{{else}}
+<div class="container">
+  {{discourse-banner user=currentUser banner=site.banner}}
+  {{#unless viewingCategoriesList}}
+    {{category-read-only-banner category=category readOnly=navigationCategory.cannotCreateTopicOnCategory}}
+  {{/unless}}
+</div>
+
+<div class="list-controls">
   <div class="container">
-    {{discourse-banner user=currentUser banner=site.banner}}
-    {{#unless viewingCategoriesList}}
-      {{category-read-only-banner category=category readOnly=navigationCategory.cannotCreateTopicOnCategory}}
-    {{/unless}}
+    {{outlet "navigation-bar"}}
   </div>
+</div>
 
-  <div class="list-controls">
-    <div class="container">
-      {{outlet "navigation-bar"}}
-    </div>
-  </div>
+{{conditional-loading-spinner condition=loading}}
 
-  {{conditional-loading-spinner condition=loading}}
+{{plugin-outlet name="discovery-above"}}
 
-  {{plugin-outlet name="discovery-above"}}
-
-  <div class="container list-container {{if loading "hidden"}}">
-    <div class="row">
-      <div class="full-width">
-        <div id="header-list-area">
-          {{outlet "header-list-container"}}
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="full-width">
-        <div id="list-area">
-          {{plugin-outlet name="discovery-list-container-top"
-                          args=(hash category=category listLoading=loading)}}
-          {{outlet "list-container"}}
-        </div>
+<div class="container list-container {{if loading "hidden"}}">
+  <div class="row">
+    <div class="full-width">
+      <div id="header-list-area">
+        {{outlet "header-list-container"}}
       </div>
     </div>
   </div>
+  <div class="row">
+    <div class="full-width">
+      <div id="list-area">
+        {{plugin-outlet name="discovery-list-container-top"
+                        args=(hash category=category listLoading=loading)}}
+        {{outlet "list-container"}}
+      </div>
+    </div>
+  </div>
+</div>
 
-  {{plugin-outlet name="discovery-below"}}
-{{/if}}
+{{plugin-outlet name="discovery-below"}}
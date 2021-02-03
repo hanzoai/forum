hanzoai/forum

commit e661bb2898b0c87421b13542f28c9e871bd0d22a

DEV: {{user-selector}} replacement (#11726)
This PR is the first step towards replacing our `{{user-selector}}` and eventually deprecating and removing it from our codebase. Some of `{{user-selector}}` problems are:

1. It's called `{{user-selector}}`, but in reality in can also select groups and emails.
2. It's an Ember component, yet it doesn't have a handlebars template and uses jQuery to render itself and modify the DOM. An example of this problem is when you want to clear the selected users programmatically, see [this](https://github.com/discourse/discourse/blob/f20351b2d84cd313c6c8087b7e95f07325949c65/app/assets/javascripts/discourse/app/components/user-selector.js#L179-L185).
3. We now have select kit which does very similar things but a lot better.

This PR introduces `{{email-group-user-chooser}}` which is meant to replace `{{user-selector}}`. It extends select kit and has the same features that `{{user-selector}}` has. `{{user-selector}}` is still used in a few places in core, but they'll all be replaced with the new component in a separate commit. 

Once `{{user-selector}}` is not used anywhere in core, it'll be deprecated and then removed after the 2.7 release.

app/assets/javascripts/select-kit/addon/components/email-group-user-chooser.js
@@ -1,0 +1,46 @@
+import UserChooserComponent from "select-kit/components/user-chooser";
+
+export default UserChooserComponent.extend({
+  pluginApiIdentifiers: ["email-group-user-chooser"],
+  classNames: ["email-group-user-chooser"],
+  valueProperty: "id",
+  nameProperty: "name",
+
+  modifyComponentForRow() {
+    return "email-group-user-chooser-row";
+  },
+
+  selectKitOptions: {
+    headerComponent: "email-group-user-chooser-header",
+    autoWrap: false,
+  },
+
+  search() {
+    const superPromise = this._super(...arguments);
+    if (!superPromise) {
+      return;
+    }
+    return superPromise.then((results) => {
+      if (!results || results.length === 0) {
+        return;
+      }
+      return results.map((item) => {
+        const reconstructed = {};
+        if (item.username) {
+          reconstructed.id = item.username;
+          if (item.username.includes("@")) {
+            reconstructed.isEmail = true;
+          } else {
+            reconstructed.isUser = true;
+            reconstructed.name = item.name;
+          }
+        } else if (item.name) {
+          reconstructed.id = item.name;
+          reconstructed.name = item.full_name;
+          reconstructed.isGroup = true;
+        }
+        return Object.assign({}, item, reconstructed);
+      });
+    });
+  },
+});

app/assets/javascripts/admin/addon/controllers/admin-api-keys-new.js
@@ -3,6 +3,7 @@ import I18n from "I18n";
 import discourseComputed from "discourse-common/utils/decorators";
 import { isBlank } from "@ember/utils";
 import { popupAjaxError } from "discourse/lib/ajax-error";
+import { get } from "@ember/object";
 import showModal from "discourse/lib/show-modal";
 
 export default Controller.extend({
@@ -30,6 +31,10 @@ export default Controller.extend({
   },
 
   actions: {
+    updateUsername(selected) {
+      this.set("model.username", get(selected, "firstObject"));
+    },
+
     changeUserMode(value) {
       if (value === "all") {
         this.model.set("username", null);

app/assets/javascripts/admin/addon/controllers/admin-email-preview-digest.js
@@ -2,6 +2,7 @@ import { empty, notEmpty, or } from "@ember/object/computed";
 import Controller from "@ember/controller";
 import EmailPreview from "admin/models/email-preview";
 import bootbox from "bootbox";
+import { get } from "@ember/object";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 
 export default Controller.extend({
@@ -14,6 +15,10 @@ export default Controller.extend({
   htmlEmpty: empty("model.html_content"),
 
   actions: {
+    updateUsername(selected) {
+      this.set("username", get(selected, "firstObject"));
+    },
+
     refresh() {
       const model = this.model;
 

app/assets/javascripts/admin/addon/controllers/modals/admin-merge-users-prompt.js
@@ -1,6 +1,6 @@
 import Controller, { inject as controller } from "@ember/controller";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
-import { action } from "@ember/object";
+import { action, get } from "@ember/object";
 import { alias } from "@ember/object/computed";
 import discourseComputed from "discourse-common/utils/decorators";
 
@@ -26,5 +26,10 @@ export default Controller.extend(ModalFunctionality, {
   @action
   close() {
     this.send("closeModal");
+  },
+
+  @action
+  updateUsername(selected) {
+    this.set("targetUsername", get(selected, "firstObject"));
   },
 });

app/assets/javascripts/admin/addon/templates/api-keys-new.hbs
@@ -25,10 +25,14 @@
 
     {{#if showUserSelector}}
       {{#admin-form-row label="admin.api.user"}}
-        {{user-selector single="true"
-          usernames=model.username
-          placeholderKey="admin.api.user_placeholder"
-          }}
+        {{email-group-user-chooser
+          value=model.username
+          onChange=(action "updateUsername")
+          options=(hash
+            maximum=1
+            filterPlaceholder="admin.api.user_placeholder"
+          )
+        }}
       {{/admin-form-row}}
     {{/if}}
 

app/assets/javascripts/admin/addon/templates/email-preview-digest.hbs
@@ -5,7 +5,13 @@
     <label for="last-seen">{{i18n "admin.email.last_seen_user"}}</label>
     {{date-picker-past value=lastSeen id="last-seen"}}
     <label>{{i18n "admin.email.user"}}:</label>
-    {{user-selector single="true" usernames=username canReceiveUpdates=true}}
+    {{email-group-user-chooser
+      value=username
+      onChange=(action "updateUsername")
+      options=(hash
+        maximum=1
+      )
+    }}
     {{d-button
       class="btn-primary digest-refresh-button"
       action=(action "refresh")

app/assets/javascripts/admin/addon/templates/modal/admin-merge-users-prompt.hbs
@@ -1,10 +1,15 @@
 <div>
   {{#d-modal-body rawTitle=(i18n "admin.user.merge.prompt.title" username=username)}}
     <p>{{html-safe (i18n "admin.user.merge.prompt.description" username=username)}}</p>
-    {{user-selector single=true
-                    placeholderKey="admin.user.merge.prompt.target_username_placeholder"
-                    usernames=targetUsername
-                    autocomplete="discourse"}}
+    {{email-group-user-chooser
+      value=targetUsername
+      autocomplete="discourse"
+      onChange=(action "updateUsername")
+      options=(hash
+        maximum=1
+        filterPlaceholder="admin.user.merge.prompt.target_username_placeholder"
+      )
+    }}
   {{/d-modal-body}}
 
   <div class="modal-footer">

app/assets/javascripts/discourse/app/components/composer-user-selector.js
@@ -1,13 +1,12 @@
-import discourseComputed, { observes } from "discourse-common/utils/decorators";
 import Component from "@ember/component";
-import I18n from "I18n";
+import discourseComputed from "discourse-common/utils/decorators";
 import putCursorAtEnd from "discourse/lib/put-cursor-at-end";
-import { schedule } from "@ember/runloop";
 
 export default Component.extend({
-  showSelector: true,
-  shouldHide: false,
-  defaultUsernameCount: 0,
+  init() {
+    this._super(...arguments);
+    this.set("_groups", []);
+  },
 
   didInsertElement() {
     this._super(...arguments);
@@ -17,78 +16,34 @@ export default Component.extend({
     }
   },
 
-  @observes("usernames")
-  _checkWidth() {
-    let width = 0;
-    const $acWrap = $(this.element).find(".ac-wrap");
-    const limit = $acWrap.width();
-    this.set("defaultUsernameCount", 0);
-
-    $acWrap
-      .find(".item")
-      .toArray()
-      .forEach((item) => {
-        width += $(item).outerWidth(true);
-        const result = width < limit;
-
-        if (result) {
-          this.incrementProperty("defaultUsernameCount");
-        }
-        return result;
-      });
-
-    if (width >= limit) {
-      this.set("shouldHide", true);
-    } else {
-      this.set("shouldHide", false);
-    }
+  @discourseComputed("recipients")
+  splitRecipients(recipients) {
+    return recipients ? recipients.split(",").filter(Boolean) : [];
   },
 
-  @observes("shouldHide")
-  _setFocus() {
-    const selector =
-      "#reply-control #reply-title, #reply-control .d-editor-input";
-
-    if (this.shouldHide) {
-      $(selector).on("focus.composer-user-selector", () => {
-        this.set("showSelector", false);
-        this.appEvents.trigger("composer:resize");
-      });
-    } else {
-      $(selector).off("focus.composer-user-selector");
-    }
-  },
-
-  @discourseComputed("usernames")
-  splitUsernames(usernames) {
-    return usernames.split(",");
-  },
-
-  @discourseComputed("splitUsernames", "defaultUsernameCount")
-  limitedUsernames(splitUsernames, count) {
-    return splitUsernames.slice(0, count).join(", ");
-  },
-
-  @discourseComputed("splitUsernames", "defaultUsernameCount")
-  hiddenUsersCount(splitUsernames, count) {
-    return `${splitUsernames.length - count} ${I18n.t("more")}`;
+  _updateGroups(selected, newGroups) {
+    const groups = [];
+    this._groups.forEach((existing) => {
+      if (selected.includes(existing)) {
+        groups.addObject(existing);
+      }
+    });
+    newGroups.forEach((newGroup) => {
+      if (!groups.includes(newGroup)) {
+        groups.addObject(newGroup);
+      }
+    });
+    this.setProperties({
+      _groups: groups,
+      hasGroups: groups.length > 0,
+    });
   },
 
   actions: {
-    toggleSelector() {
-      this.set("showSelector", true);
-
-      schedule("afterRender", () => {
-        $(this.element).find("input").focus();
-      });
-    },
-
-    triggerResize() {
-      this.appEvents.trigger("composer:resize");
-      const $this = $(this.element).find(".ac-wrap");
-      if ($this.height() >= 150) {
-        $this.scrollTop($this.height());
-      }
+    updateRecipients(selected, content) {
+      const newGroups = content.filterBy("isGroup").mapBy("id");
+      this._updateGroups(selected, newGroups);
+      this.set("recipients", selected.join(","));
     },
   },
 });

commit 1938b4e8bfcb8f298b47bf87b507d30b46c8e4bd

FIX: Restore support for pasting multiple PM recipients (#11945)
This is a regression from https://github.com/discourse/discourse/commit/e661bb2898b0c87421b13542f28c9e871bd0d22a.

Meta topic: https://meta.discourse.org/t/-/178167?u=osama.

Signed-off-by: OsamaSayegh <asooomaasoooma90@gmail.com>

app/assets/javascripts/select-kit/addon/components/email-group-user-chooser-filter.js
@@ -1,0 +1,35 @@
+import MultiSelectFilterComponent from "select-kit/components/multi-select/multi-select-filter";
+
+export default MultiSelectFilterComponent.extend({
+  classNames: ["email-group-user-chooser-filter"],
+
+  actions: {
+    onPaste(event) {
+      const data = event.originalEvent.clipboardData;
+
+      if (!data) {
+        return;
+      }
+
+      const recipients = [];
+      data
+        .getData("text")
+        .split(/[, \n]+/)
+        .forEach((recipient) => {
+          recipient = recipient.replace(/^@+/, "").trim();
+          if (recipient.length > 0) {
+            recipients.push(recipient);
+          }
+        });
+
+      if (recipients.length > 0) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        this.selectKit.append(recipients);
+
+        return false;
+      }
+    },
+  },
+});

app/assets/javascripts/select-kit/addon/components/email-group-user-chooser.js
@@ -12,6 +12,7 @@ export default UserChooserComponent.extend({
 
   selectKitOptions: {
     headerComponent: "email-group-user-chooser-header",
+    filterComponent: "email-group-user-chooser-filter",
     autoWrap: false,
   },
 
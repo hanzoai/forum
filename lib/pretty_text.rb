hanzoai/forum

commit 5b3b8cdf4ec1e56c8d1c58abcdac75f78b9b64e7

FEATURE: Add a site setting to allow emojis to come from an external URL (#12180)

lib/pretty_text.rb
@@ -238,6 +238,7 @@ module PrettyText
         __performEmojiUnescape(#{title.inspect}, {
           getURL: __getURL,
           emojiSet: #{set},
+          emojiCDNUrl: #{SiteSetting.external_emoji_url.blank? ? "''" : SiteSetting.external_emoji_url},
           customEmoji: #{custom},
           enableEmojiShortcuts: #{SiteSetting.enable_emoji_shortcuts},
           inlineEmoji: #{SiteSetting.enable_inline_emoji_translation}

app/assets/javascripts/discourse/app/controllers/create-account.js
@@ -23,6 +23,7 @@ import { notEmpty } from "@ember/object/computed";
 import { setting } from "discourse/lib/computed";
 import { userPath } from "discourse/lib/url";
 import { helperContext } from "discourse-common/lib/helpers";
+import { emojiBasePath } from "discourse/lib/settings";
 
 export default Controller.extend(
   ModalFunctionality,
@@ -84,7 +85,7 @@ export default Controller.extend(
 
       // random number between 2 -6 to render multiple skin tone waving hands
       const random = Math.floor(Math.random() * (7 - 2) + 2);
-      return getURL(`/images/emoji/${emojiSet}/wave/${random}.png`);
+      return getURL(`${emojiBasePath()}/${emojiSet}/wave/${random}.png`);
     },
 
     @discourseComputed(

app/assets/javascripts/discourse/app/controllers/login.js
@@ -19,6 +19,7 @@ import { isEmpty } from "@ember/utils";
 import { setting } from "discourse/lib/computed";
 import showModal from "discourse/lib/show-modal";
 import { helperContext } from "discourse-common/lib/helpers";
+import { emojiBasePath } from "discourse/lib/settings";
 
 // This is happening outside of the app via popup
 const AuthErrors = [
@@ -71,7 +72,7 @@ export default Controller.extend(ModalFunctionality, {
 
     // random number between 2 -6 to render multiple skin tone waving hands
     const random = Math.floor(Math.random() * (7 - 2) + 2);
-    return getURL(`/images/emoji/${emojiSet}/wave/${random}.png`);
+    return getURL(`${emojiBasePath()}/${emojiSet}/wave/${random}.png`);
   },
 
   @discourseComputed("showSecondFactor", "showSecurityKey")

app/assets/javascripts/discourse/app/lib/settings.js
@@ -7,3 +7,11 @@ export function prioritizeNameInUx(name) {
     !siteSettings.prioritize_username_in_ux && name && name.trim().length > 0
   );
 }
+
+export function emojiBasePath() {
+  let siteSettings = helperContext().siteSettings;
+
+  return siteSettings.external_emoji_url === ""
+    ? "/images/emojis"
+    : siteSettings.external_emoji_url;
+}

app/assets/javascripts/discourse/app/lib/text.js
@@ -90,6 +90,7 @@ function emojiOptions() {
     emojiSet: siteSettings.emoji_set,
     enableEmojiShortcuts: siteSettings.enable_emoji_shortcuts,
     inlineEmoji: siteSettings.enable_inline_emoji_translation,
+    emojiCDNUrl: siteSettings.external_emoji_url,
   };
 }
 

app/assets/javascripts/discourse/tests/helpers/site-settings.js
@@ -97,6 +97,7 @@ const ORIGINAL_SETTINGS = {
   enable_personal_messages: true,
   unicode_usernames: false,
   secure_media: false,
+  external_emoji_url: "",
 };
 
 let siteSettings = Object.assign({}, ORIGINAL_SETTINGS);

app/assets/javascripts/discourse/tests/unit/lib/pretty-text-test.js
@@ -17,6 +17,7 @@ const rawOpts = {
     enable_emoji_shortcuts: true,
     enable_mentions: true,
     emoji_set: "google_classic",
+    external_emoji_url: "",
     highlighted_languages: "json|ruby|javascript",
     default_code_lang: "auto",
     enable_markdown_linkify: true,
@@ -1516,6 +1517,19 @@ var bar = 'bar';
       ":smile:",
       { siteSettings: { emoji_set: "twitter" } },
       `<p><img src="/images/emoji/twitter/smile.png?v=${v}" title=":smile:" class="emoji only-emoji" alt=":smile:"></p>`
+    );
+  });
+
+  test("emoji - emojiCDN", function (assert) {
+    assert.cookedOptions(
+      ":smile:",
+      {
+        siteSettings: {
+          emoji_set: "twitter",
+          external_emoji_url: "https://emoji.hosting.service",
+        },
+      },
+      `<p><img src="https://emoji.hosting.service/twitter/smile.png?v=${v}" title=":smile:" class="emoji only-emoji" alt=":smile:"></p>`
     );
   });
 

app/assets/javascripts/pretty-text/addon/emoji.js
@@ -180,6 +180,12 @@ export function buildEmojiUrl(code, opts) {
   }
 
   const noToneMatch = code.match(/([^:]+):?/);
+
+  let emojiBasePath = "/images/emoji";
+  if (opts.emojiCDNUrl) {
+    emojiBasePath = opts.emojiCDNUrl;
+  }
+
   if (
     noToneMatch &&
     !url &&
@@ -187,7 +193,7 @@ export function buildEmojiUrl(code, opts) {
       aliasHash.hasOwnProperty(noToneMatch[1]))
   ) {
     url = opts.getURL(
-      `/images/emoji/${opts.emojiSet}/${code.replace(/:t/, "/")}.png`
+      `${emojiBasePath}/${opts.emojiSet}/${code.replace(/:t/, "/")}.png`
     );
   }
 

commit 98eede603f31ec7ea65fedb0c6aec22f9983ae7d

FIX: Missing quote in emoji cdn setting caused by 5b3b8cd (#12280)

lib/pretty_text.rb
@@ -238,7 +238,7 @@ module PrettyText
         __performEmojiUnescape(#{title.inspect}, {
           getURL: __getURL,
           emojiSet: #{set},
-          emojiCDNUrl: #{SiteSetting.external_emoji_url.blank? ? "''" : SiteSetting.external_emoji_url},
+          emojiCDNUrl: "#{SiteSetting.external_emoji_url.blank? ? "" : SiteSetting.external_emoji_url}",
           customEmoji: #{custom},
           enableEmojiShortcuts: #{SiteSetting.enable_emoji_shortcuts},
           inlineEmoji: #{SiteSetting.enable_inline_emoji_translation}
hanzoai/forum

commit f0166d42a2085809de4b72685ee5e052d1390702

FEATURE: Display unread and new counts for messages.  (#14059)
There are certain design decisions that were made in this commit.

Private messages implements its own version of topic tracking state because there are significant differences between regular and private_message topics. Regular topics have to track categories and tags while private messages do not. It is much easier to design the new topic tracking state if we maintain two different classes, instead of trying to mash this two worlds together.

One MessageBus channel per user and one MessageBus channel per group. This allows each user and each group to have their own channel backlog instead of having one global channel which requires the client to filter away unrelated messages.

lib/post_jobs_enqueuer.rb
@@ -20,11 +20,6 @@ class PostJobsEnqueuer
       after_topic_create
       make_visible
     end
-
-    if @topic.private_message?
-      TopicTrackingState.publish_private_message(@topic, post: @post)
-      TopicGroup.new_message_update(@topic.last_poster, @topic.id, @post.post_number)
-    end
   end
 
   private
@@ -46,6 +41,7 @@ class PostJobsEnqueuer
   end
 
   def make_visible
+    return if @topic.private_message?
     return unless SiteSetting.embed_unlisted?
     return unless @post.post_number > 1
     return if @topic.visible?
@@ -73,12 +69,18 @@ class PostJobsEnqueuer
     @topic.posters = @topic.posters_summary
     @topic.posts_count = 1
 
-    TopicTrackingState.publish_new(@topic)
+    klass =
+      if @topic.private_message?
+        PrivateMessageTopicTrackingState
+      else
+        TopicTrackingState
+      end
+
+    klass.publish_new(@topic)
   end
 
   def skip_after_create?
     @opts[:import_mode] ||
-      @topic.private_message? ||
       @post.post_type == Post.types[:moderator_action] ||
       @post.post_type == Post.types[:small_action]
   end

app/assets/javascripts/discourse/app/controllers/user-private-messages.js
@@ -66,12 +66,28 @@ export default Controller.extend({
     return pmView === VIEW_NAME_WARNINGS && !viewingSelf && !isAdmin;
   },
 
-  @discourseComputed("model.groups")
-  inboxes(groups) {
-    const groupsWithMessages = groups?.filter((group) => {
-      return group.has_messages;
-    });
+  @discourseComputed("pmTopicTrackingState.newIncoming.[]", "selectedInbox")
+  newLinkText() {
+    return this._linkText("new");
+  },
 
+  @discourseComputed("selectedInbox", "pmTopicTrackingState.newIncoming.[]")
+  unreadLinkText() {
+    return this._linkText("unread");
+  },
+
+  _linkText(type) {
+    const count = this.pmTopicTrackingState?.lookupCount(type) || 0;
+
+    if (count === 0) {
+      return I18n.t(`user.messages.${type}`);
+    } else {
+      return I18n.t(`user.messages.${type}_with_count`, { count });
+    }
+  },
+
+  @discourseComputed("model.groupsWithMessages")
+  inboxes(groupsWithMessages) {
     if (!groupsWithMessages || groupsWithMessages.length === 0) {
       return [];
     }

app/assets/javascripts/discourse/app/controllers/user-topics-list.js
@@ -1,8 +1,6 @@
 import Controller, { inject as controller } from "@ember/controller";
-import discourseComputed, {
-  observes,
-  on,
-} from "discourse-common/utils/decorators";
+import discourseComputed, { observes } from "discourse-common/utils/decorators";
+import { reads } from "@ember/object/computed";
 import BulkTopicSelection from "discourse/mixins/bulk-topic-selection";
 import { action } from "@ember/object";
 import Topic from "discourse/models/topic";
@@ -18,14 +16,9 @@ export default Controller.extend(BulkTopicSelection, {
 
   hideCategory: false,
   showPosters: false,
-  incomingCount: 0,
   channel: null,
   tagsForUser: null,
-
-  @on("init")
-  _initialize() {
-    this.newIncoming = [];
-  },
+  pmTopicTrackingState: null,
 
   saveScrollPosition() {
     this.session.set("topicListScrollPosition", $(window).scrollTop());
@@ -36,10 +29,7 @@ export default Controller.extend(BulkTopicSelection, {
     this.set("application.showFooter", !this.get("model.canLoadMore"));
   },
 
-  @discourseComputed("incomingCount")
-  hasIncoming(incomingCount) {
-    return incomingCount > 0;
-  },
+  incomingCount: reads("pmTopicTrackingState.newIncoming.length"),
 
   @discourseComputed("filter", "model.topics.length")
   showResetNew(filter, hasTopics) {
@@ -51,31 +41,16 @@ export default Controller.extend(BulkTopicSelection, {
     return filter === UNREAD_FILTER && hasTopics;
   },
 
-  subscribe(channel) {
-    this.set("channel", channel);
-
-    this.messageBus.subscribe(channel, (data) => {
-      if (this.newIncoming.indexOf(data.topic_id) === -1) {
-        this.newIncoming.push(data.topic_id);
-        this.incrementProperty("incomingCount");
-      }
-    });
+  subscribe() {
+    this.pmTopicTrackingState?.trackIncoming(
+      this.inbox,
+      this.filter,
+      this.group
+    );
   },
 
   unsubscribe() {
-    const channel = this.channel;
-    if (channel) {
-      this.messageBus.unsubscribe(channel);
-    }
-    this._resetTracking();
-    this.set("channel", null);
-  },
-
-  _resetTracking() {
-    this.setProperties({
-      newIncoming: [],
-      incomingCount: 0,
-    });
+    this.pmTopicTrackingState?.resetTracking();
   },
 
   @action
@@ -105,8 +80,8 @@ export default Controller.extend(BulkTopicSelection, {
 
   @action
   showInserted() {
-    this.model.loadBefore(this.newIncoming);
-    this._resetTracking();
+    this.model.loadBefore(this.pmTopicTrackingState.newIncoming);
+    this.pmTopicTrackingState.resetTracking();
     return false;
   },
 });

app/assets/javascripts/discourse/app/models/private-message-topic-tracking-state.js
@@ -1,0 +1,196 @@
+import EmberObject from "@ember/object";
+import {
+  ARCHIVE_FILTER,
+  INBOX_FILTER,
+  NEW_FILTER,
+  UNREAD_FILTER,
+} from "discourse/routes/build-private-messages-route";
+import { NotificationLevels } from "discourse/lib/notification-levels";
+
+// See private_message_topic_tracking_state.rb for documentation
+const PrivateMessageTopicTrackingState = EmberObject.extend({
+  CHANNEL_PREFIX: "/private-message-topic-tracking-state",
+
+  inbox: null,
+  filter: null,
+  activeGroup: null,
+
+  startTracking(data) {
+    this.states = new Map();
+    this.newIncoming = [];
+    this._loadStates(data);
+    this.establishChannels();
+  },
+
+  establishChannels() {
+    this.messageBus.subscribe(
+      this._userChannel(this.user.id),
+      this._processMessage.bind(this)
+    );
+
+    this.user.groupsWithMessages?.forEach((group) => {
+      this.messageBus.subscribe(
+        this._groupChannel(group.id),
+        this._processMessage.bind(this)
+      );
+    });
+  },
+
+  stopTracking() {
+    this.messageBus.unsubscribe(this._userChannel(this.user.id));
+
+    this.user.groupsWithMessages?.forEach((group) => {
+      this.messageBus.unsubscribe(this._groupChannel(group.id));
+    });
+  },
+
+  lookupCount(type) {
+    const typeFilterFn = type === "new" ? this._isNew : this._isUnread;
+    let filterFn;
+
+    if (this.inbox === "user") {
+      filterFn = this._isPersonal.bind(this);
+    } else if (this.inbox === "group") {
+      filterFn = this._isGroup.bind(this);
+    }
+
+    return Array.from(this.states.values()).filter((topic) => {
+      return typeFilterFn(topic) && (!filterFn || filterFn(topic));
+    }).length;
+  },
+
+  trackIncoming(inbox, filter, group) {
+    this.setProperties({ inbox, filter, activeGroup: group });
+  },
+
+  resetTracking() {
+    if (this.inbox) {
+      this.set("newIncoming", []);
+    }
+  },
+
+  _userChannel(userId) {
+    return `${this.CHANNEL_PREFIX}/user/${userId}`;
+  },
+
+  _groupChannel(groupId) {
+    return `${this.CHANNEL_PREFIX}/group/${groupId}`;
+  },
+
+  _isNew(topic) {
+    return (
+      !topic.last_read_post_number &&
+      ((topic.notification_level !== 0 && !topic.notification_level) ||
+        topic.notification_level >= NotificationLevels.TRACKING) &&
+      !topic.is_seen
+    );
+  },
+
+  _isUnread(topic) {
+    return (
+      topic.last_read_post_number &&
+      topic.last_read_post_number < topic.highest_post_number &&
+      topic.notification_level >= NotificationLevels.TRACKING
+    );
+  },
+
+  _isPersonal(topic) {
+    const groups = this.user.groups;
+
+    if (groups.length === 0) {
+      return true;
+    }
+
+    return !groups.some((group) => {
+      return topic.group_ids?.includes(group.id);
+    });
+  },
+
+  _isGroup(topic) {
+    return this.user.groups.some((group) => {
+      return (
+        group.name === this.activeGroup.name &&
+        topic.group_ids?.includes(group.id)
+      );
+    });
+  },
+
+  _processMessage(message) {
+    switch (message.message_type) {
+      case "new_topic":
+        this._modifyState(message.topic_id, message.payload);
+
+        if (
+          [NEW_FILTER, INBOX_FILTER].includes(this.filter) &&
+          this._shouldDisplayMessageForInbox(message)
+        ) {
+          this._notifyIncoming(message.topic_id);
+        }
+
+        break;
+      case "unread":
+        this._modifyState(message.topic_id, message.payload);
+
+        if (
+          [UNREAD_FILTER, INBOX_FILTER].includes(this.filter) &&
+          this._shouldDisplayMessageForInbox(message)
+        ) {
+          this._notifyIncoming(message.topic_id);
+        }
+
+        break;
+      case "archive":
+        if (
+          [INBOX_FILTER, ARCHIVE_FILTER].includes(this.filter) &&
+          ["user", "all"].includes(this.inbox)
+        ) {
+          this._notifyIncoming(message.topic_id);
+        }
+        break;
+      case "group_archive":
+        if (
+          [INBOX_FILTER, ARCHIVE_FILTER].includes(this.filter) &&
+          (this.inbox === "all" || this._displayMessageForGroupInbox(message))
+        ) {
+          this._notifyIncoming(message.topic_id);
+        }
+    }
+  },
+
+  _displayMessageForGroupInbox(message) {
+    return (
+      this.inbox === "group" &&
+      message.payload.group_ids.includes(this.activeGroup.id)
+    );
+  },
+
+  _shouldDisplayMessageForInbox(message) {
+    return (
+      this.inbox === "all" ||
+      this._displayMessageForGroupInbox(message) ||
+      (this.inbox === "user" &&
+        (message.payload.group_ids.length === 0 ||
+          this.currentUser.groups.filter((group) => {
+            return message.payload.group_ids.includes(group.id);
+          }).length === 0))
+    );
+  },
+
+  _notifyIncoming(topicId) {
+    if (this.newIncoming.indexOf(topicId) === -1) {
+      this.newIncoming.pushObject(topicId);
+    }
+  },
+
+  _loadStates(data) {
+    (data || []).forEach((topic) => {
+      this._modifyState(topic.topic_id, topic);
+    });
+  },
+
+  _modifyState(topicId, data) {
+    this.states.set(topicId, data);
+  },
+});
+
+export default PrivateMessageTopicTrackingState;

app/assets/javascripts/discourse/app/models/user.js
@@ -1,7 +1,7 @@
 import EmberObject, { computed, get, getProperties } from "@ember/object";
 import cookie, { removeCookie } from "discourse/lib/cookie";
 import { defaultHomepage, escapeExpression } from "discourse/lib/utilities";
-import { equal, gt, or } from "@ember/object/computed";
+import { equal, filterBy, gt, or } from "@ember/object/computed";
 import getURL, { getURLWithCDN } from "discourse-common/lib/get-url";
 import { A } from "@ember/array";
 import Badge from "discourse/models/badge";
@@ -561,6 +561,8 @@ const User = RestModel.extend({
       return !group.automatic || group.name === "moderators";
     });
   },
+
+  groupsWithMessages: filterBy("groups", "has_messages", true),
 
   @discourseComputed("filteredGroups", "numGroupsToDisplay")
   displayGroups(filteredGroups, numGroupsToDisplay) {

app/assets/javascripts/discourse/app/routes/build-private-messages-group-route.js
@@ -57,16 +57,16 @@ export default (inboxType, filter) => {
 
     setupController() {
       this._super.apply(this, arguments);
-      this.controllerFor("user-private-messages").set("group", this.group);
-      this.controllerFor("user-topics-list").set("group", this.group);
 
-      if (filter) {
-        this.controllerFor("user-topics-list").subscribe(
-          `/private-messages/group/${this.get(
-            "groupName"
-          ).toLowerCase()}/${filter}`
-        );
-      }
+      const userTopicsListController = this.controllerFor("user-topics-list");
+      userTopicsListController.set("group", this.group);
+
+      userTopicsListController.set(
+        "pmTopicTrackingState.activeGroup",
+        this.group
+      );
+
+      this.controllerFor("user-private-messages").set("group", this.group);
     },
 
     dismissReadOptions() {

app/assets/javascripts/discourse/app/routes/build-private-messages-route.js
@@ -6,6 +6,8 @@ import { action } from "@ember/object";
 
 export const NEW_FILTER = "new";
 export const UNREAD_FILTER = "unread";
+export const INBOX_FILTER = "inbox";
+export const ARCHIVE_FILTER = "archive";
 
 // A helper to build a user topic list route
 export default (inboxType, path, filter) => {
@@ -42,13 +44,13 @@ export default (inboxType, path, filter) => {
     setupController() {
       this._super.apply(this, arguments);
 
-      if (filter) {
-        this.controllerFor("user-topics-list").subscribe(
-          `/private-messages/${filter}`
-        );
-      }
+      const userPrivateMessagesController = this.controllerFor(
+        "user-private-messages"
+      );
 
-      this.controllerFor("user-topics-list").setProperties({
+      const userTopicsListController = this.controllerFor("user-topics-list");
+
+      userTopicsListController.setProperties({
         hideCategory: true,
         showPosters: true,
         tagsForUser: this.modelFor("user").get("username_lower"),
@@ -57,9 +59,13 @@ export default (inboxType, path, filter) => {
         filter: filter,
         group: null,
         inbox: inboxType,
+        pmTopicTrackingState:
+          userPrivateMessagesController.pmTopicTrackingState,
       });
 
-      this.controllerFor("user-private-messages").setProperties({
+      userTopicsListController.subscribe();
+
+      userPrivateMessagesController.setProperties({
         archive: false,
         pmView: inboxType,
         group: null,

app/assets/javascripts/discourse/app/routes/user-private-messages-archive.js
@@ -1,7 +1,9 @@
-import createPMRoute from "discourse/routes/build-private-messages-route";
+import createPMRoute, {
+  ARCHIVE_FILTER,
+} from "discourse/routes/build-private-messages-route";
 
 export default createPMRoute(
-  "archive",
+  "all",
   "private-messages-all-archive",
-  "archive"
+  ARCHIVE_FILTER
 );

commit 2cf99547a9c40459d4920163bb6a5cd612500282

FIX: Do not send emails to mailing_list_mode subscribers for PMs (#14159)
This bug was introduced by f0166d42a2085809de4b72685ee5e052d1390702.

In PostJobsEnqueuer we previously did not fire the after_post_create
event and after_topic_create event for private message topics. This was
changed in the above commit in order to publish message bus messages
for topic tracking state updates. Unfortunately this caused the
NotifyMailingListSubscribers job to be enqueued for all posts including
private messages, and admins and the users involved in the PMs got
emailed the contents of the PMs if they had mailing list mode enabled.

Luckily the impact of this was mitigated by a Guardian#can_see? check
for each mailing list mode user in the NotifyMailingListSubscribers job.
We never want to notify mailing list mode subscribers for private messages
so an early return has been added there, plus the logic in PostJobsEnqueuer
has been fixed, and tests have been added to that class where there were
none before.

app/jobs/regular/notify_mailing_list_subscribers.rb
@@ -28,7 +28,8 @@ module Jobs
       post_id = args[:post_id]
       post = post_id ? Post.with_deleted.find_by(id: post_id) : nil
 
-      return if !post || post.trashed? || post.user_deleted? || !post.topic || post.raw.blank?
+      return if !post || post.trashed? || post.user_deleted? ||
+                  !post.topic || post.raw.blank? || post.topic.private_message?
 
       users =
           User.activated.not_silenced.not_suspended.real

lib/post_jobs_enqueuer.rb
@@ -55,10 +55,13 @@ class PostJobsEnqueuer
   def after_post_create
     Jobs.enqueue(:post_update_topic_tracking_state, post_id: @post.id)
 
-    Jobs.enqueue_in(SiteSetting.email_time_window_mins.minutes,
-      :notify_mailing_list_subscribers,
-      post_id: @post.id,
-    )
+    if !@topic.private_message?
+      Jobs.enqueue_in(
+        SiteSetting.email_time_window_mins.minutes,
+        :notify_mailing_list_subscribers,
+        post_id: @post.id,
+      )
+    end
   end
 
   def after_topic_create

spec/jobs/notify_mailing_list_subscribers_spec.rb
@@ -80,6 +80,15 @@ describe Jobs::NotifyMailingListSubscribers do
       include_examples "no emails"
     end
 
+    context "with a private message" do
+      before do
+        post.topic.update!(archetype: Archetype.private_message, category: nil)
+        TopicAllowedUser.create(topic: post.topic, user: mailing_list_user)
+        post.topic.reload
+      end
+      include_examples "no emails"
+    end
+
     context "with a valid post from another user" do
 
       context "to an inactive user" do

spec/lib/post_jobs_enqueuer_spec.rb
@@ -1,0 +1,105 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PostJobsEnqueuer do
+  let!(:post) { Fabricate(:post, topic: topic) }
+  let!(:topic) { Fabricate(:topic) }
+  let(:new_topic) { false }
+  let(:opts) { { post_alert_options: {} } }
+
+  subject { described_class.new(post, topic, new_topic, opts) }
+
+  context "for regular topics" do
+    it "enqueues the :post_alert job" do
+      expect_enqueued_with(job: :post_alert, args: {
+        post_id: post.id,
+        new_record: true,
+        options: opts[:post_alert_options]
+      }) do
+        subject.enqueue_jobs
+      end
+    end
+
+    it "enqueues the :notify_mailing_list_subscribers job" do
+      expect_enqueued_with(job: :notify_mailing_list_subscribers, args: { post_id: post.id }) do
+        subject.enqueue_jobs
+      end
+    end
+
+    it "enqueues the :post_update_topic_tracking_state job" do
+      expect_enqueued_with(job: :post_update_topic_tracking_state, args: { post_id: post.id }) do
+        subject.enqueue_jobs
+      end
+    end
+
+    it "enqueues the :feature_topic_users job" do
+      expect_enqueued_with(job: :feature_topic_users, args: { topic_id: topic.id }) do
+        subject.enqueue_jobs
+      end
+    end
+
+    context "for new topics" do
+      let(:new_topic) { true }
+
+      it "calls the correct topic tracking state class to publish_new" do
+        TopicTrackingState.expects(:publish_new).with(topic)
+        PrivateMessageTopicTrackingState.expects(:publish_new).never
+        subject.enqueue_jobs
+      end
+    end
+  end
+
+  context "for private messages" do
+    let!(:topic) { Fabricate(:private_message_topic) }
+
+    it "does not enqueue the :notify_mailing_list_subscribers job" do
+      expect_not_enqueued_with(job: :notify_mailing_list_subscribers, args: { post_id: post.id }) do
+        subject.enqueue_jobs
+      end
+    end
+
+    it "enqueues the :post_update_topic_tracking_state job" do
+      expect_enqueued_with(job: :post_update_topic_tracking_state, args: { post_id: post.id }) do
+        subject.enqueue_jobs
+      end
+    end
+
+    it "enqueues the :feature_topic_users job" do
+      expect_enqueued_with(job: :feature_topic_users, args: { topic_id: topic.id }) do
+        subject.enqueue_jobs
+      end
+    end
+
+    context "for new topics" do
+      let(:new_topic) { true }
+
+      it "calls the correct topic tracking state class to publish_new" do
+        TopicTrackingState.expects(:publish_new).never
+        PrivateMessageTopicTrackingState.expects(:publish_new).with(topic)
+        subject.enqueue_jobs
+      end
+    end
+
+    context "for a post > post_number 1" do
+      let!(:post) do
+        Fabricate(:post, topic: topic)
+        Fabricate(:post, topic: topic)
+      end
+
+      context "when there is a topic embed" do
+        before do
+          SiteSetting.embed_unlisted = true
+          topic.update(visible: false)
+          Fabricate(:topic_embed, post: post, embed_url: "http://test.com")
+        end
+
+        it "does not enqueue the :make_embedded_topic_visible job" do
+          expect_not_enqueued_with(job: :make_embedded_topic_visible, args: { topic_id: topic.id }) do
+            subject.enqueue_jobs
+          end
+        end
+      end
+    end
+  end
+end
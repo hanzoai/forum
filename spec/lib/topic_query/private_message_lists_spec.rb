hanzoai/forum

commit 44a1aad9426245f9f839a8b106a8e449a34ac43f

FEATURE: New and Unread messages for user personal messages. (#13603)
* FEATURE: New and Unread messages for user personal messages.

Co-authored-by: awesomerobot <kris.aubuchon@discourse.org>

lib/topic_query/private_message_lists.rb
@@ -1,0 +1,256 @@
+# frozen_string_literal: true
+
+class TopicQuery
+  module PrivateMessageLists
+    def list_private_messages_all(user)
+      list = private_messages_for(user, :all)
+      list = filter_archived(list, user, archived: false)
+      create_list(:private_messages, {}, list)
+    end
+
+    def list_private_messages_all_sent(user)
+      list = private_messages_for(user, :all)
+
+      list = list.where(<<~SQL, user.id)
+      EXISTS (
+        SELECT 1 FROM posts
+        WHERE posts.topic_id = topics.id AND posts.user_id = ?
+      )
+      SQL
+
+      list = filter_archived(list, user, archived: false)
+      create_list(:private_messages, {}, list)
+    end
+
+    def list_private_messages_all_archive(user)
+      list = private_messages_for(user, :all)
+      list = filter_archived(list, user, archived: true)
+      create_list(:private_messages, {}, list)
+    end
+
+    def list_private_messages_all_new(user)
+      list_private_messages_new(user, :all)
+    end
+
+    def list_private_messages_all_unread(user)
+      list_private_messages_unread(user, :all)
+    end
+
+    def list_private_messages(user)
+      list = private_messages_for(user, :user)
+      list = not_archived(list, user)
+      create_list(:private_messages, {}, list)
+    end
+
+    def list_private_messages_archive(user)
+      list = private_messages_for(user, :user)
+      list = list.joins(:user_archived_messages).where('user_archived_messages.user_id = ?', user.id)
+      create_list(:private_messages, {}, list)
+    end
+
+    def list_private_messages_sent(user)
+      list = private_messages_for(user, :user)
+
+      list = list.where(<<~SQL, user.id)
+      EXISTS (
+        SELECT 1 FROM posts
+        WHERE posts.topic_id = topics.id AND posts.user_id = ?
+      )
+      SQL
+
+      list = not_archived(list, user)
+      create_list(:private_messages, {}, list)
+    end
+
+    def list_private_messages_new(user, type = :user)
+      list = TopicQuery.new_filter(
+        private_messages_for(user, type),
+        treat_as_new_topic_start_date: user.user_option.treat_as_new_topic_start_date
+      )
+
+      list = remove_muted_tags(list, user)
+
+      create_list(:private_messages, {}, list)
+    end
+
+    def list_private_messages_unread(user, type = :user)
+      list = TopicQuery.unread_filter(
+        private_messages_for(user, type),
+        staff: user.staff?
+      )
+
+      first_unread_pm_at = UserStat
+        .where(user_id: user.id)
+        .pluck_first(:first_unread_pm_at)
+
+      if first_unread_pm_at
+        list = list.where("topics.updated_at >= ?", first_unread_pm_at)
+      end
+
+      create_list(:private_messages, {}, list)
+    end
+
+    def list_private_messages_group(user)
+      list = private_messages_for(user, :group)
+
+      list = list.joins(<<~SQL)
+      LEFT JOIN group_archived_messages gm
+      ON gm.topic_id = topics.id AND gm.group_id = #{group.id.to_i}
+      SQL
+
+      list = list.where("gm.id IS NULL")
+      publish_read_state = !!group.publish_read_state
+      list = append_read_state(list, group) if publish_read_state
+      create_list(:private_messages, { publish_read_state: publish_read_state }, list)
+    end
+
+    def list_private_messages_group_archive(user)
+      list = private_messages_for(user, :group)
+
+      list = list.joins(<<~SQL)
+      INNER JOIN group_archived_messages gm
+      ON gm.topic_id = topics.id AND gm.group_id = #{group.id.to_i}
+      SQL
+
+      publish_read_state = !!group.publish_read_state
+      list = append_read_state(list, group) if publish_read_state
+      create_list(:private_messages, { publish_read_state: publish_read_state }, list)
+    end
+
+    def list_private_messages_group_new(user)
+      list = TopicQuery.new_filter(
+        private_messages_for(user, :group),
+        treat_as_new_topic_start_date: user.user_option.treat_as_new_topic_start_date
+      )
+
+      publish_read_state = !!group.publish_read_state
+      list = append_read_state(list, group) if publish_read_state
+      create_list(:private_messages, { publish_read_state: publish_read_state }, list)
+    end
+
+    def list_private_messages_group_unread(user)
+      list = TopicQuery.unread_filter(
+        private_messages_for(user, :group),
+        staff: user.staff?
+      )
+
+      first_unread_pm_at = UserStat
+        .where(user_id: user.id)
+        .pluck_first(:first_unread_pm_at)
+
+      if first_unread_pm_at
+        list = list.where("topics.updated_at >= ?", first_unread_pm_at)
+      end
+
+      publish_read_state = !!group.publish_read_state
+      list = append_read_state(list, group) if publish_read_state
+      create_list(:private_messages, { publish_read_state: publish_read_state }, list)
+    end
+
+    def list_private_messages_warnings(user)
+      list = private_messages_for(user, :user)
+      list = list.where('topics.subtype = ?', TopicSubtype.moderator_warning)
+      # Exclude official warnings that the user created, instead of received
+      list = list.where('topics.user_id <> ?', user.id)
+      create_list(:private_messages, {}, list)
+    end
+
+    def private_messages_for(user, type)
+      options = @options
+      options.reverse_merge!(per_page: per_page_setting)
+
+      result = Topic.includes(:allowed_users)
+      result = result.includes(:tags) if SiteSetting.tagging_enabled
+
+      if type == :group
+        result = result.joins(
+          "INNER JOIN topic_allowed_groups tag ON tag.topic_id = topics.id AND tag.group_id IN (SELECT id FROM groups WHERE LOWER(name) = '#{PG::Connection.escape_string(@options[:group_name].downcase)}')"
+        )
+
+        unless user.admin?
+          result = result.joins("INNER JOIN group_users gu ON gu.group_id = tag.group_id AND gu.user_id = #{user.id.to_i}")
+        end
+      elsif type == :user
+        result = result.where("topics.id IN (SELECT topic_id FROM topic_allowed_users WHERE user_id = #{user.id.to_i})")
+      elsif type == :all
+        result = result.where("topics.id IN (
+              SELECT topic_id
+              FROM topic_allowed_users
+              WHERE user_id = #{user.id.to_i}
+              UNION ALL
+              SELECT topic_id FROM topic_allowed_groups
+              WHERE group_id IN (
+                SELECT group_id FROM group_users WHERE user_id = #{user.id.to_i}
+              )
+      )")
+      end
+
+      result = result.joins("LEFT OUTER JOIN topic_users AS tu ON (topics.id = tu.topic_id AND tu.user_id = #{user.id.to_i})")
+        .order("topics.bumped_at DESC")
+        .private_messages
+
+      result = result.limit(options[:per_page]) unless options[:limit] == false
+      result = result.visible if options[:visible] || @user.nil? || @user.regular?
+
+      if options[:page]
+        offset = options[:page].to_i * options[:per_page]
+        result = result.offset(offset) if offset > 0
+      end
+      result
+    end
+
+    def list_private_messages_tag(user)
+      list = private_messages_for(user, :all)
+      list = list.joins("JOIN topic_tags tt ON tt.topic_id = topics.id
+                        JOIN tags t ON t.id = tt.tag_id AND t.name = '#{@options[:tags][0]}'")
+      create_list(:private_messages, {}, list)
+    end
+
+    private
+
+    def append_read_state(list, group)
+      group_id = group.id
+      return list if group_id.nil?
+
+      selected_values = list.select_values.empty? ? ['topics.*'] : list.select_values
+      selected_values << "COALESCE(tg.last_read_post_number, 0) AS last_read_post_number"
+
+      list
+        .joins("LEFT OUTER JOIN topic_groups tg ON topics.id = tg.topic_id AND tg.group_id = #{group_id}")
+        .select(*selected_values)
+    end
+
+    def filter_archived(list, user, archived: true)
+      list = list.joins(<<~SQL)
+      LEFT JOIN group_archived_messages gm ON gm.topic_id = topics.id
+      LEFT JOIN user_archived_messages um
+        ON um.user_id = #{user.id.to_i}
+        AND um.topic_id = topics.id
+      SQL
+
+      list =
+        if archived
+          list.where("um.user_id IS NOT NULL OR gm.topic_id IS NOT NULL")
+        else
+          list.where("um.user_id IS NULL AND gm.topic_id IS NULL")
+        end
+
+      list
+    end
+
+    def not_archived(list, user)
+      list.joins("LEFT JOIN user_archived_messages um
+                         ON um.user_id = #{user.id.to_i} AND um.topic_id = topics.id")
+        .where('um.user_id IS NULL')
+    end
+
+    def group
+      @group ||= begin
+        Group
+          .where('name ilike ?', @options[:group_name])
+          .select(:id, :publish_read_state)
+          .first
+      end
+    end
+  end
+end

spec/lib/topic_query/private_message_lists_spec.rb
@@ -1,0 +1,305 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe TopicQuery::PrivateMessageLists do
+  fab!(:user) { Fabricate(:user) }
+  fab!(:user_2) { Fabricate(:user) }
+
+  fab!(:group) do
+    Fabricate(:group, messageable_level: Group::ALIAS_LEVELS[:everyone]).tap do |g|
+      g.add(user_2)
+    end
+  end
+
+  fab!(:group_message) do
+    create_post(
+      user: user,
+      target_group_names: [group.name],
+      archetype: Archetype.private_message
+    ).topic
+  end
+
+  fab!(:private_message) do
+    create_post(
+      user: user,
+      target_usernames: [user_2.username],
+      archetype: Archetype.private_message
+    ).topic
+  end
+
+  describe '#list_private_messages_all' do
+    it 'returns a list of all private messages that a user has access to' do
+      topics = TopicQuery.new(nil).list_private_messages_all(user).topics
+
+      expect(topics).to contain_exactly(group_message, private_message)
+    end
+
+    it 'does not include user or group archived messages' do
+      UserArchivedMessage.archive!(user.id, group_message)
+      UserArchivedMessage.archive!(user.id, private_message)
+
+      topics = TopicQuery.new(nil).list_private_messages_all(user).topics
+
+      expect(topics).to eq([])
+
+      GroupArchivedMessage.archive!(user_2.id, group_message)
+
+      topics = TopicQuery.new(nil).list_private_messages_all(user_2).topics
+
+      expect(topics).to contain_exactly(private_message)
+    end
+  end
+
+  describe '#list_private_messages_all_sent' do
+    it 'returns a list of all private messages that a user has sent' do
+      topics = TopicQuery.new(nil).list_private_messages_all_sent(user_2).topics
+
+      expect(topics).to eq([])
+
+      create_post(user: user_2, topic: private_message)
+
+      topics = TopicQuery.new(nil).list_private_messages_all_sent(user_2).topics
+
+      expect(topics).to contain_exactly(private_message)
+
+      create_post(user: user_2, topic: group_message)
+
+      topics = TopicQuery.new(nil).list_private_messages_all_sent(user_2).topics
+
+      expect(topics).to contain_exactly(private_message, group_message)
+    end
+
+    it 'does not include user or group archived messages' do
+      create_post(user: user_2, topic: private_message)
+      create_post(user: user_2, topic: group_message)
+
+      UserArchivedMessage.archive!(user_2.id, private_message)
+      GroupArchivedMessage.archive!(user_2.id, group_message)
+
+      topics = TopicQuery.new(nil).list_private_messages_all_sent(user_2).topics
+
+      expect(topics).to eq([])
+    end
+  end
+
+  describe '#list_private_messages_all_archive' do
+    it 'returns a list of all private messages that has been archived' do
+      UserArchivedMessage.archive!(user_2.id, private_message)
+      GroupArchivedMessage.archive!(user_2.id, group_message)
+
+      topics = TopicQuery.new(nil).list_private_messages_all_archive(user_2).topics
+
+      expect(topics).to contain_exactly(private_message, group_message)
+    end
+  end
+
+  describe '#list_private_messages_all_new' do
+    it 'returns a list of new private messages' do
+      topics = TopicQuery.new(nil).list_private_messages_all_new(user_2).topics
+
+      expect(topics).to contain_exactly(private_message, group_message)
+
+      TopicUser.find_by(user: user_2, topic: group_message).update!(
+        last_read_post_number: 1
+      )
+
+      topics = TopicQuery.new(nil).list_private_messages_all_new(user_2).topics
+
+      expect(topics).to contain_exactly(private_message)
+    end
+  end
+
+  describe '#list_private_messages_all_unread' do
+    it 'returns a list of unread private messages' do
+      topics = TopicQuery.new(nil).list_private_messages_all_unread(user_2).topics
+
+      expect(topics).to eq([])
+
+      TopicUser.find_by(user: user_2, topic: group_message).update!(
+        last_read_post_number: 1
+      )
+
+      create_post(user: user, topic: group_message)
+
+      topics = TopicQuery.new(nil).list_private_messages_all_unread(user_2).topics
+
+      expect(topics).to contain_exactly(group_message)
+    end
+  end
+
+  describe '#list_private_messages' do
+    it 'returns a list of all private messages that a user has access to' do
+      topics = TopicQuery.new(nil).list_private_messages(user_2).topics
+
+      expect(topics).to contain_exactly(private_message)
+    end
+  end
+
+  describe '#list_private_messages_group' do
+    it 'should return the right list for a group user' do
+      group.add(user_2)
+
+      topics = TopicQuery.new(nil, group_name: group.name)
+        .list_private_messages_group(user_2)
+        .topics
+
+      expect(topics).to contain_exactly(group_message)
+    end
+
+    it 'should return the right list for an admin not part of the group' do
+      group.update!(name: group.name.capitalize)
+
+      topics = TopicQuery.new(nil, group_name: group.name.upcase)
+        .list_private_messages_group(Fabricate(:admin))
+        .topics
+
+      expect(topics).to contain_exactly(group_message)
+    end
+
+    it "should not allow a moderator not part of the group to view the group's messages" do
+      topics = TopicQuery.new(nil, group_name: group.name)
+        .list_private_messages_group(Fabricate(:moderator))
+        .topics
+
+      expect(topics).to eq([])
+    end
+
+    it "should not allow a user not part of the group to view the group's messages" do
+      topics = TopicQuery.new(nil, group_name: group.name)
+        .list_private_messages_group(Fabricate(:user))
+        .topics
+
+      expect(topics).to eq([])
+    end
+
+    context "Calculating minimum unread count for a topic" do
+      before do
+        group.update!(publish_read_state: true)
+        group.add(user)
+      end
+
+      let(:listed_message) do
+        TopicQuery.new(nil, group_name: group.name)
+          .list_private_messages_group(user)
+          .topics.first
+      end
+
+      it 'returns the last read post number' do
+        topic_group = TopicGroup.create!(
+          topic: group_message, group: group, last_read_post_number: 10
+        )
+
+        expect(listed_message.last_read_post_number).to eq(topic_group.last_read_post_number)
+      end
+    end
+  end
+
+  describe '#list_private_messages_group_new' do
+    it 'returns a list of new private messages for a group that user is a part of' do
+      topics = TopicQuery.new(nil, group_name: group.name)
+        .list_private_messages_group_new(user_2)
+        .topics
+
+      expect(topics).to contain_exactly(group_message)
+    end
+  end
+
+  describe '#list_private_messages_group_unread' do
+    it 'returns a list of unread private messages for a group that user is a part of' do
+      topics = TopicQuery.new(nil, group_name: group.name)
+        .list_private_messages_group_unread(user_2)
+        .topics
+
+      expect(topics).to eq([])
+
+      TopicUser.find_by(user: user_2, topic: group_message).update!(
+        last_read_post_number: 1
+      )
+
+      create_post(user: user, topic: group_message)
+
+      topics = TopicQuery.new(nil, group_name: group.name)
+        .list_private_messages_group_unread(user_2)
+        .topics
+
+      expect(topics).to contain_exactly(group_message)
+    end
+  end
+
+  describe '#list_private_messages_unread' do
+    fab!(:user) { Fabricate(:user) }
+    fab!(:user_2) { Fabricate(:user) }
+
+    fab!(:pm) do
+      create_post(
+        user: user,
+        target_usernames: [user_2.username],
+        archetype: Archetype.private_message
+      ).topic
+    end
+
+    fab!(:pm_2) do
+      create_post(
+        user: user,
+        target_usernames: [user_2.username],
+        archetype: Archetype.private_message
+      ).topic
+    end
+
+    fab!(:pm_3) do
+      create_post(
+        user: user,
+        target_usernames: [user_2.username],
+        archetype: Archetype.private_message
+      ).topic
+    end
+
+    it 'returns a list of private messages with unread posts that user is at least tracking' do
+      freeze_time 1.minute.from_now do
+        create_post(user: user_2, topic_id: pm.id)
+        create_post(user: user_2, topic_id: pm_3.id)
+      end
+
+      TopicUser.find_by(user: user, topic: pm_3).update!(
+        notification_level: TopicUser.notification_levels[:regular]
+      )
+
+      expect(TopicQuery.new(user).list_private_messages_unread(user).topics)
+        .to contain_exactly(pm)
+    end
+  end
+
+  describe '#list_private_messages_new' do
+    fab!(:user) { Fabricate(:user) }
+    fab!(:user_2) { Fabricate(:user) }
+
+    fab!(:pm) do
+      create_post(
+        user: user,
+        target_usernames: [user_2.username],
+        archetype: Archetype.private_message
+      ).topic
+    end
+
+    it 'returns a list of new private messages' do
+      expect(TopicQuery.new(user_2).list_private_messages_new(user_2).topics)
+        .to contain_exactly(pm)
+    end
+
+    it 'returns a list of new private messages accounting for muted tags' do
+      tag = Fabricate(:tag)
+
+      pm.tags << tag
+
+      TagUser.create!(
+        tag: tag,
+        user: user_2,
+        notification_level: TopicUser.notification_levels[:muted]
+      )
+
+      expect(TopicQuery.new(user_2).list_private_messages_new(user_2).topics)
+        .to eq([])
+    end
+  end
+end

app/assets/javascripts/discourse/app/controllers/topic.js
@@ -154,10 +154,14 @@ export default Controller.extend(bufferedProperty("model"), {
   showCategoryChooser: not("model.isPrivateMessage"),
 
   gotoInbox(name) {
-    let url = userPath(this.get("currentUser.username_lower") + "/messages");
+    let url = userPath(`${this.get("currentUser.username_lower")}/messages`);
+
     if (name) {
-      url = url + "/group/" + name;
+      url = `${url}/group/${name}`;
+    } else {
+      url = `${url}/personal`;
     }
+
     DiscourseURL.routeTo(url);
   },
 

app/assets/javascripts/discourse/app/controllers/user-private-messages.js
@@ -3,6 +3,10 @@ import { action } from "@ember/object";
 import { alias, and, equal } from "@ember/object/computed";
 import discourseComputed from "discourse-common/utils/decorators";
 import { VIEW_NAME_WARNINGS } from "discourse/routes/user-private-messages-warnings";
+import I18n from "I18n";
+
+export const PERSONAL_INBOX = "__personal_inbox__";
+const ALL_INBOX = "__all_inbox__";
 
 export default Controller.extend({
   user: controller(),
@@ -10,19 +14,101 @@ export default Controller.extend({
   pmView: false,
   viewingSelf: alias("user.viewingSelf"),
   isGroup: equal("pmView", "groups"),
+  group: null,
+  groupFilter: alias("group.name"),
   currentPath: alias("router._router.currentPath"),
   pmTaggingEnabled: alias("site.can_tag_pms"),
   tagId: null,
 
   showNewPM: and("user.viewingSelf", "currentUser.can_send_private_messages"),
 
+  @discourseComputed("inboxes", "isAllInbox")
+  displayGlobalFilters(inboxes, isAllInbox) {
+    if (inboxes.length === 0) {
+      return true;
+    }
+    if (inboxes.length && isAllInbox) {
+      return true;
+    }
+    return false;
+  },
+
+  @discourseComputed("inboxes")
+  sectionClass(inboxes) {
+    const defaultClass = "user-secondary-navigation user-messages";
+
+    return inboxes.length
+      ? `${defaultClass} user-messages-inboxes`
+      : defaultClass;
+  },
+
+  @discourseComputed("pmView")
+  isPersonalInbox(pmView) {
+    return pmView && pmView.startsWith("personal");
+  },
+
+  @discourseComputed("isPersonalInbox", "group.name")
+  isAllInbox(isPersonalInbox, groupName) {
+    return !this.isPersonalInbox && !groupName;
+  },
+
+  @discourseComputed("isPersonalInbox", "group.name")
+  selectedInbox(isPersonalInbox, groupName) {
+    if (groupName) {
+      return groupName;
+    }
+
+    return isPersonalInbox ? PERSONAL_INBOX : ALL_INBOX;
+  },
+
   @discourseComputed("viewingSelf", "pmView", "currentUser.admin")
   showWarningsWarning(viewingSelf, pmView, isAdmin) {
     return pmView === VIEW_NAME_WARNINGS && !viewingSelf && !isAdmin;
   },
 
+  @discourseComputed("model.groups")
+  inboxes(groups) {
+    const groupsWithMessages = groups?.filter((group) => {
+      return group.has_messages;
+    });
+
+    if (!groupsWithMessages || groupsWithMessages.length === 0) {
+      return [];
+    }
+
+    const inboxes = [];
+
+    inboxes.push({
+      id: ALL_INBOX,
+      name: I18n.t("user.messages.all"),
+    });
+
+    inboxes.push({
+      id: PERSONAL_INBOX,
+      name: I18n.t("user.messages.personal"),
+      icon: "envelope",
+    });
+
+    groupsWithMessages.forEach((group) => {
+      inboxes.push({ id: group.name, name: group.name, icon: "users" });
+    });
+
+    return inboxes;
+  },
+
   @action
   changeGroupNotificationLevel(notificationLevel) {
     this.group.setNotification(notificationLevel, this.get("user.model.id"));
+  },
+
+  @action
+  updateInbox(inbox) {
+    if (inbox === ALL_INBOX) {
+      this.transitionToRoute("userPrivateMessages.index");
+    } else if (inbox === PERSONAL_INBOX) {
+      this.transitionToRoute("userPrivateMessages.personal");
+    } else {
+      this.transitionToRoute("userPrivateMessages.group", inbox);
+    }
   },
 });

app/assets/javascripts/discourse/app/lib/cached-topic-list.js
@@ -1,5 +1,6 @@
 export function findOrResetCachedTopicList(session, filter) {
   const lastTopicList = session.get("topicList");
+
   if (lastTopicList && lastTopicList.filter === filter) {
     return lastTopicList;
   } else {

app/assets/javascripts/discourse/app/routes/app-route-map.js
@@ -140,11 +140,20 @@ export default function () {
         "userPrivateMessages",
         { path: "/messages", resetNamespace: true },
         function () {
-          this.route("sent");
+          this.route("new");
+          this.route("unread");
           this.route("archive");
+          this.route("sent");
+          this.route("personal");
+          this.route("personalSent", { path: "personal/sent" });
+          this.route("personalNew", { path: "personal/new" });
+          this.route("personalUnread", { path: "personal/unread" });
+          this.route("personalArchive", { path: "personal/archive" });
           this.route("warnings");
           this.route("group", { path: "group/:name" });
           this.route("groupArchive", { path: "group/:name/archive" });
+          this.route("groupNew", { path: "group/:name/new" });
+          this.route("groupUnread", { path: "group/:name/unread" });
           this.route("tags");
           this.route("tagsShow", { path: "tags/:id" });
         }

app/assets/javascripts/discourse/app/routes/build-private-messages-group-route.js
@@ -1,0 +1,68 @@
+import I18n from "I18n";
+import createPMRoute from "discourse/routes/build-private-messages-route";
+import { findOrResetCachedTopicList } from "discourse/lib/cached-topic-list";
+
+export default (viewName, channel) => {
+  return createPMRoute("groups", "private-messages-groups").extend({
+    groupName: null,
+
+    titleToken() {
+      const groupName = this.groupName;
+
+      if (groupName) {
+        let title = groupName.capitalize();
+
+        if (viewName !== "index") {
+          title = `${title} ${I18n.t("user.messages." + viewName)}`;
+        }
+
+        return [title, I18n.t(`user.private_messages`)];
+      }
+    },
+
+    model(params) {
+      const username = this.modelFor("user").get("username_lower");
+      let filter = `topics/private-messages-group/${username}/${params.name}`;
+
+      if (viewName !== "index") {
+        filter = `${filter}/${viewName}`;
+      }
+
+      const lastTopicList = findOrResetCachedTopicList(this.session, filter);
+
+      return lastTopicList
+        ? lastTopicList
+        : this.store.findFiltered("topicList", { filter });
+    },
+
+    afterModel(model) {
+      const filters = model.get("filter").split("/");
+      let groupName;
+
+      if (viewName !== "index") {
+        groupName = filters[filters.length - 2];
+      } else {
+        groupName = filters.pop();
+      }
+
+      const group = this.modelFor("user")
+        .get("groups")
+        .filterBy("name", groupName)[0];
+
+      this.setProperties({ groupName: groupName, group });
+    },
+
+    setupController() {
+      this._super.apply(this, arguments);
+      this.controllerFor("user-private-messages").set("group", this.group);
+
+      if (channel) {
+        this.controllerFor("user-topics-list").subscribe(
+          `/private-messages/group/${this.get(
+            "groupName"
+          ).toLowerCase()}/${channel}`
+        );
+      }
+    },
+  });
+};

app/assets/javascripts/discourse/app/routes/build-private-messages-route.js
@@ -23,7 +23,9 @@ export default (viewName, path, channel) => {
     model() {
       const filter =
         "topics/" + path + "/" + this.modelFor("user").get("username_lower");
+
       const lastTopicList = findOrResetCachedTopicList(this.session, filter);
+
       return lastTopicList
         ? lastTopicList
         : this.store.findFiltered("topicList", { filter });
@@ -49,6 +51,7 @@ export default (viewName, path, channel) => {
       this.controllerFor("user-private-messages").setProperties({
         archive: false,
         pmView: viewName,
+        group: null,
       });
 
       this.searchService.set("contextType", "private_messages");

commit ec8c4126ca3c0af341b4ff4dd184bad15e3cb9b7

FIX: Exclude PMs that user sent to themselves. (#14496)
Regression from 44a1aad9426245f9f839a8b106a8e449a34ac43f

Follow-up to 44a1aad9426245f9f839a8b106a8e449a34ac43f

lib/topic_query/private_message_lists.rb
@@ -5,6 +5,15 @@ class TopicQuery
     def list_private_messages(user)
       list = private_messages_for(user, :user)
       list = not_archived(list, user)
+
+      list = list.where(<<~SQL)
+        NOT (
+          topics.participant_count = 1
+          AND topics.user_id = #{user.id.to_i}
+          AND topics.moderator_posts_count = 0
+        )
+      SQL
+
       create_list(:private_messages, {}, list)
     end
 

spec/lib/topic_query/private_message_lists_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 describe TopicQuery::PrivateMessageLists do
+  fab!(:admin) { Fabricate(:admin) }
   fab!(:user) { Fabricate(:user) }
   fab!(:user_2) { Fabricate(:user) }
   fab!(:user_3) { Fabricate(:user) }
@@ -49,6 +50,17 @@ describe TopicQuery::PrivateMessageLists do
       topics = TopicQuery.new(nil).list_private_messages(user_2).topics
 
       expect(topics).to contain_exactly(private_message)
+    end
+
+    it "includes topics with moderator posts" do
+      pm = Fabricate(:private_message_post, user: user_4).topic
+
+      expect(TopicQuery.new(user_4).list_private_messages(user_4).topics).to be_empty
+
+      pm.add_moderator_post(admin, "Thank you for your flag")
+
+      expect(TopicQuery.new(user_4).list_private_messages(user_4).topics)
+        .to contain_exactly(pm)
     end
   end
 
hanzoai/forum

commit 7f0265521d90a5184cef3bad992ee083fcd9506c

DEV: Ensure Sidekiq job arguments have stringified keys
The latest version of Sidekiq introduced a warning when jobs are queued with arguments which 'do not stringify to JSON safely'. In the vast majority of cases, this is because a hash is passed with symbols as keys. When those args are passed to the job, the keys will be stringified.

Our job wrapper already takes care of this issue by calling '.with_indifferent_access' on the args before passing them to `#execute`, so we don't need to change anything about our use. All we need to do is satisfy Sidekiq's warning system by 'stringifying' all the keys before enqueuing the job.

app/jobs/base.rb
@@ -298,7 +298,7 @@ module Jobs
     if ::Jobs.run_later?
       hash = {
         'class' => klass,
-        'args' => [opts]
+        'args' => [opts.deep_stringify_keys]
       }
 
       if delay = opts.delete(:delay_for)

commit 226e2e2824a8dcbc135d5446beda761328551d23

DEV: Ensure `delay_for` and `queue` are not passed as job arguments (#15824)
This regressed in 7f0265521d because deep_stringify_keys makes a copy of the `opts` hash

app/jobs/base.rb
@@ -297,8 +297,7 @@ module Jobs
 
     if ::Jobs.run_later?
       hash = {
-        'class' => klass,
-        'args' => [opts.deep_stringify_keys]
+        'class' => klass
       }
 
       if delay = opts.delete(:delay_for)
@@ -310,6 +309,8 @@ module Jobs
       if queue = opts.delete(:queue)
         hash['queue'] = queue
       end
+
+      hash['args'] = [opts.deep_stringify_keys]
 
       DB.after_commit { klass.client_push(hash) }
     else
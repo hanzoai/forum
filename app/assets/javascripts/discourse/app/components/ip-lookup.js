hanzoai/forum

commit 89b89ed3bd3daf84bd1ac894d84020ccb18f741d

DEV: Generate template backing classes for app (#17300)
```sh
npx ember-holy-futuristic-template-namespacing-batman-codemod ensure-template-only-has-backing-class
```

We would like to colocate templates but, when no backing class exists, colocated templates extend `templateOnly` instead of `Ember.Component`. Generating the backing class helps avoid any behavioral changes.

We can also run the `tagless-ember-components-codemod` to put the tags into the templates at which point we could convert these back to template-only components.

app/assets/javascripts/discourse/app/components/ip-lookup.js
@@ -1,0 +1,3 @@
+import Component from "@ember/component";
+
+export default Component.extend({});

app/assets/javascripts/admin/addon/components/dashboard-new-feature-item.js
@@ -1,0 +1,3 @@
+import Component from "@ember/component";
+
+export default Component.extend({});

app/assets/javascripts/admin/addon/components/dashboard-problems.js
@@ -1,0 +1,3 @@
+import Component from "@ember/component";
+
+export default Component.extend({});

app/assets/javascripts/admin/addon/components/flag-user.js
@@ -1,0 +1,3 @@
+import Component from "@ember/component";
+
+export default Component.extend({});

app/assets/javascripts/admin/addon/components/resumable-upload.js
@@ -1,0 +1,3 @@
+import Component from "@ember/component";
+
+export default Component.extend({});

app/assets/javascripts/admin/addon/components/setting-validation-message.js
@@ -1,0 +1,3 @@
+import Component from "@ember/component";
+
+export default Component.extend({});

app/assets/javascripts/admin/addon/components/site-customization-change-details.js
@@ -1,0 +1,3 @@
+import Component from "@ember/component";
+
+export default Component.extend({});

app/assets/javascripts/admin/addon/components/site-customization-change-field.js
@@ -1,0 +1,3 @@
+import Component from "@ember/component";
+
+export default Component.extend({});

commit c39e934fa8f9a50efe14d085a359575795e4cddb

FIX: Correct error in ip-lookup component definition (#17339)
89b89ed introduced a duplicate definition of the component JS. This likely happened because we had the template defined under `javascripts/discourse`, and the component JS defined under `javascript/admin`. This commit removes the duplicate definition, and moves the template to the admin addon

app/assets/javascripts/discourse/app/components/ip-lookup.js
@@ -1,3 +1,0 @@
-import Component from "@ember/component";
-
-export default Component.extend({});
hanzoai/forum

commit 6765cf0fbc92ff24543e99e7c56f3f79091821e5

FEATURE: Create upload_references table (#16146)
This table holds associations between uploads and other models. This can be used to prevent removing uploads that are still in use.

* DEV: Create upload_references
* DEV: Use UploadReference instead of PostUpload
* DEV: Use UploadReference for SiteSetting
* DEV: Use UploadReference for Badge
* DEV: Use UploadReference for Category
* DEV: Use UploadReference for CustomEmoji
* DEV: Use UploadReference for Group
* DEV: Use UploadReference for ThemeField
* DEV: Use UploadReference for ThemeSetting
* DEV: Use UploadReference for User
* DEV: Use UploadReference for UserAvatar
* DEV: Use UploadReference for UserExport
* DEV: Use UploadReference for UserProfile
* DEV: Add method to extract uploads from raw text
* DEV: Use UploadReference for Draft
* DEV: Use UploadReference for ReviewableQueuedPost
* DEV: Use UploadReference for UserProfile's bio_raw
* DEV: Do not copy user uploads to upload references
* DEV: Copy post uploads again after deploy
* DEV: Use created_at and updated_at from uploads table
* FIX: Check if upload site setting is empty
* DEV: Copy user uploads to upload references
* DEV: Make upload extraction less strict

lib/shrink_uploaded_image.rb
@@ -19,7 +19,7 @@ class ShrinkUploadedImage
       return false
     end
 
-    posts = Post.unscoped.joins(:post_uploads).where(post_uploads: { upload_id: original_upload.id }).uniq.sort_by(&:created_at)
+    posts = Post.unscoped.joins(:upload_references).where(upload_references: { upload_id: original_upload.id }).uniq.sort_by(&:created_at)
 
     if posts.empty?
       log "Upload not used in any posts"
@@ -134,7 +134,10 @@ class ShrinkUploadedImage
 
     if existing_upload
       begin
-        PostUpload.where(upload_id: original_upload.id).update_all(upload_id: upload.id)
+        UploadReferences
+          .where(target_type: 'Post')
+          .where(upload_id: original_upload.id)
+          .update_all(upload_id: upload.id)
       rescue ActiveRecord::RecordNotUnique, PG::UniqueViolation
       end
     else

lib/tasks/uploads.rake
@@ -529,7 +529,7 @@ task "uploads:disable_secure_media" => :environment do
 
     SiteSetting.secure_media = false
 
-    secure_uploads = Upload.joins(:post_uploads).where(secure: true)
+    secure_uploads = Upload.joins(:upload_references).where(upload_references: { target_type: 'Post' }).where(secure: true)
     secure_upload_count = secure_uploads.count
     secure_upload_ids = secure_uploads.pluck(:id)
 
@@ -541,7 +541,7 @@ task "uploads:disable_secure_media" => :environment do
     )
 
     post_ids_to_rebake = DB.query_single(
-      "SELECT DISTINCT post_id FROM post_uploads WHERE upload_id IN (?)", secure_upload_ids
+      "SELECT DISTINCT target_id FROM upload_references WHERE upload_id IN (?) AND target_type = 'Post'", secure_upload_ids
     )
     adjust_acls(secure_upload_ids)
     post_rebake_errors = rebake_upload_posts(post_ids_to_rebake)
@@ -621,8 +621,8 @@ def mark_all_as_secure_login_required
   post_upload_ids_marked_secure = DB.query_single(<<~SQL)
     WITH upl AS (
       SELECT DISTINCT ON (upload_id) upload_id
-      FROM post_uploads
-      INNER JOIN posts ON posts.id = post_uploads.post_id
+      FROM upload_references
+      INNER JOIN posts ON posts.id = upload_references.target_id AND upload_references.target_type = 'Post'
       INNER JOIN topics ON topics.id = posts.topic_id
     )
     UPDATE uploads
@@ -646,7 +646,7 @@ def mark_all_as_secure_login_required
   puts "Finished marking upload(s) as secure."
 
   post_ids_to_rebake = DB.query_single(
-    "SELECT DISTINCT post_id FROM post_uploads WHERE upload_id IN (?)", post_upload_ids_marked_secure
+    "SELECT DISTINCT target_id FROM upload_references WHERE upload_id IN (?) AND target_type = 'Post'", post_upload_ids_marked_secure
   )
   [post_ids_to_rebake, (post_upload_ids_marked_secure + upload_ids_marked_not_secure).uniq]
 end
@@ -665,8 +665,8 @@ def update_specific_upload_security_no_login_required
   post_upload_ids_marked_secure = DB.query_single(<<~SQL)
     WITH upl AS (
       SELECT DISTINCT ON (upload_id) upload_id
-      FROM post_uploads
-      INNER JOIN posts ON posts.id = post_uploads.post_id
+      FROM upload_references
+      INNER JOIN posts ON posts.id = upload_references.target_id AND upload_references.target_type = 'Post'
       INNER JOIN topics ON topics.id = posts.topic_id
       LEFT JOIN categories ON categories.id = topics.category_id
       WHERE (topics.category_id IS NOT NULL AND categories.read_restricted) OR
@@ -686,8 +686,8 @@ def update_specific_upload_security_no_login_required
   post_upload_ids_marked_not_secure = DB.query_single(<<~SQL)
     WITH upl AS (
       SELECT DISTINCT ON (upload_id) upload_id
-      FROM post_uploads
-      INNER JOIN posts ON posts.id = post_uploads.post_id
+      FROM upload_references
+      INNER JOIN posts ON posts.id = upload_references.target_id AND upload_references.target_type = 'Post'
       INNER JOIN topics ON topics.id = posts.topic_id
       LEFT JOIN categories ON categories.id = topics.category_id
       WHERE (topics.archetype = 'regular' AND topics.category_id IS NOT NULL AND NOT categories.read_restricted) OR
@@ -716,14 +716,17 @@ def update_specific_upload_security_no_login_required
   puts "Finished updating upload security. Marked #{upload_ids_marked_not_secure.length} uploads not linked to posts as not secure."
 
   all_upload_ids_changed = (upload_ids_changed + upload_ids_marked_not_secure).uniq
-  post_ids_to_rebake = DB.query_single("SELECT DISTINCT post_id FROM post_uploads WHERE upload_id IN (?)", upload_ids_changed)
+  post_ids_to_rebake = DB.query_single("SELECT DISTINCT target_id FROM upload_references WHERE upload_id IN (?) AND target_type = 'Post'", upload_ids_changed)
   [post_ids_to_rebake, all_upload_ids_changed]
 end
 
 def update_uploads_access_control_post
   DB.exec(<<~SQL)
     WITH upl AS (
-      SELECT DISTINCT ON (upload_id) upload_id, post_id FROM post_uploads ORDER BY upload_id, post_id
+      SELECT DISTINCT ON (upload_id) upload_id, target_id AS post_id
+      FROM upload_references
+      WHERE target_type = 'Post'
+      ORDER BY upload_id, target_id
     )
     UPDATE uploads
     SET access_control_post_id = upl.post_id
@@ -855,9 +858,9 @@ def analyze_missing_s3
   puts "List of posts with missing images:"
   sql = <<~SQL
     SELECT post_id, url, sha1, extension, uploads.id
-    FROM post_uploads pu
-    RIGHT JOIN uploads on uploads.id = pu.upload_id
-    WHERE verification_status = :invalid_etag
+    FROM upload_references ur
+    RIGHT JOIN uploads on uploads.id = ur.upload_id
+    WHERE ur.target_type = 'Post' AND verification_status = :invalid_etag
     ORDER BY created_at
   SQL
 
@@ -867,9 +870,9 @@ def analyze_missing_s3
 
   DB.query(sql, invalid_etag: Upload.verification_statuses[:invalid_etag]).each do |r|
     all << r
-    if r.post_id
-      lookup[r.post_id] ||= []
-      lookup[r.post_id] << [r.url, r.sha1, r.extension]
+    if r.target_id
+      lookup[r.target_id] ||= []
+      lookup[r.target_id] << [r.url, r.sha1, r.extension]
     else
       other << r
     end
@@ -894,7 +897,7 @@ def analyze_missing_s3
     ids = all.map { |r| r.id }
 
     lookups = [
-      [:post_uploads, :upload_id],
+      [:upload_references, :upload_id],
       [:users, :uploaded_avatar_id],
       [:user_avatars, :gravatar_upload_id],
       [:user_avatars, :custom_upload_id],
@@ -1026,7 +1029,7 @@ def fix_missing_s3
           puts "Failed to save upload #{save_error}"
         else
           OptimizedImage.where(upload_id: upload.id).destroy_all
-          rebake_ids = PostUpload.where(upload_id: upload.id).pluck(:post_id)
+          rebake_ids = UploadReferences.where(upload_id: upload.id).where(target_type: 'Post').pluck(:target_id)
 
           if rebake_ids.present?
             Post.where(id: rebake_ids).each do |post|
@@ -1044,11 +1047,12 @@ def fix_missing_s3
   puts "Rebaking posts with missing uploads, this can take a while as all rebaking runs inline"
 
   sql = <<~SQL
-    SELECT post_id
-    FROM post_uploads pu
-    JOIN uploads on uploads.id = pu.upload_id
+    SELECT target_id
+    FROM upload_references ur
+    JOIN uploads on uploads.id = ur.upload_id
+    WHERE ur.target_type = 'Post'
     WHERE verification_status = :invalid_etag
-    ORDER BY post_id DESC
+    ORDER BY target_id DESC
   SQL
 
   DB.query_single(sql, invalid_etag: Upload.verification_statuses[:invalid_etag]).each do |post_id|

app/jobs/scheduled/clean_up_uploads.rb
@@ -31,18 +31,19 @@ module Jobs
         .where("uploads.retain_hours IS NULL OR uploads.created_at < current_timestamp - interval '1 hour' * uploads.retain_hours")
         .where("uploads.created_at < ?", grace_period.hour.ago)
         .where("uploads.access_control_post_id IS NULL")
-        .joins("LEFT JOIN post_uploads pu ON pu.upload_id = uploads.id")
-        .where("pu.upload_id IS NULL")
+        .joins("LEFT JOIN upload_references ON upload_references.upload_id = uploads.id")
+        .where("upload_references.upload_id IS NULL")
         .with_no_non_post_relations
 
       result.find_each do |upload|
+        next if Upload.in_use_callbacks&.any? { |callback| callback.call(upload) }
+
         if upload.sha1.present?
+          # TODO: Remove this check after UploadReferences records were created
           encoded_sha = Base62.encode(upload.sha1.hex)
           next if ReviewableQueuedPost.pending.where("payload->>'raw' LIKE '%#{upload.sha1}%' OR payload->>'raw' LIKE '%#{encoded_sha}%'").exists?
           next if Draft.where("data LIKE '%#{upload.sha1}%' OR data LIKE '%#{encoded_sha}%'").exists?
           next if UserProfile.where("bio_raw LIKE '%#{upload.sha1}%' OR bio_raw LIKE '%#{encoded_sha}%'").exists?
-
-          next if Upload.in_use_callbacks&.any? { |callback| callback.call(upload) }
 
           upload.destroy
         else

app/models/badge.rb
@@ -108,6 +108,7 @@ class Badge < ActiveRecord::Base
   belongs_to :image_upload, class_name: 'Upload'
 
   has_many :user_badges, dependent: :destroy
+  has_many :upload_references, as: :target, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :badge_type, presence: true
@@ -118,6 +119,12 @@ class Badge < ActiveRecord::Base
 
   before_create :ensure_not_system
   before_save :sanitize_description
+
+  after_save do
+    if saved_change_to_image_upload_id?
+      UploadReference.ensure_exist!(upload_ids: [self.image_upload_id], target: self)
+    end
+  end
 
   after_commit do
     SvgSprite.expire_cache

app/models/category.rb
@@ -45,6 +45,7 @@ class Category < ActiveRecord::Base
   has_many :category_groups, dependent: :destroy
   has_many :groups, through: :category_groups
   has_many :topic_timers, dependent: :destroy
+  has_many :upload_references, as: :target, dependent: :destroy
 
   has_and_belongs_to_many :web_hooks
 
@@ -79,6 +80,13 @@ class Category < ActiveRecord::Base
   after_save :clear_subcategory_ids
   after_save :clear_url_cache
   after_save :update_reviewables
+
+  after_save do
+    if saved_change_to_uploaded_logo_id? || saved_change_to_uploaded_background_id?
+      upload_ids = [self.uploaded_logo_id, self.uploaded_background_id]
+      UploadReference.ensure_exist!(upload_ids: upload_ids, target: self)
+    end
+  end
 
   after_destroy :reset_topic_ids_cache
   after_destroy :publish_category_deletion

app/models/custom_emoji.rb
@@ -3,8 +3,16 @@
 class CustomEmoji < ActiveRecord::Base
   belongs_to :upload
 
+  has_many :upload_references, as: :target, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
   validates :upload_id, presence: true
+
+  after_save do
+    if saved_change_to_upload_id?
+      UploadReference.ensure_exist!(upload_ids: [self.upload_id], target: self)
+    end
+  end
 end
 
 # == Schema Information

app/models/draft.rb
@@ -82,7 +82,7 @@ class Draft < ActiveRecord::Base
         owner: owner
       }
 
-      DB.exec(<<~SQL, opts)
+      draft_id = DB.query_single(<<~SQL, opts).first
         INSERT INTO drafts (user_id, draft_key, data, sequence, owner, created_at, updated_at)
         VALUES (:user_id, :draft_key, :data, :sequence, :owner, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
         ON CONFLICT (user_id, draft_key) DO
@@ -93,10 +93,17 @@ class Draft < ActiveRecord::Base
           revisions = drafts.revisions + 1,
           owner = :owner,
           updated_at = CURRENT_TIMESTAMP
+        RETURNING id
       SQL
 
       UserStat.update_draft_count(user.id)
     end
+
+    UploadReference.ensure_exist!(
+      upload_ids: Upload.extract_upload_ids(data),
+      target_type: 'Draft',
+      target_id: draft_id
+    )
 
     sequence
   end

app/models/group.rb
@@ -36,6 +36,8 @@ class Group < ActiveRecord::Base
   has_many :associated_groups, through: :group_associated_groups, dependent: :destroy
 
   belongs_to :flair_upload, class_name: 'Upload'
+  has_many :upload_references, as: :target, dependent: :destroy
+
   belongs_to :smtp_updated_by, class_name: 'User'
   belongs_to :imap_updated_by, class_name: 'User'
 
@@ -50,6 +52,12 @@ class Group < ActiveRecord::Base
 
   after_save :enqueue_update_mentions_job,
     if: Proc.new { |g| g.name_before_last_save && g.saved_change_to_name? }
+
+  after_save do
+    if saved_change_to_flair_upload_id?
+      UploadReference.ensure_exist!(upload_ids: [self.flair_upload_id], target: self)
+    end
+  end
 
   after_save :expire_cache
   after_destroy :expire_cache

commit 40d93e3eeecfa05725363f1751183a4778b82c62

FIX: Should be UploadReference instead of UploadReferences (#17361)
This fixes a couple of typos that were introduced in 6765cf0

lib/shrink_uploaded_image.rb
@@ -134,7 +134,7 @@ class ShrinkUploadedImage
 
     if existing_upload
       begin
-        UploadReferences
+        UploadReference
           .where(target_type: 'Post')
           .where(upload_id: original_upload.id)
           .update_all(upload_id: upload.id)

lib/tasks/uploads.rake
@@ -1029,7 +1029,7 @@ def fix_missing_s3
           puts "Failed to save upload #{save_error}"
         else
           OptimizedImage.where(upload_id: upload.id).destroy_all
-          rebake_ids = UploadReferences.where(upload_id: upload.id).where(target_type: 'Post').pluck(:target_id)
+          rebake_ids = UploadReference.where(upload_id: upload.id).where(target_type: 'Post').pluck(:target_id)
 
           if rebake_ids.present?
             Post.where(id: rebake_ids).each do |post|
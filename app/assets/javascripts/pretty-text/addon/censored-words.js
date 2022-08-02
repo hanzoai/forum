hanzoai/forum

commit 1672d76a0a88b3bfbebef4809e7dd5799942d28c

FEATURE:  Add support for case-sensitive Watched Words (#17445)
* FEATURE: Add case-sensitivity flag to watched_words

Currently, all watched words are matched case-insensitively. This flag
allows a watched word to be flagged for case-sensitive matching.
To allow allow for backwards compatibility the flag is set to false by
default.

* FEATURE: Support case-sensitive creation of Watched Words via API

Extend admin creation and upload of Watched Words to support case
sensitive flag. This lays the ground work for supporting
case-insensitive matching of Watched Words.

Support for an extra column has also been introduced for the Watched
Words upload CSV file. The new column structure is as follows:

 word,replacement,case_sentive

* FEATURE: Enable case-sensitive matching of Watched Words

WordWatcher's word_matcher_regexp now returns a list of regular
expressions instead of one case-insensitive regular expression.

With the ability to flag a Watched Word as case-sensitive, an action
can have words of both sensitivities.This makes the use of the global
Regexp::IGNORECASE flag added to all words problematic.

To get around platform limitations around the use of subexpression level
switches/flags, a list of regular expressions is returned instead, one for each
case sensitivity.

Word matching has also been updated to use this list of regular expressions
instead of one.

* FEATURE: Use case-sensitive regular expressions for Watched Words

Update Watched Words regular expressions matching and processing to handle
the extra metadata which comes along with the introduction of
case-sensitive Watched Words.

This allows case-sensitive Watched Words to matched as such.

* DEV: Simplify type casting of case-sensitive flag from uploads

Use builtin semantics instead of a custom method for converting
string case flags in uploaded Watched Words to boolean.

* UX: Add case-sensitivity details to Admin Watched Words UI

app/assets/javascripts/pretty-text/addon/censored-words.js
@@ -1,15 +1,24 @@
-export function censorFn(regexpString, replacementLetter) {
-  if (regexpString) {
-    let censorRegexp = new RegExp(regexpString, "ig");
+import {
+  createWatchedWordRegExp,
+  toWatchedWord,
+} from "discourse-common/utils/watched-words";
+
+export function censorFn(regexpList, replacementLetter) {
+  if (regexpList.length) {
     replacementLetter = replacementLetter || "&#9632;";
+    let censorRegexps = regexpList.map((regexp) => {
+      return createWatchedWordRegExp(toWatchedWord(regexp));
+    });
 
     return function (text) {
-      text = text.replace(censorRegexp, (fullMatch, ...groupMatches) => {
-        const stringMatch = groupMatches.find((g) => typeof g === "string");
-        return fullMatch.replace(
-          stringMatch,
-          new Array(stringMatch.length + 1).join(replacementLetter)
-        );
+      censorRegexps.forEach((censorRegexp) => {
+        text = text.replace(censorRegexp, (fullMatch, ...groupMatches) => {
+          const stringMatch = groupMatches.find((g) => typeof g === "string");
+          return fullMatch.replace(
+            stringMatch,
+            new Array(stringMatch.length + 1).join(replacementLetter)
+          );
+        });
       });
 
       return text;

app/assets/javascripts/admin/addon/components/admin-watched-word.js
@@ -1,5 +1,5 @@
 import Component from "@ember/component";
-import { equal } from "@ember/object/computed";
+import { alias, equal } from "@ember/object/computed";
 import bootbox from "bootbox";
 import discourseComputed from "discourse-common/utils/decorators";
 import { action } from "@ember/object";
@@ -11,6 +11,7 @@ export default Component.extend({
   isReplace: equal("actionKey", "replace"),
   isTag: equal("actionKey", "tag"),
   isLink: equal("actionKey", "link"),
+  isCaseSensitive: alias("word.case_sensitive"),
 
   @discourseComputed("word.replacement")
   tags(replacement) {

app/assets/javascripts/admin/addon/components/watched-word-form.js
@@ -14,6 +14,7 @@ export default Component.extend({
   actionKey: null,
   showMessage: false,
   selectedTags: null,
+  isCaseSensitive: false,
 
   canReplace: equal("actionKey", "replace"),
   canTag: equal("actionKey", "tag"),
@@ -78,6 +79,7 @@ export default Component.extend({
               ? this.replacement
               : null,
           action: this.actionKey,
+          isCaseSensitive: this.isCaseSensitive,
         });
 
         watchedWord
@@ -90,6 +92,7 @@ export default Component.extend({
               selectedTags: [],
               showMessage: true,
               message: I18n.t("admin.watched_words.form.success"),
+              isCaseSensitive: false,
             });
             this.action(WatchedWord.create(result));
             schedule("afterRender", () =>

app/assets/javascripts/admin/addon/controllers/modals/admin-watched-word-test.js
@@ -2,6 +2,10 @@ import Controller from "@ember/controller";
 import ModalFunctionality from "discourse/mixins/modal-functionality";
 import discourseComputed from "discourse-common/utils/decorators";
 import { equal } from "@ember/object/computed";
+import {
+  createWatchedWordRegExp,
+  toWatchedWord,
+} from "discourse-common/utils/watched-words";
 
 export default Controller.extend(ModalFunctionality, {
   isReplace: equal("model.nameKey", "replace"),
@@ -16,16 +20,17 @@ export default Controller.extend(ModalFunctionality, {
     "isTag",
     "isLink"
   )
-  matches(value, regexpString, words, isReplace, isTag, isLink) {
-    if (!value || !regexpString) {
+  matches(value, regexpList, words, isReplace, isTag, isLink) {
+    if (!value || regexpList.length === 0) {
       return [];
     }
 
     if (isReplace || isLink) {
       const matches = [];
       words.forEach((word) => {
-        const regexp = new RegExp(word.regexp, "gi");
+        const regexp = createWatchedWordRegExp(word);
         let match;
+
         while ((match = regexp.exec(value)) !== null) {
           matches.push({
             match: match[1],
@@ -37,8 +42,9 @@ export default Controller.extend(ModalFunctionality, {
     } else if (isTag) {
       const matches = {};
       words.forEach((word) => {
-        const regexp = new RegExp(word.regexp, "gi");
+        const regexp = createWatchedWordRegExp(word);
         let match;
+
         while ((match = regexp.exec(value)) !== null) {
           if (!matches[match[1]]) {
             matches[match[1]] = new Set();
@@ -56,7 +62,14 @@ export default Controller.extend(ModalFunctionality, {
         tags: Array.from(entry[1]),
       }));
     } else {
-      return value.match(new RegExp(regexpString, "ig")) || [];
+      let matches = [];
+      regexpList.forEach((regexp) => {
+        const wordRegexp = createWatchedWordRegExp(toWatchedWord(regexp));
+
+        matches.push(...(value.match(wordRegexp) || []));
+      });
+
+      return matches;
     }
   },
 });

app/assets/javascripts/admin/addon/models/watched-word.js
@@ -14,6 +14,7 @@ const WatchedWord = EmberObject.extend({
           word: this.word,
           replacement: this.replacement,
           action_key: this.action,
+          case_sensitive: this.isCaseSensitive,
         },
         dataType: "json",
       }

app/assets/javascripts/admin/addon/templates/components/admin-watched-word.hbs
@@ -7,3 +7,6 @@
     <span class="tag">{{tag}}</span>
   {{/each}}
 {{/if}}
+{{#if this.isCaseSensitive}}
+  <span class="case-sensitive">{{i18n "admin.watched_words.case_sensitive"}}</span>
+{{/if}}

app/assets/javascripts/admin/addon/templates/components/watched-word-form.hbs
@@ -27,6 +27,14 @@
   </div>
 {{/if}}
 
+<div class="watched-word-input">
+  <label for="watched-case-sensitivity">{{i18n "admin.watched_words.form.case_sensitivity_label"}}</label>
+  <label class="case-sensitivity-checkbox">
+    <Input @type="checkbox" @checked={{this.isCaseSensitive}} disabled={{this.formSubmitted}} />
+    {{i18n "admin.watched_words.form.case_sensitivity_description"}}
+  </label>
+</div>
+
 <DButton @type="submit" @class="btn btn-primary" @action={{action "submit"}} @disabled={{this.formSubmitted}} @label="admin.watched_words.form.add" />
 
 {{#if this.showMessage}}

app/assets/javascripts/discourse-common/addon/utils/watched-words.js
@@ -1,0 +1,9 @@
+export function createWatchedWordRegExp(word) {
+  const caseFlag = word.case_sensitive ? "" : "i";
+  return new RegExp(word.regexp, `${caseFlag}g`);
+}
+
+export function toWatchedWord(regexp) {
+  const [[regexpString, options]] = Object.entries(regexp);
+  return { regexp: regexpString, ...options };
+}

commit 661109cfec1827d37123bade6a12cce03e5ab478

DEV: Ensure `censorFn` copes with null `regexpList` (#17754)
This fixes the test suite failures introduced by 1672d76a0a88b3bfbebef4809e7dd5799942d28c

app/assets/javascripts/pretty-text/addon/censored-words.js
@@ -4,7 +4,7 @@ import {
 } from "discourse-common/utils/watched-words";
 
 export function censorFn(regexpList, replacementLetter) {
-  if (regexpList.length) {
+  if (regexpList?.length) {
     replacementLetter = replacementLetter || "&#9632;";
     let censorRegexps = regexpList.map((regexp) => {
       return createWatchedWordRegExp(toWatchedWord(regexp));
hanzoai/forum

commit 5a30aeb1fbebf09740e0fad61a1d4a3db3ca93c8

DEV: Load client site settings YML into JS tests (#18413)
Our method of loading a subset of client settings into tests via
tests/helpers/site-settings.js can be improved upon. Currently we have a
hardcoded subset of the client settings, which may get out of date and not have
the correct defaults. As well as this plugins do not get their settings into the
tests, so whenever you need a setting from a plugin, even if it has a default,
you have to do needs.setting({ ... }) which is inconvenient.

This commit introduces an ember CLI build step to take the site_settings.yml and
all the plugin settings.yml files, pull out the client settings, and dump them
into a variable in a single JS file we can load in our tests, so we have the
correct selection of settings and default values in our JS tests. It also fixes
many, many tests that were operating under incorrect assumptions or old
settings.

Co-authored-by: Joffrey JAFFEUX <j.jaffeux@gmail.com>

app/assets/javascripts/discourse/app/widgets/search-menu.js
@@ -367,7 +367,7 @@ export default createWidget("search-menu", {
       return;
     }
 
-    if (e.which === 65 /* a */) {
+    if (e.key === "A") {
       if (document.activeElement?.classList.contains("search-link")) {
         if (document.querySelector("#reply-control.open")) {
           // add a link and focus composer
@@ -388,8 +388,8 @@ export default createWidget("search-menu", {
       }
     }
 
-    const up = e.which === 38;
-    const down = e.which === 40;
+    const up = e.key === "ArrowUp";
+    const down = e.key === "ArrowDown";
     if (up || down) {
       let focused = document.activeElement.closest(".search-menu")
         ? document.activeElement
@@ -443,7 +443,7 @@ export default createWidget("search-menu", {
     }
 
     const searchInput = document.querySelector("#search-term");
-    if (e.which === 13 && e.target === searchInput) {
+    if (e.key === "Enter" && e.target === searchInput) {
       const recentEnterHit =
         this.state._lastEnterTimestamp &&
         Date.now() - this.state._lastEnterTimestamp < SECOND_ENTER_MAX_DELAY;
@@ -463,7 +463,7 @@ export default createWidget("search-menu", {
       this.state._lastEnterTimestamp = Date.now();
     }
 
-    if (e.target === searchInput && e.which === 8 /* backspace */) {
+    if (e.target === searchInput && e.key === "Backspace") {
       if (!searchInput.value) {
         this.clearTopicContext();
         this.clearPMInboxContext();

app/assets/javascripts/discourse/tests/acceptance/search-test.js
@@ -4,7 +4,13 @@ import {
   exists,
   query,
 } from "discourse/tests/helpers/qunit-helpers";
-import { click, fillIn, triggerKeyEvent, visit } from "@ember/test-helpers";
+import {
+  click,
+  fillIn,
+  settled,
+  triggerKeyEvent,
+  visit,
+} from "@ember/test-helpers";
 import I18n from "I18n";
 import searchFixtures from "discourse/tests/fixtures/search-fixtures";
 import selectKit from "discourse/tests/helpers/select-kit-helper";
@@ -333,7 +339,10 @@ acceptance("Search - Anonymous", function (needs) {
 
 acceptance("Search - Authenticated", function (needs) {
   needs.user();
-  needs.settings({ log_search_queries: true });
+  needs.settings({
+    log_search_queries: true,
+    allow_uncategorized_topics: true,
+  });
 
   needs.pretender((server, helper) => {
     server.get("/search/query", (request) => {
@@ -476,6 +485,7 @@ acceptance("Search - Authenticated", function (needs) {
       "href"
     );
     await triggerKeyEvent(".search-menu", "keydown", "A");
+    await settled();
 
     assert.strictEqual(
       query("#reply-control textarea").value,

app/assets/javascripts/discourse-plugins/index.js
@@ -113,18 +113,22 @@ module.exports = {
         directoryName,
         "test/javascripts"
       );
+      const configDirectory = path.resolve(root, directoryName, "config");
       const hasJs = fs.existsSync(jsDirectory);
       const hasAdminJs = fs.existsSync(adminJsDirectory);
       const hasTests = fs.existsSync(testDirectory);
+      const hasConfig = fs.existsSync(configDirectory);
       return {
         pluginName,
         directoryName,
         jsDirectory,
         adminJsDirectory,
         testDirectory,
+        configDirectory,
         hasJs,
         hasAdminJs,
         hasTests,
+        hasConfig,
       };
     });
   },

app/assets/javascripts/discourse/app/initializers/register-media-optimization-upload-processor.js
@@ -1,5 +1,6 @@
 import { addComposerUploadPreProcessor } from "discourse/components/composer-editor";
 import UppyMediaOptimization from "discourse/lib/uppy-media-optimization-plugin";
+import { Promise } from "rsvp";
 
 export default {
   name: "register-media-optimization-upload-processor",
@@ -11,10 +12,15 @@ export default {
         UppyMediaOptimization,
         ({ isMobileDevice }) => {
           return {
-            optimizeFn: (data, opts) =>
-              container
+            optimizeFn: (data, opts) => {
+              if (container.isDestroyed || container.isDestroying) {
+                return Promise.resolve();
+              }
+
+              return container
                 .lookup("service:media-optimization-worker")
-                .optimizeImage(data, opts),
+                .optimizeImage(data, opts);
+            },
             runParallel: !isMobileDevice,
           };
         }

app/assets/javascripts/discourse/ember-cli-build.js
@@ -6,6 +6,7 @@ const mergeTrees = require("broccoli-merge-trees");
 const concat = require("broccoli-concat");
 const prettyTextEngine = require("./lib/pretty-text-engine");
 const { createI18nTree } = require("./lib/translation-plugin");
+const { parsePluginClientSettings } = require("./lib/site-settings-plugin");
 const discourseScss = require("./lib/discourse-scss");
 const generateScriptsTree = require("./lib/scripts");
 const funnel = require("broccoli-funnel");
@@ -168,6 +169,7 @@ module.exports = function (defaults) {
 
   return mergeTrees([
     createI18nTree(discourseRoot, vendorJs),
+    parsePluginClientSettings(discourseRoot, vendorJs, app),
     app.toTree(),
     funnel(`${discourseRoot}/public/javascripts`, { destDir: "javascripts" }),
     funnel(`${vendorJs}/highlightjs`, {

app/assets/javascripts/discourse/lib/site-settings-plugin.js
@@ -1,0 +1,96 @@
+const Plugin = require("broccoli-plugin");
+const Yaml = require("js-yaml");
+const fs = require("fs");
+const concat = require("broccoli-concat");
+const mergeTrees = require("broccoli-merge-trees");
+const deepmerge = require("deepmerge");
+const glob = require("glob");
+const { shouldLoadPluginTestJs } = require("discourse/lib/plugin-js");
+
+let built = false;
+
+class SiteSettingsPlugin extends Plugin {
+  constructor(inputNodes, inputFile, options) {
+    super(inputNodes, {
+      ...options,
+      persistentOutput: true,
+    });
+  }
+
+  build() {
+    if (built) {
+      return;
+    }
+
+    let parsed = {};
+
+    this.inputPaths.forEach((path) => {
+      let inputFile;
+      if (path.includes("plugins")) {
+        inputFile = "settings.yml";
+      } else {
+        inputFile = "site_settings.yml";
+      }
+      const file = path + "/" + inputFile;
+      let yaml;
+      try {
+        yaml = fs.readFileSync(file, { encoding: "UTF-8" });
+      } catch (err) {
+        // the plugin does not have a config file, go to the next file
+        return;
+      }
+      const loaded = Yaml.load(yaml, { json: true });
+      parsed = deepmerge(parsed, loaded);
+    });
+
+    let clientSettings = {};
+    // eslint-disable-next-line no-unused-vars
+    for (const [category, settings] of Object.entries(parsed)) {
+      for (const [setting, details] of Object.entries(settings)) {
+        if (details.client) {
+          clientSettings[setting] = details.default;
+        }
+      }
+    }
+    const contents = `var CLIENT_SITE_SETTINGS_WITH_DEFAULTS  = ${JSON.stringify(
+      clientSettings
+    )}`;
+
+    fs.writeFileSync(`${this.outputPath}/` + "settings_out.js", contents);
+    built = true;
+  }
+}
+
+module.exports = function siteSettingsPlugin(...params) {
+  return new SiteSettingsPlugin(...params);
+};
+
+module.exports.parsePluginClientSettings = function (
+  discourseRoot,
+  vendorJs,
+  app
+) {
+  let settings = [discourseRoot + "/config"];
+
+  if (shouldLoadPluginTestJs()) {
+    const pluginInfos = app.project
+      .findAddonByName("discourse-plugins")
+      .pluginInfos();
+    pluginInfos.forEach(({ hasConfig, configDirectory }) => {
+      if (hasConfig) {
+        settings = settings.concat(glob.sync(configDirectory));
+      }
+    });
+  }
+
+  const loadedSettings = new SiteSettingsPlugin(settings, "site_settings.yml");
+
+  return concat(mergeTrees([loadedSettings]), {
+    inputFiles: [],
+    headerFiles: [],
+    footerFiles: [],
+    outputFile: `assets/test-site-settings.js`,
+  });
+};
+
+module.exports.SiteSettingsPlugin = SiteSettingsPlugin;

app/assets/javascripts/discourse/tests/acceptance/bootstrap-mode-notice-test.js
@@ -1,10 +1,9 @@
 import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
 import { test } from "qunit";
 import { click, currentURL, settled, visit } from "@ember/test-helpers";
-import { set } from "@ember/object";
 
 acceptance("Bootstrap Mode Notice", function (needs) {
-  needs.user();
+  needs.user({ admin: true });
   needs.site({ wizard_required: true });
   needs.settings({
     bootstrap_mode_enabled: true,
@@ -36,8 +35,8 @@ acceptance("Bootstrap Mode Notice", function (needs) {
       "it transitions to the wizard page"
     );
 
+    this.siteSettings.bootstrap_mode_enabled = false;
     await visit("/");
-    set(this.siteSettings, "bootstrap_mode_enabled", false);
     await settled();
     assert.ok(
       !exists(".bootstrap-mode-notice"),

app/assets/javascripts/discourse/tests/acceptance/composer-actions-test.js
@@ -24,7 +24,7 @@ acceptance("Composer Actions", function (needs) {
   });
   needs.settings({
     prioritize_username_in_ux: true,
-    display_name_on_post: false,
+    display_name_on_posts: false,
     enable_whispers: true,
   });
   needs.site({ can_tag_topics: true });
@@ -489,7 +489,7 @@ acceptance("Prioritize Username", function (needs) {
   needs.user();
   needs.settings({
     prioritize_username_in_ux: true,
-    display_name_on_post: false,
+    display_name_on_posts: false,
   });
 
   test("Reply to post use username", async function (assert) {
@@ -517,7 +517,7 @@ acceptance("Prioritize Full Name", function (needs) {
   needs.user();
   needs.settings({
     prioritize_username_in_ux: false,
-    display_name_on_post: true,
+    display_name_on_posts: true,
   });
 
   test("Reply to post use full name", async function (assert) {
@@ -555,7 +555,7 @@ acceptance("Prioritizing Name fall back", function (needs) {
   needs.user();
   needs.settings({
     prioritize_username_in_ux: false,
-    display_name_on_post: true,
+    display_name_on_posts: true,
   });
 
   test("Quotes fall back to username if name is not present", async function (assert) {

commit fdf48df615eaf536f21001267a33b206ad1b48bf

FIX: Regression with special `a` keyword in search (#19063)
Fixes a regression introduced in 5a30aeb.

app/assets/javascripts/discourse/app/widgets/search-menu.js
@@ -373,7 +373,7 @@ export default createWidget("search-menu", {
       return;
     }
 
-    if (e.key === "A") {
+    if (e.which === 65 /* a */) {
       if (document.activeElement?.classList.contains("search-link")) {
         if (document.querySelector("#reply-control.open")) {
           // add a link and focus composer

app/assets/javascripts/discourse/tests/acceptance/search-test.js
@@ -374,6 +374,17 @@ acceptance("Search - Authenticated", function (needs) {
 
       return helper.response(searchFixtures["search/query"]);
     });
+
+    server.get("/inline-onebox", () =>
+      helper.response({
+        "inline-oneboxes": [
+          {
+            url: "http://www.something.com",
+            title: searchFixtures["search/query"].topics[0].title,
+          },
+        ],
+      })
+    );
   });
 
   test("Right filters are shown in full page search", async function (assert) {
@@ -517,6 +528,30 @@ acceptance("Search - Authenticated", function (needs) {
     assert.ok(exists(query(`${container} ul li`)), "has a list of items");
     await triggerKeyEvent("#search-term", "keydown", "Enter");
     assert.ok(exists(query(`.search-menu`)), "search dropdown is visible");
+  });
+
+  test("search while composer is open", async function (assert) {
+    await visit("/t/internationalization-localization/280");
+    await click(".reply");
+    await fillIn(".d-editor-input", "a link");
+    await click("#search-button");
+    await fillIn("#search-term", "dev");
+
+    await triggerKeyEvent("#search-term", "keydown", "Enter");
+    await triggerKeyEvent(".search-menu", "keydown", "ArrowDown");
+    await triggerKeyEvent("#search-term", "keydown", 65); // maps to lowercase a
+
+    assert.ok(
+      query(".d-editor-input").value.includes("a link"),
+      "still has the original composer content"
+    );
+
+    assert.ok(
+      query(".d-editor-input").value.includes(
+        searchFixtures["search/query"].topics[0].slug
+      ),
+      "adds link from search to composer"
+    );
   });
 
   test("Shows recent search results", async function (assert) {
hanzoai/forum

commit 7d286a4f6a5667dab9700f17be7fc150cf07fb26

DEV: Remove user options from current user serializer (#19089)
User options were serialized at the root level of CurrentUserSerializer,
but UserSerializer has a user_option field. This inconsistency caused
issues in the past because user_option fields had to be duplicated on
the frontend.

app/assets/javascripts/admin/addon/routes/admin-customize-themes-show.js
@@ -31,7 +31,7 @@ export default Route.extend({
       model,
       parentController,
       allThemes: parentController.get("model"),
-      colorSchemeId: model.get("color_scheme_id"),
+      colorSchemeId: model.get("user_option.color_scheme_id"),
       colorSchemes: parentController.get("model.extras.color_schemes"),
       editingName: false,
     });

commit dccb4d71facddf22a57bd12e4fd4d3345e52e683

FIX: Use correct property for theme's `color_scheme_id` (#19363)
Regressed in 7d286a4f6a5667dab9700f17be7fc150cf07fb26

app/assets/javascripts/admin/addon/routes/admin-customize-themes-show.js
@@ -31,7 +31,7 @@ export default Route.extend({
       model,
       parentController,
       allThemes: parentController.get("model"),
-      colorSchemeId: model.get("user_option.color_scheme_id"),
+      colorSchemeId: model.get("color_scheme_id"),
       colorSchemes: parentController.get("model.extras.color_schemes"),
       editingName: false,
     });
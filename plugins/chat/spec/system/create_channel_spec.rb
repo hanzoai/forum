hanzoai/forum

commit cee8b7319675cdbd8f9c4707519a6615ac5e7c76

REFACTOR: Fix pluralized strings in chat plugin (#20357)
* FIX: Use pluralized string

* REFACTOR: Fix misuse of pluralized string

* REFACTOR: Fix misuse of pluralized string

* DEV: Remove linting of `one` key in MessageFormat string, it doesn't work

* REFACTOR: Fix misuse of pluralized string

This also ensures that the URL works on subfolder and shows the site setting link only for admins instead of staff. The string is quite complicated, so the best option was to switch to MessageFormat.

* REFACTOR: Fix misuse of pluralized string

* FIX: Use pluralized string

This also ensures that the URL works on subfolder and shows the site setting link only for admins instead of staff.

* REFACTOR: Correctly pluralize reaction tooltips in chat

This also ensures that maximum 5 usernames are shown and fixes the number of "others" which was off by 1 if the current user reacted on a message.

* REFACTOR: Use translatable string as comma separator

* DEV: Add comment to translation to clarify the meaning of `%{identifier}`

* REFACTOR: Use translatable comma separator and use explicit interpolation keys

* REFACTOR: Don't interpolate lowercase channel status

* REFACTOR: Fix misuse of pluralized string

* REFACTOR: Don't interpolate channel status

* REFACTOR: Use %{count} interpolation key

* REFACTOR: Fix misuse of pluralized string

* REFACTOR: Correctly pluralize DM chat channel titles

plugins/chat/assets/javascripts/discourse/controllers/create-channel.js
@@ -103,20 +103,40 @@ export default class CreateChannelController extends Controller.extend(
 
   _updateAutoJoinConfirmWarning(category, catPermissions) {
     const allowedGroups = catPermissions.allowed_groups;
+    let warning;
 
     if (catPermissions.private) {
-      const warningTranslationKey =
-        allowedGroups.length < 3 ? "warning_groups" : "warning_multiple_groups";
-
-      this.set(
-        "autoJoinWarning",
-        I18n.t(`chat.create_channel.auto_join_users.${warningTranslationKey}`, {
-          members_count: catPermissions.members_count,
-          group: escapeExpression(allowedGroups[0]),
-          group_2: escapeExpression(allowedGroups[1]),
-          count: allowedGroups.length,
-        })
-      );
+      switch (allowedGroups.length) {
+        case 1:
+          warning = I18n.t(
+            "chat.create_channel.auto_join_users.warning_1_group",
+            {
+              count: catPermissions.members_count,
+              group: escapeExpression(allowedGroups[0]),
+            }
+          );
+          break;
+        case 2:
+          warning = I18n.t(
+            "chat.create_channel.auto_join_users.warning_2_groups",
+            {
+              count: catPermissions.members_count,
+              group1: escapeExpression(allowedGroups[0]),
+              group2: escapeExpression(allowedGroups[1]),
+            }
+          );
+          break;
+        default:
+          warning = I18n.messageFormat(
+            "chat.create_channel.auto_join_users.warning_multiple_groups_MF",
+            {
+              groupCount: allowedGroups.length - 1,
+              userCount: catPermissions.members_count,
+              groupName: escapeExpression(allowedGroups[0]),
+            }
+          );
+          break;
+      }
     } else {
       this.set(
         "autoJoinWarning",
@@ -125,6 +145,8 @@ export default class CreateChannelController extends Controller.extend(
         })
       );
     }
+
+    this.set("autoJoinWarning", warning);
   }
 
   _updatePermissionsHint(category) {
@@ -136,20 +158,42 @@ export default class CreateChannelController extends Controller.extend(
         .then((catPermissions) => {
           this._updateAutoJoinConfirmWarning(category, catPermissions);
           const allowedGroups = catPermissions.allowed_groups;
-          const translationKey =
-            allowedGroups.length < 3 ? "hint_groups" : "hint_multiple_groups";
+          const settingLink = `/c/${escapeExpression(fullSlug)}/edit/security`;
+          let hint;
 
-          this.set(
-            "categoryPermissionsHint",
-            htmlSafe(
-              I18n.t(`chat.create_channel.choose_category.${translationKey}`, {
-                link: `/c/${escapeExpression(fullSlug)}/edit/security`,
-                hint: escapeExpression(allowedGroups[0]),
-                hint_2: escapeExpression(allowedGroups[1]),
-                count: allowedGroups.length,
-              })
-            )
-          );
+          switch (allowedGroups.length) {
+            case 1:
+              hint = I18n.t(
+                "chat.create_channel.choose_category.hint_1_group",
+                {
+                  settingLink,
+                  group: escapeExpression(allowedGroups[0]),
+                }
+              );
+              break;
+            case 2:
+              hint = I18n.t(
+                "chat.create_channel.choose_category.hint_2_groups",
+                {
+                  settingLink,
+                  group1: escapeExpression(allowedGroups[0]),
+                  group2: escapeExpression(allowedGroups[1]),
+                }
+              );
+              break;
+            default:
+              hint = I18n.t(
+                "chat.create_channel.choose_category.hint_multiple_groups",
+                {
+                  settingLink,
+                  group: escapeExpression(allowedGroups[0]),
+                  count: allowedGroups.length - 1,
+                }
+              );
+              break;
+          }
+
+          this.set("categoryPermissionsHint", htmlSafe(hint));
         });
     } else {
       this.set("categoryPermissionsHint", DEFAULT_HINT);

plugins/chat/spec/system/create_channel_spec.rb
@@ -69,7 +69,7 @@ RSpec.describe "Create channel", type: :system, js: true do
       end
     end
 
-    context "when category has a malicous group name" do
+    context "when category has a malicious group name" do
       fab!(:group_1) do
         group = Group.new(name: "<script>e</script>")
         group.save(validate: false)

app/models/translation_override.rb
@@ -138,7 +138,8 @@ class TranslationOverride < ActiveRecord::Base
           :base,
           I18n.t(
             "activerecord.errors.models.translation_overrides.attributes.value.invalid_interpolation_keys",
-            keys: invalid_keys.join(", "),
+            keys: invalid_keys.join(I18n.t("word_connector.comma")),
+            count: invalid_keys.size,
           ),
         )
 

config/locales/client.en.yml
@@ -161,6 +161,9 @@ en:
       email: "Send via email"
       url: "Copy and share URL"
 
+    word_connector:
+      comma: ", "
+
     action_codes:
       public_topic: "Made this topic public %{when}"
       open_topic: "Converted this to a topic %{when}"

config/locales/server.en.yml
@@ -641,7 +641,9 @@ en:
         translation_overrides:
           attributes:
             value:
-              invalid_interpolation_keys: 'The following interpolation key(s) are invalid: "%{keys}"'
+              invalid_interpolation_keys:
+                one: 'The following interpolation key is invalid: %{keys}'
+                other: 'The following interpolation keys are invalid: %{keys}'
         watched_word:
           attributes:
             word:

plugins/chat/app/models/chat_channel.rb
@@ -75,10 +75,6 @@ class ChatChannel < ActiveRecord::Base
     Chat::ChatChannelMembershipManager.new(self).unfollow(user)
   end
 
-  def status_name
-    I18n.t("chat.channel.statuses.#{self.status}")
-  end
-
   def url
     "#{Discourse.base_url}/chat/c/#{self.slug || "-"}/#{self.id}"
   end

plugins/chat/app/models/chat_message.rb
@@ -52,7 +52,7 @@ class ChatMessage < ActiveRecord::Base
         :base,
         I18n.t(
           "chat.errors.minimum_length_not_met",
-          minimum: SiteSetting.chat_minimum_message_length,
+          count: SiteSetting.chat_minimum_message_length,
         ),
       )
     end
@@ -60,7 +60,7 @@ class ChatMessage < ActiveRecord::Base
     if message_too_long?
       self.errors.add(
         :base,
-        I18n.t("chat.errors.message_too_long", maximum: SiteSetting.chat_maximum_message_length),
+        I18n.t("chat.errors.message_too_long", count: SiteSetting.chat_maximum_message_length),
       )
     end
   end

plugins/chat/app/models/direct_message.rb
@@ -25,7 +25,7 @@ class DirectMessage < ActiveRecord::Base
 
     # direct message to self
     if users.empty?
-      return I18n.t("chat.channel.dm_title.single_user", user: "@#{acting_user.username}")
+      return I18n.t("chat.channel.dm_title.single_user", username: "@#{acting_user.username}")
     end
 
     # all users deleted
@@ -36,13 +36,16 @@ class DirectMessage < ActiveRecord::Base
       return(
         I18n.t(
           "chat.channel.dm_title.multi_user_truncated",
-          users: usernames_formatted[0..4].join(", "),
-          leftover: usernames_formatted.length - 5,
+          comma_separated_usernames: usernames_formatted[0..4].join(I18n.t("word_connector.comma")),
+          count: usernames_formatted.length - 5,
         )
       )
     end
 
-    I18n.t("chat.channel.dm_title.multi_user", users: usernames_formatted.join(", "))
+    I18n.t(
+      "chat.channel.dm_title.multi_user",
+      comma_separated_usernames: usernames_formatted.join(I18n.t("word_connector.comma")),
+    )
   end
 end
 

commit d94e2912f9e2dcb4ac27522ebadfebc120b87738

FIX: Blank dialog when auto-join selected in channel creation modal (#20405)
Fixes issue introduced in cee8b7319675cdbd8f9c4707519a6615ac5e7c76
where the correct warning message was not shown when enabling auto-join
for public categories when creating a channel. Adds more system specs
as well to avoid regressions.

plugins/chat/assets/javascripts/discourse/controllers/create-channel.js
@@ -138,11 +138,11 @@ export default class CreateChannelController extends Controller.extend(
           break;
       }
     } else {
-      this.set(
-        "autoJoinWarning",
-        I18n.t(`chat.create_channel.auto_join_users.public_category_warning`, {
+      warning = I18n.t(
+        "chat.create_channel.auto_join_users.public_category_warning",
+        {
           category: escapeExpression(category.name),
-        })
+        }
       );
     }
 

plugins/chat/spec/system/create_channel_spec.rb
@@ -123,6 +123,112 @@ RSpec.describe "Create channel", type: :system, js: true do
     end
 
     context "when saving" do
+      context "when user has chosen to automatically add users" do
+        let(:dialog) { PageObjects::Components::Dialog.new }
+        let(:name) { "Cats & Dogs" }
+
+        before do
+          chat_page.visit_browse
+          chat_page.new_channel_button.click
+          channel_modal.fill_name(name)
+        end
+
+        context "for a public category" do
+          before do
+            channel_modal.select_category(category_1)
+            find(".auto-join-channel__label").click
+            channel_modal.click_primary_button
+          end
+
+          it "displays the correct warning" do
+            expect(dialog).to have_content(
+              I18n.t(
+                "js.chat.create_channel.auto_join_users.public_category_warning",
+                category: category_1.name,
+              ),
+            )
+          end
+
+          it "allows the user to proceed with channel creation" do
+            dialog.click_yes
+            expect(page).to have_content(name)
+            created_channel = ChatChannel.find_by(chatable_id: category_1.id)
+            expect(page).to have_current_path(
+              chat.channel_path(created_channel.slug, created_channel.id),
+            )
+          end
+
+          it "does nothing if no is clicked" do
+            dialog.click_no
+            expect(page).to have_css(".create-channel-modal")
+            expect(ChatChannel.exists?(chatable_id: category_1.id)).to eq(false)
+          end
+        end
+
+        context "for a private category" do
+          fab!(:group_1) { Fabricate(:group) }
+          fab!(:user_1) { Fabricate(:user) }
+          fab!(:private_category) { Fabricate(:private_category, group: group_1) }
+
+          before do
+            group_1.add(user_1)
+            channel_modal.select_category(private_category)
+            find(".auto-join-channel__label").click
+            channel_modal.click_primary_button
+          end
+
+          context "when only 1 group can access the category" do
+            it "displays the correct warning" do
+              expect(dialog).to have_content(
+                I18n.t(
+                  "js.chat.create_channel.auto_join_users.warning_1_group",
+                  count: 1,
+                  group: "@#{group_1.name}",
+                ),
+              )
+            end
+          end
+
+          context "when 2 groups can access the category" do
+            fab!(:group_2) { Fabricate(:group) }
+            fab!(:category_group_2) do
+              CategoryGroup.create(group: group_2, category: private_category)
+            end
+
+            it "displays the correct warning" do
+              expect(dialog).to have_content(
+                I18n.t(
+                  "js.chat.create_channel.auto_join_users.warning_2_groups",
+                  count: 1,
+                  group1: "@#{group_1.name}",
+                  group2: "@#{group_2.name}",
+                ),
+              )
+            end
+          end
+
+          context "when > 2 groups can access the category" do
+            fab!(:group_2) { Fabricate(:group) }
+            fab!(:category_group_2) do
+              CategoryGroup.create(group: group_2, category: private_category)
+            end
+
+            fab!(:group_3) { Fabricate(:group) }
+            fab!(:category_group_3) do
+              CategoryGroup.create(group: group_3, category: private_category)
+            end
+
+            it "displays the correct warning" do
+              # NOTE: This has to be hardcoded because the I18n module in ruby
+              # does not support messageFormat.
+              expect(dialog).to have_content(
+                "Automatically add 1 user from @#{group_1.name} and 2 other groups?",
+              )
+            end
+          end
+        end
+      end
+
       context "when error" do
         it "displays the error" do
           existing_channel = Fabricate(:chat_channel)

spec/system/page_objects/components/dialog.rb
@@ -1,0 +1,19 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Components
+    class Dialog < PageObjects::Components::Base
+      def has_content?(content)
+        find(".dialog-container").has_content?(content)
+      end
+
+      def click_yes
+        find(".dialog-footer .btn-primary").click
+      end
+
+      def click_no
+        find(".dialog-footer .btn-default").click
+      end
+    end
+  end
+end
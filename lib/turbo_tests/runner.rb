hanzoai/forum

commit 3f4a3775918d7fffa78b706c20fb659477d54a63

DEV: Minimal first pass of rails system test setup (#16311)
This commit introduces rails system tests run with chromedriver, selenium,
and headless chrome to our testing toolbox.

We use the `webdrivers` gem and `selenium-webdriver` which is what
the latest Rails uses so the tests run locally and in CI out of the box.

You can use `SELENIUM_VERBOSE_DRIVER_LOGS=1` to show extra
verbose logs of what selenium is doing to communicate with the system
tests.

By default JS logs are verbose so errors from JS are shown when
running system tests, you can disable this with
`SELENIUM_DISABLE_VERBOSE_JS_LOGS=1`

You can use `SELENIUM_HEADLESS=0` to run the system
tests inside a chrome browser instead of headless, which can be useful to debug things
and see what the spec sees. See note above about `bin/ember-cli` to avoid
surprises.

I have modified `bin/turbo_rspec` to exclude `spec/system` by default,
support for parallel system specs is a little shaky right now and we don't
want them slowing down the turbo by default either.

### PageObjects and System Tests

To make querying and inspecting parts of the page easier
and more reusable inbetween system tests, we are using the
concept of [PageObjects](https://www.selenium.dev/documentation/test_practices/encouraged/page_object_models/) in
our system tests. A "Page" here is generally corresponds to
an overarching ember route, e.g. "Topic" for `/t/324345/some-topic`,
and this contains logic for querying components within the topic
such as "Posts".

I have also split "Modals" into their own entity. Further down the
line we may want to explore creating independent "Component"
contexts.

Capybara DSL should be included in each PageObject class,
reference for this can be found at https://rubydoc.info/github/teamcapybara/capybara/master#the-dsl

For system tests, since they are so slow, we want to focus on
the "happy path" and not do every different possible context

bin/turbo_rspec
@@ -59,10 +59,18 @@ formatters.each do |formatter|
   end
 end
 
+# We do not want to include system specs by default, they are super
+# slow and require a selenium server to be running.
+default_spec_dirs = Dir.entries("#{Rails.root}/spec").reject do |entry|
+  !File.directory?("spec/#{entry}") || ["..", ".", "system"].include?(entry)
+end.map { |entry| "spec/#{entry}" }
+files = ARGV.empty? ? default_spec_dirs : ARGV
+
+puts "Running turbo_rspec using files in #{files}"
 success =
   TurboTests::Runner.run(
     formatters: formatters,
-    files: ARGV.empty? ? ["spec"] : ARGV,
+    files: files,
     verbose: verbose,
     fail_fast: fail_fast
   )

.github/workflows/tests.yml
@@ -19,7 +19,7 @@ jobs:
   build:
     name: ${{ matrix.target }} ${{ matrix.build_type }}
     runs-on: ${{ (matrix.build_type == 'annotations') && 'ubuntu-latest' || 'ubuntu-20.04-8core' }}
-    container: discourse/discourse_test:slim${{ startsWith(matrix.build_type, 'frontend') && '-browsers' || '' }}
+    container: discourse/discourse_test:slim${{ (matrix.build_type == 'frontend' || matrix.build_type == 'system') && '-browsers' || '' }}
     timeout-minutes: 20
 
     env:
@@ -34,13 +34,15 @@ jobs:
       fail-fast: false
 
       matrix:
-        build_type: [backend, frontend, annotations]
+        build_type: [backend, frontend, system, annotations]
         target: [core, plugins]
         exclude:
           - build_type: annotations
             target: plugins
           - build_type: frontend
             target: core # Handled by core_frontend_tests job (below)
+          - build_type: system
+            target: plugins # Enable once at least 1 plugin has system tests
 
     steps:
       - uses: actions/checkout@v3
@@ -165,6 +167,25 @@ jobs:
         if: matrix.build_type == 'frontend' && matrix.target == 'plugins'
         run: QUNIT_PARALLEL=3 bin/rake plugin:qunit['*','1200000']
         timeout-minutes: 30
+
+      - name: Ember Build for System Tests
+        if: matrix.build_type == 'system'
+        run: bin/ember-cli --build
+
+      - name: Core System Tests
+        if: matrix.build_type == 'system' && matrix.target == 'core'
+        run: bin/system_rspec
+
+      - name: Plugin System Tests
+        if: matrix.build_type == 'system' && matrix.target == 'plugins'
+        run: bin/system_rspec plugins/*/spec/system
+
+      - name: Upload failed system test screenshots
+        uses: actions/upload-artifact@v3
+        if: matrix.build_type == 'system' && failure()
+        with:
+          name: failed-system-test-screenshots
+          path: tmp/screenshots/*.png
 
       - name: Check Annotations
         if: matrix.build_type == 'annotations'

Gemfile
@@ -149,11 +149,14 @@ group :assets do
 end
 
 group :test do
+  gem 'capybara', require: false
   gem 'webmock', require: false
   gem 'fakeweb', require: false
   gem 'minitest', require: false
   gem 'simplecov', require: false
+  gem 'selenium-webdriver', require: false
   gem "test-prof"
+  gem 'webdrivers', require: false
 end
 
 group :test, :development do

bin/ember-cli
@@ -7,7 +7,7 @@ RAILS_ROOT = File.expand_path("../../", Pathname.new(__FILE__).realpath)
 PORT = ENV["UNICORN_PORT"] ||= "3000"
 HOSTNAME = ENV["DISCOURSE_HOSTNAME"] ||= "127.0.0.1"
 YARN_DIR = File.join(RAILS_ROOT, "app/assets/javascripts/discourse")
-CUSTOM_ARGS = ["--try", "--test", "--unicorn", "-u", "--forward-host"]
+CUSTOM_ARGS = ["--try", "--test", "--build", "--unicorn", "-u", "--forward-host"]
 PROXY =
   if ARGV.include?("--try")
     "https://try.discourse.org"
@@ -24,6 +24,8 @@ end
 command =
   if ARGV.include?("--test")
     "test"
+  elsif ARGV.include?("--build")
+    "build"
   else
     "server"
   end
@@ -49,7 +51,7 @@ end
 
 args = ["-s", "--cwd", YARN_DIR, "run", "ember", command] + (ARGV - CUSTOM_ARGS)
 
-if !args.include?("test") && !args.include?("--proxy")
+if !args.include?("test") && !args.include?("build") && !args.include?("--proxy")
   args << "--proxy"
   args << PROXY
 end

bin/system_rspec
@@ -1,0 +1,7 @@
+#!/bin/bash
+
+if [[ -z "$@" ]]; then
+  bin/rspec spec/system
+elif [[ -n "$@" ]]; then
+  bin/rspec "$@"
+fi

spec/rails_helper.rb
@@ -57,6 +57,9 @@ require 'shoulda-matchers'
 require 'sidekiq/testing'
 require 'test_prof/recipes/rspec/let_it_be'
 require 'test_prof/before_all/adapters/active_record'
+require 'webdrivers'
+require 'selenium-webdriver'
+require 'capybara/rails'
 
 # The shoulda-matchers gem no longer detects the test framework
 # you're using or mixes itself into that framework automatically.
@@ -71,6 +74,11 @@ end
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+require Rails.root.join("spec/system/page_objects/pages/base.rb")
+require Rails.root.join("spec/system/page_objects/modals/base.rb")
+Dir[Rails.root.join("spec/system/page_objects/**/*.rb")].each { |f| require f }
+
 Dir[Rails.root.join("spec/fabricators/*.rb")].each { |f| require f }
 require_relative './helpers/redis_snapshot_helper'
 
@@ -81,6 +89,10 @@ if ENV['LOAD_PLUGINS'] == "1"
   end
 
   Dir[Rails.root.join("plugins/*/spec/fabricators/**/*.rb")].each do |f|
+    require f
+  end
+
+  Dir[Rails.root.join("plugins/*/spec/system/page_objects/**/*.rb")].each do |f|
     require f
   end
 end
@@ -177,6 +189,7 @@ RSpec.configure do |config|
   config.include MessageBus
   config.include RSpecHtmlMatchers
   config.include IntegrationHelpers, type: :request
+  config.include SystemHelpers, type: :system
   config.include WebauthnIntegrationHelpers
   config.include SiteSettingsHelpers
   config.include SidekiqHelpers
@@ -228,7 +241,41 @@ RSpec.configure do |config|
 
     SiteSetting.provider = TestLocalProcessProvider.new
 
-    WebMock.disable_net_connect!
+    WebMock.disable_net_connect!(
+      allow_localhost: true,
+      allow: [Webdrivers::Chromedriver.base_url]
+    )
+
+    Capybara.configure do |capybara_config|
+      capybara_config.server_host = "localhost"
+      capybara_config.server_port = 31337
+    end
+
+    chrome_browser_options = Selenium::WebDriver::Chrome::Options.new(
+      logging_prefs: { "browser" => "ALL", "driver" => "ALL" }
+    ).tap do |options|
+      options.add_argument("--window-size=1400,1400")
+      options.add_argument("--no-sandbox")
+      options.add_argument("--disable-dev-shm-usage")
+    end
+
+    Capybara.register_driver :selenium_chrome do |app|
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :chrome,
+        capabilities: chrome_browser_options,
+      )
+    end
+
+    Capybara.register_driver :selenium_chrome_headless do |app|
+      chrome_browser_options.add_argument("--headless")
+
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :chrome,
+        capabilities: chrome_browser_options,
+      )
+    end
 
     if ENV['ELEVATED_UPLOADS_ID']
       DB.exec "SELECT setval('uploads_id_seq', 10000)"
@@ -246,14 +293,14 @@ RSpec.configure do |config|
     end
   end
 
-  config.after :each do |x|
-    if x.exception && ex = RspecErrorTracker.last_exception
+  config.after :each do |example|
+    if example.exception && ex = RspecErrorTracker.last_exception
       # magic in a cause if we have none
-      unless x.exception.cause
-        class << x.exception
+      unless example.exception.cause
+        class << example.exception
           attr_accessor :cause
         end
-        x.exception.cause = ex
+        example.exception.cause = ex
       end
     end
 
@@ -286,8 +333,53 @@ RSpec.configure do |config|
   end
 
   # Match the request hostname to the value in `database.yml`
-  config.before(:all, type: [:request, :multisite]) { host! "test.localhost" }
-  config.before(:each, type: [:request, :multisite]) { host! "test.localhost" }
+  config.before(:all, type: [:request, :multisite, :system]) { host! "test.localhost" }
+  config.before(:each, type: [:request, :multisite, :system]) { host! "test.localhost" }
+
+  last_driven_by = nil
+  config.before(:each, type: :system) do |example|
+    if example.metadata[:js]
+      driver = "selenium_chrome"
+      driver += "_headless" unless ENV["SELENIUM_HEADLESS"] == "0"
+      driven_by driver.to_sym
+    end
+    setup_system_test
+  end
+
+  config.after(:each, type: :system) do |example|
+    # This is disabled by default because it is super verbose,
+    # if you really need to dig into how selenium is communicating
+    # for system tests then enable it.
+    if ENV["SELENIUM_VERBOSE_DRIVER_LOGS"]
+      puts "~~~~~~ DRIVER LOGS: ~~~~~~~"
+      page.driver.browser.logs.get(:driver).each do |log|
+        puts log.message
+      end
+    end
+
+    # Recommended that this is not disabled, since it makes debugging
+    # failed system tests a lot trickier.
+    if ENV["SELENIUM_DISABLE_VERBOSE_JS_LOGS"].blank?
+      if example.exception
+        skip_js_errors = false
+        if example.exception.kind_of?(RSpec::Core::MultipleExceptionError)
+          puts "~~~~~~ SYSTEM TEST ERRORS: ~~~~~~~"
+          example.exception.all_exceptions.each do |ex|
+            puts ex.message
+          end
+
+          skip_js_errors = true
+        end
+
+        if !skip_js_errors
+          puts "~~~~~~ JS ERRORS: ~~~~~~~"
+          page.driver.browser.logs.get(:browser).each do |log|
+            puts log.message
+          end
+        end
+      end
+    end
+  end
 
   config.before(:each, type: :multisite) do
     Rails.configuration.multisite = true # rubocop:disable Discourse/NoDirectMultisiteManipulation

spec/support/system_helpers.rb
@@ -1,0 +1,27 @@
+# frozen_string_literal: true
+
+module SystemHelpers
+  def sign_in(user)
+    visit "/session/#{user.encoded_username}/become"
+  end
+
+  def setup_system_test
+    SiteSetting.login_required = false
+    SiteSetting.content_security_policy = false
+    SiteSetting.force_hostname = "#{Capybara.server_host}:#{Capybara.server_port}"
+    SiteSetting.external_system_avatars_enabled = false
+  end
+
+  def try_until_success(timeout: 2, frequency: 0.01)
+    start ||= Time.zone.now
+    backoff ||= frequency
+    yield
+  rescue RSpec::Expectations::ExpectationNotMetError
+    if Time.zone.now >= start + timeout.seconds
+      raise
+    end
+    sleep backoff
+    backoff += frequency
+    retry
+  end
+end

spec/system/bookmarks_spec.rb
@@ -1,0 +1,67 @@
+# frozen_string_literal: true
+
+describe "Bookmarking posts and topics", type: :system, js: true do
+  fab!(:topic) { Fabricate(:topic) }
+  fab!(:user) { Fabricate(:user, username: "bookmarkguy") }
+  fab!(:post) { Fabricate(:post, topic: topic, raw: "This is some post to bookmark") }
+  fab!(:post2) { Fabricate(:post, topic: topic, raw: "Some interesting post content") }
+
+  it "allows logged in user to create bookmarks with and without reminders" do
+    sign_in user
+    visit "/t/#{topic.id}"
+    topic_page = PageObjects::Pages::Topic.new
+    expect(topic_page).to have_post_content(post)
+    topic_page.expand_post_actions(post)
+    topic_page.click_post_action_button(post, :bookmark)
+
+    bookmark_modal = PageObjects::Modals::Bookmark.new
+    bookmark_modal.fill_name("something important")
+    bookmark_modal.save
+
+    expect(topic_page).to have_post_bookmarked(post)
+    bookmark = Bookmark.find_by(bookmarkable: post, user: user)
+    expect(bookmark.name).to eq("something important")
+
+    topic_page.expand_post_actions(post2)
+    topic_page.click_post_action_button(post2, :bookmark)
+
+    bookmark_modal = PageObjects::Modals::Bookmark.new
+    bookmark_modal.select_preset_reminder(:tomorrow)
+    expect(topic_page).to have_post_bookmarked(post2)
+    bookmark = Bookmark.find_by(bookmarkable: post2, user: user)
+    expect(bookmark.reminder_at).not_to eq(nil)
+    expect(bookmark.reminder_set_at).not_to eq(nil)
+  end
+
+  it "does not create a bookmark if the modal is closed with the cancel button" do
+    sign_in user
+    visit "/t/#{topic.id}"
+    topic_page = PageObjects::Pages::Topic.new
+    topic_page.expand_post_actions(post)
+    topic_page.click_post_action_button(post, :bookmark)
+
+    bookmark_modal = PageObjects::Modals::Bookmark.new
+    bookmark_modal.fill_name("something important")
+    bookmark_modal.cancel
+
+    expect(topic_page).not_to have_post_bookmarked(post)
+    expect(Bookmark.exists?(bookmarkable: post, user: user)).to eq(false)
+  end
+
+  it "allows the topic to be bookmarked" do
+    sign_in user
+    visit "/t/#{topic.id}"
+    topic_page = PageObjects::Pages::Topic.new
+    topic_page.click_topic_footer_button(:bookmark)
+
+    bookmark_modal = PageObjects::Modals::Bookmark.new
+    bookmark_modal.fill_name("something important")
+    bookmark_modal.save
+
+    expect(topic_page).to have_topic_bookmarked
+    bookmark = try_until_success do
+      expect(Bookmark.exists?(bookmarkable: topic, user: user)).to eq(true)
+    end
+    expect(bookmark).not_to eq(nil)
+  end
+end

commit 59e9db395b0398589f07a837790fbf501741aa71

DEV: Fix bin/turbo_rspec runtime recording (#20407)
This commit 3f4a3775918d7fffa78b706c20fb659477d54a63 broke
`bin/turbo_rspec` timing recording via `TurboTests::Runner`,
because we changed to using all `spec/*` folders except
`spec/system` as default for the runner, rather than
the old `['spec']` array, which is what `TurboTests::Runner`
was relying on to determine whether to record test run
time with `ParallelTests::RSpec::RuntimeLogger`.

Instead, we can just pass a new `use_runtime_info` boolean to the
runner class and use it when running against the default set of
spec files using `bin/turbo_rspec` and the turbo rspec rake task.

bin/turbo_rspec
@@ -1,70 +1,56 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 
-require './lib/turbo_tests'
-require 'optparse'
+require "./lib/turbo_tests"
+require "optparse"
 
 requires = []
 formatters = []
 verbose = false
 fail_fast = nil
 
-OptionParser.new do |opts|
-  opts.on("-r", "--require PATH", "Require a file.") do |filename|
-    requires << filename
-  end
+OptionParser
+  .new do |opts|
+    opts.on("-r", "--require PATH", "Require a file.") { |filename| requires << filename }
 
-  opts.on("-f", "--format FORMATTER", "Choose a formatter.") do |name|
-    formatters << {
-      name: name,
-      outputs: []
-    }
-  end
-
-  opts.on("-o", "--out FILE", "Write output to a file instead of $stdout") do |filename|
-    if formatters.empty?
-      formatters << {
-        name: "progress",
-        outputs: []
-      }
+    opts.on("-f", "--format FORMATTER", "Choose a formatter.") do |name|
+      formatters << { name: name, outputs: [] }
     end
-    formatters.last[:outputs] << filename
-  end
 
-  opts.on("-v", "--verbose", "More output") do
-    verbose = true
-  end
+    opts.on("-o", "--out FILE", "Write output to a file instead of $stdout") do |filename|
+      formatters << { name: "progress", outputs: [] } if formatters.empty?
+      formatters.last[:outputs] << filename
+    end
 
-  opts.on("--fail-fast=[N]") do |n|
-    n = Integer(n) rescue nil
-    fail_fast = (n.nil? || n < 1) ? 1 : n
-  end
+    opts.on("-v", "--verbose", "More output") { verbose = true }
 
-end.parse!(ARGV)
+    opts.on("--fail-fast=[N]") do |n|
+      n =
+        begin
+          Integer(n)
+        rescue StandardError
+          nil
+        end
+      fail_fast = (n.nil? || n < 1) ? 1 : n
+    end
+  end
+  .parse!(ARGV)
 
 requires.each { |f| require(f) }
 
-if formatters.empty?
-  formatters << {
-    name: "progress",
-    outputs: []
-  }
-end
+formatters << { name: "progress", outputs: [] } if formatters.empty?
 
-formatters.each do |formatter|
-  if formatter[:outputs].empty?
-    formatter[:outputs] << '-'
-  end
-end
+formatters.each { |formatter| formatter[:outputs] << "-" if formatter[:outputs].empty? }
 
-# We do not want to include system specs by default, they are super
-# slow and require a selenium server to be running.
-default_spec_dirs = Dir.entries("#{Rails.root}/spec").reject do |entry|
-  !File.directory?("spec/#{entry}") || ["..", ".", "system"].include?(entry)
-end.map { |entry| "spec/#{entry}" }
-files = ARGV.empty? ? default_spec_dirs : ARGV
+if ARGV.empty?
+  files = TurboTests::Runner.default_spec_folders
+  use_runtime_info = true
+else
+  files = ARGV
+  use_runtime_info = false
+end
 
 puts "Running turbo_rspec using files in #{files}"
 success =
@@ -72,7 +58,8 @@ success =
     formatters: formatters,
     files: files,
     verbose: verbose,
-    fail_fast: fail_fast
+    fail_fast: fail_fast,
+    use_runtime_info: use_runtime_info,
   )
 
 if success

lib/tasks/turbo.rake
@@ -3,5 +3,9 @@
 task "turbo:spec" => :test do |t|
   require "./lib/turbo_tests"
 
-  TurboTests::Runner.run(formatters: [{ name: "progress", outputs: ["-"] }], files: ["spec"])
+  TurboTests::Runner.run(
+    formatters: [{ name: "progress", outputs: ["-"] }],
+    files: TurboTests::Runner.default_spec_folders,
+    use_runtime_info: true,
+  )
 end

lib/turbo_tests/runner.rb
@@ -8,12 +8,27 @@ module TurboTests
       start_time = opts.fetch(:start_time) { Time.now }
       verbose = opts.fetch(:verbose, false)
       fail_fast = opts.fetch(:fail_fast, nil)
+      use_runtime_info = opts.fetch(:use_runtime_info, false)
 
       STDERR.puts "VERBOSE" if verbose
 
       reporter = Reporter.from_config(formatters, start_time)
 
-      new(reporter: reporter, files: files, verbose: verbose, fail_fast: fail_fast).run
+      new(
+        reporter: reporter,
+        files: files,
+        verbose: verbose,
+        fail_fast: fail_fast,
+        use_runtime_info: use_runtime_info,
+      ).run
+    end
+
+    def self.default_spec_folders
+      # We do not want to include system specs by default, they are quite slow.
+      Dir
+        .entries("#{Rails.root}/spec")
+        .reject { |entry| !File.directory?("spec/#{entry}") || %w[.. . system].include?(entry) }
+        .map { |entry| "spec/#{entry}" }
     end
 
     def initialize(opts)
@@ -21,6 +36,7 @@ module TurboTests
       @files = opts[:files]
       @verbose = opts[:verbose]
       @fail_fast = opts[:fail_fast]
+      @use_runtime_info = opts[:use_runtime_info]
       @failure_count = 0
 
       @messages = Queue.new
@@ -32,11 +48,10 @@ module TurboTests
       check_for_migrations
 
       @num_processes = ParallelTests.determine_number_of_processes(nil)
-      use_runtime_info = @files == ["spec"]
 
       group_opts = {}
 
-      if use_runtime_info
+      if @use_runtime_info
         group_opts[:runtime_log] = "tmp/turbo_rspec_runtime.log"
       else
         group_opts[:group_by] = :filesize
@@ -47,7 +62,7 @@ module TurboTests
 
       setup_tmp_dir
 
-      subprocess_opts = { record_runtime: use_runtime_info }
+      subprocess_opts = { record_runtime: @use_runtime_info }
 
       start_multisite_subprocess(@files, **subprocess_opts)
 
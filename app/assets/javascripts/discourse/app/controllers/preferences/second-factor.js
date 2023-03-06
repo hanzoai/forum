hanzoai/forum

commit 817b40bf1a8ca259812e89e662597f994362e6d0

FEATURE: better UI to manage 2fa (#19338)
In this PR, we introduced an option, that when all authenticators are disabled, but backup codes still exists, user can authenticate with those backup codes. This was reverted as this is not expected behavior.

https://github.com/discourse/discourse/pull/18982

Instead, when the last authenticator is deleted, backup codes should be deleted as well. Because this disables 2fa, user is asked to confirm that action by typing text.

In addition, UI for 2fa preferences was refreshed.

app/assets/javascripts/discourse/app/controllers/preferences/second-factor.js
@@ -10,6 +10,7 @@ import { findAll } from "discourse/models/login-method";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import showModal from "discourse/lib/show-modal";
 import { inject as service } from "@ember/service";
+import { htmlSafe } from "@ember/template";
 
 export default Controller.extend(CanCheckEmails, {
   dialog: service(),
@@ -113,6 +114,29 @@ export default Controller.extend(CanCheckEmails, {
       .finally(() => this.set("resetPasswordLoading", false));
   },
 
+  disableAllMessage() {
+    let templateElements = [I18n.t("user.second_factor.delete_confirm_header")];
+    templateElements.push("<ul>");
+    this.totps.forEach((totp) => {
+      templateElements.push(`<li>${totp.name}</li>`);
+    });
+    this.security_keys.forEach((key) => {
+      templateElements.push(`<li>${key.name}</li>`);
+    });
+    if (this.currentUser.second_factor_backup_enabled) {
+      templateElements.push(
+        `<li>${I18n.t("user.second_factor_backup.title")}</li>`
+      );
+    }
+    templateElements.push("</ul>");
+    templateElements.push(
+      I18n.t("user.second_factor.delete_confirm_instruction", {
+        confirm: I18n.t("user.second_factor.disable"),
+      })
+    );
+    return htmlSafe(templateElements.join(""));
+  },
+
   actions: {
     confirmPassword() {
       if (!this.password) {
@@ -130,8 +154,11 @@ export default Controller.extend(CanCheckEmails, {
 
       this.dialog.deleteConfirm({
         title: I18n.t("user.second_factor.disable_confirm"),
+        message: this.disableAllMessage(),
         confirmButtonLabel: "user.second_factor.disable",
+        confirmPhrase: I18n.t("user.second_factor.disable"),
         confirmButtonIcon: "ban",
+        cancelButtonClass: "btn-flat",
         didConfirm: () => {
           this.model
             .disableAllSecondFactors()
@@ -140,6 +167,88 @@ export default Controller.extend(CanCheckEmails, {
               DiscourseURL.redirectTo(userPath(`${usernameLower}/preferences`));
             })
             .catch((e) => this.handleError(e))
+            .finally(() => this.set("loading", false));
+        },
+      });
+    },
+    disableSingleSecondFactor(secondFactorMethod) {
+      if (this.totps.concat(this.security_keys).length === 1) {
+        this.send("disableAllSecondFactors");
+        return;
+      }
+      this.dialog.deleteConfirm({
+        title: I18n.t("user.second_factor.delete_single_confirm_title"),
+        message: I18n.t("user.second_factor.delete_single_confirm_message", {
+          name: secondFactorMethod.name,
+        }),
+        confirmButtonLabel: "user.second_factor.delete",
+        confirmButtonIcon: "ban",
+        cancelButtonClass: "btn-flat",
+        didConfirm: () => {
+          this.currentUser
+            .updateSecondFactor(
+              secondFactorMethod.id,
+              secondFactorMethod.name,
+              true,
+              secondFactorMethod.method
+            )
+            .then((response) => {
+              if (response.error) {
+                return;
+              }
+              this.markDirty();
+            })
+            .catch((e) => this.handleError(e))
+            .finally(() => {
+              this.setProperties({
+                totps: this.totps.filter(
+                  (totp) =>
+                    totp.id !== secondFactorMethod.id ||
+                    totp.method !== secondFactorMethod.method
+                ),
+                security_keys: this.security_keys.filter(
+                  (key) =>
+                    key.id !== secondFactorMethod.id ||
+                    key.method !== secondFactorMethod.method
+                ),
+              });
+
+              this.set("loading", false);
+            });
+        },
+      });
+    },
+
+    disableSecondFactorBackup() {
+      this.dialog.deleteConfirm({
+        title: I18n.t("user.second_factor.delete_backup_codes_confirm_title"),
+        message: I18n.t(
+          "user.second_factor.delete_backup_codes_confirm_message"
+        ),
+        confirmButtonLabel: "user.second_factor.delete",
+        confirmButtonIcon: "ban",
+        cancelButtonClass: "btn-flat",
+        didConfirm: () => {
+          this.set("backupCodes", []);
+          this.set("loading", true);
+
+          this.model
+            .updateSecondFactor(0, "", true, SECOND_FACTOR_METHODS.BACKUP_CODE)
+            .then((response) => {
+              if (response.error) {
+                this.set("errorMessage", response.error);
+                return;
+              }
+
+              this.set("errorMessage", null);
+              this.model.set("second_factor_backup_enabled", false);
+              this.markDirty();
+              this.send("closeModal");
+            })
+            .catch((error) => {
+              this.send("closeModal");
+              this.onError(error);
+            })
             .finally(() => this.set("loading", false));
         },
       });

app/assets/javascripts/discourse/app/templates/preferences-second-factor.hbs
@@ -30,59 +30,79 @@
           <div class="control-group totp">
             <div class="controls">
               <h2>{{i18n "user.second_factor.totp.title"}}</h2>
-              <DButton @action={{action "createTotp"}} @class="btn-primary new-totp" @icon="plus" @disabled={{this.loading}} @label="user.second_factor.totp.add" />
               {{#each this.totps as |totp|}}
-                <div class="second-factor-item">
-                  {{#if totp.name}}
-                    {{totp.name}}
-                  {{else}}
-                    {{i18n "user.second_factor.totp.default_name"}}
-                  {{/if}}
+                <div class="second-factor-item row">
+                  <div class="details">
+                    {{#if totp.name}}
+                      {{totp.name}}
+                    {{else}}
+                      {{i18n "user.second_factor.totp.default_name"}}
+                    {{/if}}
+                  </div>
 
                   {{#if this.isCurrentUser}}
-                    <DButton @action={{action "editSecondFactor" totp}} @class="btn-default btn-small btn-icon pad-left no-text edit" @disabled={{this.loading}} @icon="pencil-alt" @aria-label="user.second_factor.edit" @title="user.second_factor.edit" />
+                    <div class="actions">
+                      <DButton @action={{action "editSecondFactor" totp}} @class="btn-default btn-flat btn-small btn-icon pad-left no-text edit" @disabled={{this.loading}} @icon="pencil-alt" @aria-label="user.second_factor.edit" @title="user.second_factor.edit" />
+                      <DButton @action={{action "disableSingleSecondFactor" totp}} @class="btn-danger btn-flat no-text" @icon="trash-alt" @aria-label="user.second_factor.disable" @title="user.second_factor.disable" />
+                    </div>
                   {{/if}}
                 </div>
               {{/each}}
+              <DButton @action={{action "createTotp"}} @class="btn-primary new-totp" @icon="plus" @disabled={{this.loading}} @label="user.second_factor.totp.add" />
             </div>
           </div>
 
           <div class="control-group security-key">
             <div class="controls">
               <h2>{{i18n "user.second_factor.security_key.title"}}</h2>
-              <DButton @action={{action "createSecurityKey"}} @class="btn-primary new-security-key" @icon="plus" @disabled={{this.loading}} @label="user.second_factor.security_key.add" />
               {{#each this.security_keys as |security_key|}}
-                <div class="second-factor-item">
-                  {{#if security_key.name}}
-                    {{security_key.name}}
-                  {{else}}
-                    {{i18n "user.second_factor.security_key.default_name"}}
-                  {{/if}}
+                <div class="second-factor-item row">
+                  <div class="details">
+                    {{#if security_key.name}}
+                      {{security_key.name}}
+                    {{else}}
+                      {{i18n "user.second_factor.security_key.default_name"}}
+                    {{/if}}
+                  </div>
 
                   {{#if this.isCurrentUser}}
-                    <DButton @action={{action "editSecurityKey" security_key}} @class="btn-default btn-small btn-icon pad-left no-text edit" @disabled={{this.loading}} @icon="pencil-alt" @aria-label="user.second_factor.edit" @title="user.second_factor.edit" />
+                    <div class="actions">
+                      <DButton @action={{action "editSecurityKey" security_key}} @class="btn-default btn-flat btn-small btn-icon pad-left no-text edit" @disabled={{this.loading}} @icon="pencil-alt" @aria-label="user.second_factor.edit" @title="user.second_factor.edit" />
+                      <DButton @action={{action "disableSingleSecondFactor" security_key}} @class="btn-danger btn-flat no-text" @icon="trash-alt" @aria-label="user.second_factor.disable" @title="user.second_factor.disable" />
+                    </div>
                   {{/if}}
                 </div>
               {{/each}}
+              <DButton @action={{action "createSecurityKey"}} @class="btn-primary new-security-key" @icon="plus" @disabled={{this.loading}} @label="user.second_factor.security_key.add" />
             </div>
           </div>
 
           <div class="control-group pref-second-factor-backup">
             <div class="controls pref-second-factor-backup">
               <h2>{{i18n "user.second_factor_backup.title"}}</h2>
-              {{#if this.model.second_factor_enabled}}
-                {{#if this.model.second_factor_backup_enabled}}
-                  {{html-safe (i18n "user.second_factor_backup.manage" count=this.model.second_factor_remaining_backup_codes)}}
-                {{else}}
-                  {{i18n "user.second_factor_backup.enable_long"}}
-                {{/if}}
+              <div class="second-factor-item row">
+                {{#if this.model.second_factor_enabled}}
+                  <div class="details">
+                    {{#if this.model.second_factor_backup_enabled}}
+                      {{html-safe (i18n "user.second_factor_backup.manage" count=this.model.second_factor_remaining_backup_codes)}}
+                    {{else}}
+                      {{i18n "user.second_factor_backup.enable_long"}}
+                    {{/if}}
+                  </div>
 
-                {{#if this.isCurrentUser}}
-                  <DButton @action={{action "editSecondFactorBackup"}} @class="btn-default btn-small btn-icon pad-left no-text edit edit-2fa-backup" @disabled={{this.loading}} @icon="pencil-alt" @aria-label="user.second_factor.edit" @title="user.second_factor.edit" />
+                  {{#if this.isCurrentUser}}
+                    <div class="actions">
+                      <DButton @action={{action "editSecondFactorBackup"}} @class="btn-default btn-flat btn-small btn-icon pad-left no-text edit edit-2fa-backup" @disabled={{this.loading}} @icon="pencil-alt" @aria-label="user.second_factor.edit" @title="user.second_factor.edit" />
+
+                      {{#if this.model.second_factor_backup_enabled}}
+                        <DButton @action={{action "disableSecondFactorBackup"}} @class="btn-danger btn-flat no-text" @icon="trash-alt" @aria-label="user.second_factor.disable" @title="user.second_factor.disable" />
+                      {{/if}}
+                    </div>
+                  {{/if}}
+                {{else}}
+                  {{i18n "user.second_factor_backup.enable_prerequisites"}}
                 {{/if}}
-              {{else}}
-                {{i18n "user.second_factor_backup.enable_prerequisites"}}
-              {{/if}}
+              </div>
             </div>
           </div>
 

app/assets/javascripts/discourse/tests/acceptance/user-preferences-second-factor-test.js
@@ -4,6 +4,7 @@ import {
   acceptance,
   exists,
   query,
+  updateCurrentUser,
 } from "discourse/tests/helpers/qunit-helpers";
 
 acceptance("User Preferences - Second Factor", function (needs) {
@@ -14,6 +15,8 @@ acceptance("User Preferences - Second Factor", function (needs) {
       return helper.response({
         success: "OK",
         password_required: "true",
+        totps: [{ id: 1, name: "one of them" }],
+        security_keys: [{ id: 2, name: "key" }],
       });
     });
 
@@ -89,5 +92,34 @@ acceptance("User Preferences - Second Factor", function (needs) {
         "shows name missing error message"
       );
     }
+  });
+
+  test("delete second factor security method", async function (assert) {
+    updateCurrentUser({ moderator: false, admin: false, trust_level: 1 });
+    await visit("/u/eviltrout/preferences/second-factor");
+
+    assert.ok(exists("#password"), "it has a password input");
+
+    await fillIn("#password", "secrets");
+    await click(".user-preferences .btn-primary");
+    await click(".totp .btn-danger");
+    assert.strictEqual(
+      query("#dialog-title").innerText.trim(),
+      "Deleting an authenticator"
+    );
+    await click(".dialog-close");
+
+    await click(".security-key .btn-danger");
+    assert.strictEqual(
+      query("#dialog-title").innerText.trim(),
+      "Deleting an authenticator"
+    );
+    await click(".dialog-close");
+
+    await click(".btn-danger.btn-icon-text");
+    assert.strictEqual(
+      query("#dialog-title").innerText.trim(),
+      "Are you sure you want to disable two-factor authentication?"
+    );
   });
 });

app/assets/javascripts/dialog-holder/addon/components/dialog-holder.hbs
@@ -10,9 +10,14 @@
         </div>
       {{/if}}
 
-      {{#if this.dialog.message}}
+      {{#if (or this.dialog.message this.dialog.confirmPhrase)}}
         <div class="dialog-body">
-          {{this.dialog.message}}
+          {{#if this.dialog.message}}
+            <p>{{this.dialog.message}}</p>
+          {{/if}}
+          {{#if this.dialog.confirmPhrase}}
+            <TextField @value={{this.dialog.confirmPhraseInput}} {{on "input" this.dialog.onConfirmPhraseInput}} @id="confirm-phrase" @autocorrect="off" @autocapitalize="off" />
+          {{/if}}
         </div>
       {{/if}}
 
@@ -21,9 +26,9 @@
           {{#each this.dialog.buttons as |button|}}
             <DButton @icon={{button.icon}} @class={{button.class}} @action={{action "handleButtonAction" button}} @translatedLabel={{button.label}} />
           {{else}}
-            <DButton @class={{this.dialog.confirmButtonClass}} @action={{this.dialog.didConfirmWrapped}} @icon={{this.dialog.confirmButtonIcon}} @label={{this.dialog.confirmButtonLabel}} />
+            <DButton @class={{this.dialog.confirmButtonClass}} @disabled={{this.dialog.confirmButtonDisabled}} @action={{this.dialog.didConfirmWrapped}} @icon={{this.dialog.confirmButtonIcon}} @label={{this.dialog.confirmButtonLabel}} />
             {{#if this.dialog.shouldDisplayCancel}}
-              <DButton @class="btn-default" @action={{this.dialog.cancel}} @label={{this.dialog.cancelButtonLabel}} />
+              <DButton @class={{this.dialog.cancelButtonClass}} @action={{this.dialog.cancel}} @label={{this.dialog.cancelButtonLabel}} />
             {{/if}}
           {{/each}}
         </div>

app/assets/javascripts/dialog-holder/addon/services/dialog.js
@@ -1,6 +1,7 @@
 import Service from "@ember/service";
 import A11yDialog from "a11y-dialog";
 import { bind } from "discourse-common/utils/decorators";
+import { isBlank } from "@ember/utils";
 
 export default Service.extend({
   message: null,
@@ -13,7 +14,10 @@ export default Service.extend({
   confirmButtonIcon: null,
   confirmButtonLabel: null,
   confirmButtonClass: null,
+  confirmPhrase: null,
+  confirmPhraseInput: null,
   cancelButtonLabel: null,
+  cancelButtonClass: null,
   shouldDisplayCancel: null,
 
   didConfirm: null,
@@ -32,12 +36,16 @@ export default Service.extend({
       confirmButtonLabel = "ok_value",
       confirmButtonClass = "btn-primary",
       cancelButtonLabel = "cancel_value",
+      cancelButtonClass = "btn-default",
+      confirmPhrase,
       shouldDisplayCancel,
 
       didConfirm,
       didCancel,
       buttons,
     } = params;
+
+    let confirmButtonDisabled = !isBlank(confirmPhrase);
 
     const element = document.getElementById("dialog-holder");
 
@@ -49,10 +57,13 @@ export default Service.extend({
       title,
       titleElementId: title !== null ? "dialog-title" : null,
 
+      confirmButtonDisabled,
       confirmButtonClass,
       confirmButtonLabel,
       confirmButtonIcon,
+      confirmPhrase,
       cancelButtonLabel,
+      cancelButtonClass,
       shouldDisplayCancel,
 
       didConfirm,
@@ -131,7 +142,10 @@ export default Service.extend({
       confirmButtonLabel: null,
       confirmButtonIcon: null,
       cancelButtonLabel: null,
+      cancelButtonClass: null,
       shouldDisplayCancel: null,
+      confirmPhrase: null,
+      confirmPhraseInput: null,
 
       didConfirm: null,
       didCancel: null,
@@ -159,5 +173,13 @@ export default Service.extend({
   @bind
   cancel() {
     this.dialogInstance.hide();
+  },
+
+  @bind
+  onConfirmPhraseInput() {
+    this.set(
+      "confirmButtonDisabled",
+      this.confirmPhrase && this.confirmPhraseInput !== this.confirmPhrase
+    );
   },
 });

app/assets/javascripts/discourse/app/components/second-factor-form.js
@@ -42,12 +42,10 @@ export default Component.extend({
     }
   },
 
-  @discourseComputed("backupEnabled", "totpEnabled", "secondFactorMethod")
-  showToggleMethodLink(backupEnabled, totpEnabled, secondFactorMethod) {
+  @discourseComputed("backupEnabled", "secondFactorMethod")
+  showToggleMethodLink(backupEnabled, secondFactorMethod) {
     return (
-      backupEnabled &&
-      totpEnabled &&
-      secondFactorMethod !== SECOND_FACTOR_METHODS.SECURITY_KEY
+      backupEnabled && secondFactorMethod !== SECOND_FACTOR_METHODS.SECURITY_KEY
     );
   },
 

app/assets/javascripts/discourse/app/controllers/login.js
@@ -223,30 +223,22 @@ export default Controller.extend(ModalFunctionality, {
             this.clearFlash();
 
             if (
-              (result.security_key_enabled ||
-                result.totp_enabled ||
-                result.backup_enabled) &&
+              (result.security_key_enabled || result.totp_enabled) &&
               !this.secondFactorRequired
             ) {
-              let secondFactorMethod;
-              if (result.security_key_enabled) {
-                secondFactorMethod = SECOND_FACTOR_METHODS.SECURITY_KEY;
-              } else if (result.totp_enabled) {
-                secondFactorMethod = SECOND_FACTOR_METHODS.TOTP;
-              } else {
-                secondFactorMethod = SECOND_FACTOR_METHODS.BACKUP_CODE;
-              }
               this.setProperties({
                 otherMethodAllowed: result.multiple_second_factor_methods,
                 secondFactorRequired: true,
                 showLoginButtons: false,
                 backupEnabled: result.backup_enabled,
                 totpEnabled: result.totp_enabled,
-                showSecondFactor: result.totp_enabled || result.backup_enabled,
+                showSecondFactor: result.totp_enabled,
                 showSecurityKey: result.security_key_enabled,
+                secondFactorMethod: result.security_key_enabled
+                  ? SECOND_FACTOR_METHODS.SECURITY_KEY
+                  : SECOND_FACTOR_METHODS.TOTP,
                 securityKeyChallenge: result.challenge,
                 securityKeyAllowedCredentialIds: result.allowed_credential_ids,
-                secondFactorMethod,
               });
 
               // only need to focus the 2FA input for TOTP

app/assets/javascripts/discourse/app/controllers/second-factor-backup-edit.js
@@ -40,30 +40,6 @@ export default Controller.extend(ModalFunctionality, {
       this._hideCopyMessage();
     },
 
-    disableSecondFactorBackup() {
-      this.set("backupCodes", []);
-      this.set("loading", true);
-
-      this.model
-        .updateSecondFactor(0, "", true, SECOND_FACTOR_METHODS.BACKUP_CODE)
-        .then((response) => {
-          if (response.error) {
-            this.set("errorMessage", response.error);
-            return;
-          }
-
-          this.set("errorMessage", null);
-          this.model.set("second_factor_backup_enabled", false);
-          this.markDirty();
-          this.send("closeModal");
-        })
-        .catch((error) => {
-          this.send("closeModal");
-          this.onError(error);
-        })
-        .finally(() => this.set("loading", false));
-    },
-
     generateSecondFactorCodes() {
       this.set("loading", true);
       this.model

commit 1acd8dbd0ea0866b4b2460342a3fc16cd7363632

FIX: Deleting security keys was not working (#20427)
Bug was introduced in 817b40bf1a8ca259812e89e662597f994362e6d0. There is
a workaround, using the trash icon in the edit modal, but that UI is
quite confusing to users.

app/assets/javascripts/discourse/app/controllers/preferences/second-factor.js
@@ -166,40 +166,61 @@ export default Controller.extend(CanCheckEmails, {
         confirmButtonIcon: "ban",
         cancelButtonClass: "btn-flat",
         didConfirm: () => {
-          this.currentUser
-            .updateSecondFactor(
-              secondFactorMethod.id,
-              secondFactorMethod.name,
-              true,
-              secondFactorMethod.method
-            )
-            .then((response) => {
-              if (response.error) {
-                return;
-              }
-              this.markDirty();
-            })
-            .catch((e) => this.handleError(e))
-            .finally(() => {
-              this.setProperties({
-                totps: this.totps.filter(
-                  (totp) =>
-                    totp.id !== secondFactorMethod.id ||
-                    totp.method !== secondFactorMethod.method
-                ),
-                security_keys: this.security_keys.filter(
-                  (key) =>
-                    key.id !== secondFactorMethod.id ||
-                    key.method !== secondFactorMethod.method
-                ),
+          if (this.totps.includes(secondFactorMethod)) {
+            this.currentUser
+              .updateSecondFactor(
+                secondFactorMethod.id,
+                secondFactorMethod.name,
+                true,
+                secondFactorMethod.method
+              )
+              .then((response) => {
+                if (response.error) {
+                  return;
+                }
+                this.markDirty();
+                this.set(
+                  "totps",
+                  this.totps.filter(
+                    (totp) =>
+                      totp.id !== secondFactorMethod.id ||
+                      totp.method !== secondFactorMethod.method
+                  )
+                );
+              })
+              .catch((e) => this.handleError(e))
+              .finally(() => {
+                this.set("loading", false);
               });
+          }
 
-              this.set("loading", false);
-            });
+          if (this.security_keys.includes(secondFactorMethod)) {
+            this.currentUser
+              .updateSecurityKey(
+                secondFactorMethod.id,
+                secondFactorMethod.name,
+                true
+              )
+              .then((response) => {
+                if (response.error) {
+                  return;
+                }
+                this.markDirty();
+                this.set(
+                  "security_keys",
+                  this.security_keys.filter(
+                    (securityKey) => securityKey.id !== secondFactorMethod.id
+                  )
+                );
+              })
+              .catch((e) => this.handleError(e))
+              .finally(() => {
+                this.set("loading", false);
+              });
+          }
         },
       });
     },
-
     disableSecondFactorBackup() {
       this.dialog.deleteConfirm({
         title: I18n.t("user.second_factor.delete_backup_codes_confirm_title"),

app/assets/javascripts/discourse/app/controllers/second-factor-edit-security-key.js
@@ -3,25 +3,6 @@ import ModalFunctionality from "discourse/mixins/modal-functionality";
 
 export default Controller.extend(ModalFunctionality, {
   actions: {
-    disableSecurityKey() {
-      this.user
-        .updateSecurityKey(this.model.id, this.model.name, true)
-        .then((response) => {
-          if (response.error) {
-            return;
-          }
-          this.markDirty();
-        })
-        .catch((error) => {
-          this.send("closeModal");
-          this.onError(error);
-        })
-        .finally(() => {
-          this.set("loading", false);
-          this.send("closeModal");
-        });
-    },
-
     editSecurityKey() {
       this.user
         .updateSecurityKey(this.model.id, this.model.name, false)

app/assets/javascripts/discourse/app/templates/modal/second-factor-edit-security-key.hbs
@@ -15,11 +15,4 @@
     @class="btn-primary"
     @label="user.second_factor.security_key.save"
   />
-  <DButton
-    @action={{action "disableSecurityKey"}}
-    @class="btn-danger no-text"
-    @icon="trash-alt"
-    @aria-label="user.second_factor.disable"
-    @title="user.second_factor.disable"
-  />
 </div>

app/assets/javascripts/discourse/app/templates/preferences-second-factor.hbs
@@ -169,7 +169,7 @@
 
           {{#if this.model.second_factor_enabled}}
             {{#unless this.showEnforcedNotice}}
-              <div class="control-group">
+              <div class="control-group pref-second-factor-disable-all">
                 <div class="controls">
                   <DButton
                     @class="btn-danger"

app/assets/javascripts/discourse/tests/acceptance/user-preferences-second-factor-test.js
@@ -41,6 +41,12 @@ acceptance("User Preferences - Second Factor", function (needs) {
       });
     });
 
+    server.put("/u/security_key.json", () => {
+      return helper.response({
+        success: "OK",
+      });
+    });
+
     server.put("/u/second_factors_backup.json", () => {
       return helper.response({
         backup_codes: ["dsffdsd", "fdfdfdsf", "fddsds"],
@@ -109,14 +115,23 @@ acceptance("User Preferences - Second Factor", function (needs) {
     );
     await click(".dialog-close");
 
+    assert.ok(
+      exists(".security-key .second-factor-item"),
+      "User has a physical security key"
+    );
+
     await click(".security-key .btn-danger");
     assert.strictEqual(
       query("#dialog-title").innerText.trim(),
       "Deleting an authenticator"
     );
-    await click(".dialog-close");
+    await click(".dialog-footer .btn-danger");
+    assert.notOk(
+      exists(".security-key .second-factor-item"),
+      "security key row is removed after a successful delete"
+    );
 
-    await click(".btn-danger.btn-icon-text");
+    await click(".pref-second-factor-disable-all .btn-danger");
     assert.strictEqual(
       query("#dialog-title").innerText.trim(),
       "Are you sure you want to disable two-factor authentication?"
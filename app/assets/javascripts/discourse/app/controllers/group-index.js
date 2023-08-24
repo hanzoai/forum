hanzoai/forum

commit 3e293a8641fb391e33f82e7e60d4a1e093e052c5

DEV: Introduce declarative hide-application-footer helper (#23088)
Previously, we had a `showFooter` boolean on the application controller which would be set true/false in various routes by different routes/controllers. A global `routeWillChange` hook would set it `false` before every route transition, and the destination route/controller would have to set it `true` for the footer to show correctly.

This commit replaces that with a new 'declarative' system. Instead of having to set the value true/false manually, UIs which need the footer to be hidden can simply include the `{{hide-application-footer}}` helper in their template when needed. The helper/service will automatically keep track of all the current invocations of that helper, and only show the footer when there are 0 invocations.

This significantly simplifies things, and removes the need for many observers and controller injections, both of which are considered 'code smells' in modern Ember applications.

app/assets/javascripts/discourse/app/controllers/group-index.js
@@ -1,4 +1,4 @@
-import Controller, { inject as controller } from "@ember/controller";
+import Controller from "@ember/controller";
 import discourseComputed, {
   debounce,
   observes,
@@ -9,8 +9,6 @@ import { gt } from "@ember/object/computed";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 
 export default Controller.extend({
-  application: controller(),
-
   queryParams: ["order", "asc", "filter"],
 
   order: "",
@@ -23,6 +21,10 @@ export default Controller.extend({
   showActions: false,
 
   bulkSelection: null,
+
+  get canLoadMore() {
+    return this.get("model.members")?.length >= this.get("model.user_count");
+  },
 
   @observes("filterInput")
   filterInputChanged() {
@@ -44,18 +46,13 @@ export default Controller.extend({
       return;
     }
 
-    if (!refresh && this.model.members.length >= this.model.user_count) {
-      this.set("application.showFooter", true);
+    if (!refresh && !this.canLoadMore) {
       return;
     }
 
     this.set("loading", true);
     this.model.reloadMembers(this.memberParams, refresh).finally(() => {
-      this.setProperties({
-        "application.showFooter":
-          this.model.members.length >= this.model.user_count,
-        loading: false,
-      });
+      this.set("loading", false);
 
       if (this.refresh) {
         this.set("bulkSelection", []);

commit a37140c8d4406b029f06929cbf0d36b8f1394d4f

FIX: Infinite loading broken on group members list (#23214)
This regressed in 3e293a8641fb391e33f82e7e60d4a1e093e052c5 where
the condition to load more members into the list on the client side was
inverted.

app/assets/javascripts/discourse/app/controllers/group-index.js
@@ -23,7 +23,7 @@ export default Controller.extend({
   bulkSelection: null,
 
   get canLoadMore() {
-    return this.get("model.members")?.length >= this.get("model.user_count");
+    return this.get("model.members")?.length < this.get("model.user_count");
   },
 
   @observes("filterInput")

app/controllers/groups_controller.rb
@@ -230,14 +230,15 @@ class GroupsController < ApplicationController
     render "posts/latest", formats: [:rss]
   end
 
-  MEMBERS_LIMIT = 1_000
+  MEMBERS_MAX_PAGE_SIZE = 1_000
+  MEMBERS_DEFAULT_PAGE_SIZE = 50
 
   def members
     group = find_group(:group_id)
 
     guardian.ensure_can_see_group_members!(group)
 
-    limit = fetch_limit_from_params(default: 50, max: MEMBERS_LIMIT)
+    limit = fetch_limit_from_params(default: MEMBERS_DEFAULT_PAGE_SIZE, max: MEMBERS_MAX_PAGE_SIZE)
     offset = params[:offset].to_i
 
     raise Discourse::InvalidParameters.new(:offset) if offset < 0

spec/system/viewing_group_members_spec.rb
@@ -1,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.describe "Viewing group members", type: :system do
+  fab!(:group) { Fabricate(:group) }
+  fab!(:user_in_group_1) { Fabricate(:user).tap { |u| group.add(u) } }
+  fab!(:user_in_group_2) { Fabricate(:user).tap { |u| group.add(u) } }
+  fab!(:user_in_group_3) { Fabricate(:user).tap { |u| group.add(u) } }
+
+  it "loads more group members when a user scrolls to the bottom of the list" do
+    stub_const(GroupsController, "MEMBERS_DEFAULT_PAGE_SIZE", 2) do
+      visit("/g/#{group.name}/members")
+
+      expect(page).to have_selector(".group-member", count: 3)
+    end
+  end
+end
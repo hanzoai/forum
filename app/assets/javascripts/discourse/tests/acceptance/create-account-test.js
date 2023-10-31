hanzoai/forum

commit c5eb96b01b65a6d539ec19a6401999eb756e1ee2

DEV: Migrate `create-account` to the new modal API (#24098)
plugin/theme-breaking changes:

1. `controller:create-account` is gone (use `component:modal/create-account` in modifyClass, **if** absolutely necessary)
2. `create-account-body` css class is gone (target `.d-modal.create-account` or any of the inner classes: `.modal-outer-container`, `.modal-middle-container`, `.modal-inner-container`, or `.modal-body`)

app/assets/javascripts/discourse/app/components/modal/create-account.js
@@ -1,0 +1,522 @@
+import { A } from "@ember/array";
+import Component from "@ember/component";
+import EmberObject, { action } from "@ember/object";
+import { alias, notEmpty } from "@ember/object/computed";
+import { inject as service } from "@ember/service";
+import { isEmpty } from "@ember/utils";
+import $ from "jquery";
+import { Promise } from "rsvp";
+import LoginModal from "discourse/components/modal/login";
+import { ajax } from "discourse/lib/ajax";
+import { setting } from "discourse/lib/computed";
+import cookie, { removeCookie } from "discourse/lib/cookie";
+import { userPath } from "discourse/lib/url";
+import { emailValid } from "discourse/lib/utilities";
+import { wavingHandURL } from "discourse/lib/waving-hand-url";
+import NameValidation from "discourse/mixins/name-validation";
+import PasswordValidation from "discourse/mixins/password-validation";
+import UserFieldsValidation from "discourse/mixins/user-fields-validation";
+import UsernameValidation from "discourse/mixins/username-validation";
+import { findAll } from "discourse/models/login-method";
+import User from "discourse/models/user";
+import discourseDebounce from "discourse-common/lib/debounce";
+import discourseComputed, {
+  bind,
+  observes,
+} from "discourse-common/utils/decorators";
+import I18n from "discourse-i18n";
+
+export default class CreateAccount extends Component.extend(
+  PasswordValidation,
+  UsernameValidation,
+  NameValidation,
+  UserFieldsValidation
+) {
+  @service modal;
+  @service site;
+  @service siteSettings;
+
+  accountChallenge = 0;
+  accountHoneypot = 0;
+  formSubmitted = false;
+  rejectedEmails = A();
+  prefilledUsername = null;
+  userFields = null;
+  isDeveloper = false;
+  maskPassword = true;
+
+  @notEmpty("model.authOptions") hasAuthOptions;
+  @setting("enable_local_logins") canCreateLocal;
+  @setting("require_invite_code") requireInviteCode;
+
+  // For UsernameValidation mixin
+  @alias("model.authOptions") authOptions;
+  @alias("model.accountEmail") accountEmail;
+  @alias("model.accountUsername") accountUsername;
+
+  init() {
+    super.init(...arguments);
+
+    if (cookie("email")) {
+      this.set("model.accountEmail", cookie("email"));
+    }
+
+    this.fetchConfirmationValue();
+
+    if (this.model.skipConfirmation) {
+      this.performAccountCreation().finally(() =>
+        this.set("model.skipConfirmation", false)
+      );
+    }
+  }
+
+  // used for animating the label inside of inputs
+  @bind
+  userInputFocus(event) {
+    const userField = event.target.parentElement.parentElement;
+    if (!userField.classList.contains("value-entered")) {
+      userField.classList.toggle("value-entered");
+    }
+  }
+
+  // used for animating the label inside of inputs
+  @bind
+  userInputFocusOut(event) {
+    const userField = event.target.parentElement.parentElement;
+    if (
+      event.target.value.length === 0 &&
+      userField.classList.contains("value-entered")
+    ) {
+      userField.classList.toggle("value-entered");
+    }
+  }
+
+  @bind
+  actionOnEnter(event) {
+    if (!this.submitDisabled && event.key === "Enter") {
+      event.preventDefault();
+      event.stopPropagation();
+      this.createAccount();
+      return false;
+    }
+  }
+
+  @bind
+  selectKitFocus(event) {
+    const target = document.getElementById(event.target.getAttribute("for"));
+    if (target?.classList.contains("select-kit")) {
+      event.preventDefault();
+      target.querySelector(".select-kit-header").click();
+    }
+  }
+
+  @discourseComputed(
+    "hasAuthOptions",
+    "canCreateLocal",
+    "model.skipConfirmation"
+  )
+  showCreateForm(hasAuthOptions, canCreateLocal, skipConfirmation) {
+    return (hasAuthOptions || canCreateLocal) && !skipConfirmation;
+  }
+
+  @discourseComputed("site.desktopView", "hasAuthOptions")
+  showExternalLoginButtons(desktopView, hasAuthOptions) {
+    return desktopView && !hasAuthOptions;
+  }
+
+  @discourseComputed("formSubmitted")
+  submitDisabled() {
+    return this.formSubmitted;
+  }
+
+  @discourseComputed()
+  wavingHandURL() {
+    return wavingHandURL();
+  }
+
+  @discourseComputed("userFields", "hasAtLeastOneLoginButton", "hasAuthOptions")
+  modalBodyClasses(userFields, hasAtLeastOneLoginButton, hasAuthOptions) {
+    const classes = [];
+    if (userFields) {
+      classes.push("has-user-fields");
+    }
+    if (hasAtLeastOneLoginButton && !hasAuthOptions) {
+      classes.push("has-alt-auth");
+    }
+    if (!this.canCreateLocal) {
+      classes.push("no-local-logins");
+    }
+    return classes.join(" ");
+  }
+
+  @discourseComputed("model.authOptions", "model.authOptions.can_edit_username")
+  usernameDisabled(authOptions, canEditUsername) {
+    return authOptions && !canEditUsername;
+  }
+
+  @discourseComputed("model.authOptions", "model.authOptions.can_edit_name")
+  nameDisabled(authOptions, canEditName) {
+    return authOptions && !canEditName;
+  }
+
+  @discourseComputed
+  fullnameRequired() {
+    return (
+      this.siteSettings.full_name_required || this.siteSettings.enable_names
+    );
+  }
+
+  @discourseComputed("model.authOptions.auth_provider")
+  passwordRequired(authProvider) {
+    return isEmpty(authProvider);
+  }
+
+  @discourseComputed
+  disclaimerHtml() {
+    if (this.site.tos_url && this.site.privacy_policy_url) {
+      return I18n.t("create_account.disclaimer", {
+        tos_link: this.site.tos_url,
+        privacy_link: this.site.privacy_policy_url,
+      });
+    }
+  }
+
+  // Check the email address
+  @discourseComputed(
+    "serverAccountEmail",
+    "serverEmailValidation",
+    "model.accountEmail",
+    "rejectedEmails.[]",
+    "forceValidationReason"
+  )
+  emailValidation(
+    serverAccountEmail,
+    serverEmailValidation,
+    email,
+    rejectedEmails,
+    forceValidationReason
+  ) {
+    const failedAttrs = {
+      failed: true,
+      ok: false,
+      element: document.querySelector("#new-account-email"),
+    };
+
+    if (serverAccountEmail === email && serverEmailValidation) {
+      return serverEmailValidation;
+    }
+
+    // If blank, fail without a reason
+    if (isEmpty(email)) {
+      return EmberObject.create(
+        Object.assign(failedAttrs, {
+          message: I18n.t("user.email.required"),
+          reason: forceValidationReason ? I18n.t("user.email.required") : null,
+        })
+      );
+    }
+
+    if (rejectedEmails.includes(email) || !emailValid(email)) {
+      return EmberObject.create(
+        Object.assign(failedAttrs, {
+          reason: I18n.t("user.email.invalid"),
+        })
+      );
+    }
+
+    if (
+      this.get("model.authOptions.email") === email &&
+      this.get("model.authOptions.email_valid")
+    ) {
+      return EmberObject.create({
+        ok: true,
+        reason: I18n.t("user.email.authenticated", {
+          provider: this.authProviderDisplayName(
+            this.get("model.authOptions.auth_provider")
+          ),
+        }),
+      });
+    }
+
+    return EmberObject.create({
+      ok: true,
+      reason: I18n.t("user.email.ok"),
+    });
+  }
+
+  @action
+  checkEmailAvailability() {
+    if (
+      !this.emailValidation.ok ||
+      this.serverAccountEmail === this.model.accountEmail
+    ) {
+      return;
+    }
+
+    return User.checkEmail(this.model.accountEmail)
+      .then((result) => {
+        if (this.isDestroying || this.isDestroyed) {
+          return;
+        }
+
+        if (result.failed) {
+          this.setProperties({
+            serverAccountEmail: this.model.accountEmail,
+            serverEmailValidation: EmberObject.create({
+              failed: true,
+              element: document.querySelector("#new-account-email"),
+              reason: result.errors[0],
+            }),
+          });
+        } else {
+          this.setProperties({
+            serverAccountEmail: this.model.accountEmail,
+            serverEmailValidation: EmberObject.create({
+              ok: true,
+              reason: I18n.t("user.email.ok"),
+            }),
+          });
+        }
+      })
+      .catch(() => {
+        this.setProperties({
+          serverAccountEmail: null,
+          serverEmailValidation: null,
+        });
+      });
+  }
+
+  @discourseComputed(
+    "model.accountEmail",
+    "model.authOptions.email",
+    "model.authOptions.email_valid"
+  )
+  emailDisabled() {
+    return (
+      this.get("model.authOptions.email") === this.model.accountEmail &&
+      this.get("model.authOptions.email_valid")
+    );
+  }
+
+  authProviderDisplayName(providerName) {
+    const matchingProvider = findAll().find((provider) => {
+      return provider.name === providerName;
+    });
+    return matchingProvider ? matchingProvider.get("prettyName") : providerName;
+  }
+
+  @observes("emailValidation", "model.accountEmail")
+  prefillUsername() {
+    if (this.prefilledUsername) {
+      // If username field has been filled automatically, and email field just changed,
+      // then remove the username.
+      if (this.model.accountUsername === this.prefilledUsername) {
+        this.set("model.accountUsername", "");
+      }
+      this.set("prefilledUsername", null);
+    }
+    if (
+      this.get("emailValidation.ok") &&
+      (isEmpty(this.model.accountUsername) ||
+        this.get("model.authOptions.email"))
+    ) {
+      // If email is valid and username has not been entered yet,
+      // or email and username were filled automatically by 3rd party auth,
+      // then look for a registered username that matches the email.
+      discourseDebounce(this, this.fetchExistingUsername, 500);
+    }
+  }
+
+  // Determines whether at least one login button is enabled
+  @discourseComputed
+  hasAtLeastOneLoginButton() {
+    return findAll().length > 0;
+  }
+
+  fetchConfirmationValue() {
+    if (this._challengeDate === undefined && this._hpPromise) {
+      // Request already in progress
+      return this._hpPromise;
+    }
+
+    this._hpPromise = ajax("/session/hp.json")
+      .then((json) => {
+        if (this.isDestroying || this.isDestroyed) {
+          return;
+        }
+
+        this._challengeDate = new Date();
+        // remove 30 seconds for jitter, make sure this works for at least
+        // 30 seconds so we don't have hard loops
+        this._challengeExpiry = parseInt(json.expires_in, 10) - 30;
+        if (this._challengeExpiry < 30) {
+          this._challengeExpiry = 30;
+        }
+
+        this.setProperties({
+          accountHoneypot: json.value,
+          accountChallenge: json.challenge.split("").reverse().join(""),
+        });
+      })
+      .finally(() => (this._hpPromise = undefined));
+
+    return this._hpPromise;
+  }
+
+  performAccountCreation() {
+    if (
+      !this._challengeDate ||
+      new Date() - this._challengeDate > 1000 * this._challengeExpiry
+    ) {
+      return this.fetchConfirmationValue().then(() =>
+        this.performAccountCreation()
+      );
+    }
+
+    const attrs = this.getProperties(
+      "model.accountName",
+      "model.accountEmail",
+      "accountPassword",
+      "model.accountUsername",
+      "accountChallenge",
+      "inviteCode"
+    );
+
+    attrs["accountPasswordConfirm"] = this.accountHoneypot;
+
+    const userFields = this.userFields;
+    const destinationUrl = this.get("model.authOptions.destination_url");
+
+    if (!isEmpty(destinationUrl)) {
+      cookie("destination_url", destinationUrl, { path: "/" });
+    }
+
+    // Add the userFields to the data
+    if (!isEmpty(userFields)) {
+      attrs.userFields = {};
+      userFields.forEach(
+        (f) => (attrs.userFields[f.get("field.id")] = f.get("value"))
+      );
+    }
+
+    this.set("formSubmitted", true);
+    return User.createAccount(attrs).then(
+      (result) => {
+        if (this.isDestroying || this.isDestroyed) {
+          return;
+        }
+
+        this.set("isDeveloper", false);
+        if (result.success) {
+          // invalidate honeypot
+          this._challengeExpiry = 1;
+
+          // Trigger the browser's password manager using the hidden static login form:
+          const $hidden_login_form = $("#hidden-login-form");
+          $hidden_login_form
+            .find("input[name=username]")
+            .val(attrs.accountUsername);
+          $hidden_login_form
+            .find("input[name=password]")
+            .val(attrs.accountPassword);
+          $hidden_login_form
+            .find("input[name=redirect]")
+            .val(userPath("account-created"));
+          $hidden_login_form.submit();
+          return new Promise(() => {}); // This will never resolve, the page will reload instead
+        } else {
+          this.set("flash", result.message || I18n.t("create_account.failed"));
+          if (result.is_developer) {
+            this.set("isDeveloper", true);
+          }
+          if (
+            result.errors &&
+            result.errors.email &&
+            result.errors.email.length > 0 &&
+            result.values
+          ) {
+            this.rejectedEmails.pushObject(result.values.email);
+          }
+          if (
+            result.errors &&
+            result.errors.password &&
+            result.errors.password.length > 0
+          ) {
+            this.rejectedPasswords.pushObject(attrs.accountPassword);
+          }
+          this.set("formSubmitted", false);
+          removeCookie("destination_url");
+        }
+      },
+      () => {
+        this.set("formSubmitted", false);
+        removeCookie("destination_url");
+        return this.set("flash", I18n.t("create_account.failed"));
+      }
+    );
+  }
+
+  @discourseComputed(
+    "model.authOptions.associate_url",
+    "model.authOptions.auth_provider"
+  )
+  associateHtml(url, provider) {
+    if (!url) {
+      return;
+    }
+    return I18n.t("create_account.associate", {
+      associate_link: url,
+      provider: I18n.t(`login.${provider}.name`),
+    });
+  }
+
+  @action
+  togglePasswordMask() {
+    this.toggleProperty("maskPassword");
+  }
+
+  @action
+  externalLogin(provider) {
+    // we will automatically redirect to the external auth service
+    this.modal.show(LoginModal, {
+      model: {
+        isExternalLogin: true,
+        externalLoginMethod: provider,
+        signup: true,
+      },
+    });
+  }
+
+  @action
+  createAccount() {
+    this.set("flash", "");
+    this.set("forceValidationReason", true);
+
+    const validation = [
+      this.emailValidation,
+      this.usernameValidation,
+      this.nameValidation,
+      this.passwordValidation,
+      this.userFieldsValidation,
+    ].find((v) => v.failed);
+
+    if (validation) {
+      const element = validation.element;
+      if (element) {
+        if (element.tagName === "DIV") {
+          if (element.scrollIntoView) {
+            element.scrollIntoView();
+          }
+          element.click();
+        } else {
+          element.focus();
+        }
+      }
+
+      return;
+    }
+
+    this.set("forceValidationReason", false);
+    this.performAccountCreation();
+  }
+}

app/assets/javascripts/discourse/app/components/create-account.js
@@ -1,91 +1,0 @@
-import Component from "@ember/component";
-import cookie from "discourse/lib/cookie";
-import { bind } from "discourse-common/utils/decorators";
-
-export default Component.extend({
-  classNames: ["create-account-body"],
-
-  // used for animating the label inside of inputs
-  userInputFocus(event) {
-    const userField = event.target.parentElement.parentElement;
-    if (!userField.classList.contains("value-entered")) {
-      userField.classList.toggle("value-entered");
-    }
-  },
-
-  // used for animating the label inside of inputs
-  userInputFocusOut(event) {
-    const userField = event.target.parentElement.parentElement;
-    if (
-      event.target.value.length === 0 &&
-      userField.classList.contains("value-entered")
-    ) {
-      userField.classList.toggle("value-entered");
-    }
-  },
-
-  @bind
-  actionOnEnter(event) {
-    if (!this.disabled && event.key === "Enter") {
-      event.preventDefault();
-      event.stopPropagation();
-      this.action();
-      return false;
-    }
-  },
-
-  @bind
-  selectKitFocus(event) {
-    const target = document.getElementById(event.target.getAttribute("for"));
-    if (target?.classList.contains("select-kit")) {
-      event.preventDefault();
-      target.querySelector(".select-kit-header").click();
-    }
-  },
-
-  didInsertElement() {
-    this._super(...arguments);
-
-    if (cookie("email")) {
-      this.set("email", cookie("email"));
-    }
-
-    let userTextFields = document.getElementsByClassName("user-fields")[0];
-
-    if (userTextFields) {
-      userTextFields =
-        userTextFields.getElementsByClassName("ember-text-field");
-    }
-
-    if (userTextFields) {
-      for (let element of userTextFields) {
-        element.addEventListener("focus", this.userInputFocus);
-        element.addEventListener("focusout", this.userInputFocusOut);
-      }
-    }
-
-    this.element.addEventListener("keydown", this.actionOnEnter);
-    this.element.addEventListener("click", this.selectKitFocus);
-  },
-
-  willDestroyElement() {
-    this._super(...arguments);
-
-    this.element.removeEventListener("keydown", this.actionOnEnter);
-    this.element.removeEventListener("click", this.selectKitFocus);
-
-    let userTextFields = document.getElementsByClassName("user-fields")[0];
-
-    if (userTextFields) {
-      userTextFields =
-        userTextFields.getElementsByClassName("ember-text-field");
-    }
-
-    if (userTextFields) {
-      for (let element of userTextFields) {
-        element.removeEventListener("focus", this.userInputFocus);
-        element.removeEventListener("focusout", this.userInputFocusOut);
-      }
-    }
-  },
-});

commit 5b79ba692d60000c1ac269b81e5a4d55df78cfdb

FIX: Account creation regression (#24169)
Regressed in c5eb96b01b65a6d539ec19a6401999eb756e1ee2

app/assets/javascripts/discourse/app/components/modal/create-account.js
@@ -373,16 +373,15 @@ export default class CreateAccount extends Component.extend(
       );
     }
 
-    const attrs = this.getProperties(
-      "model.accountName",
-      "model.accountEmail",
-      "accountPassword",
-      "model.accountUsername",
-      "accountChallenge",
-      "inviteCode"
-    );
-
-    attrs["accountPasswordConfirm"] = this.accountHoneypot;
+    const attrs = {
+      accountName: this.model.accountName,
+      accountEmail: this.model.accountEmail,
+      accountPassword: this.accountPassword,
+      accountUsername: this.model.accountUsername,
+      accountChallenge: this.accountChallenge,
+      inviteCode: this.inviteCode,
+      accountPasswordConfirm: this.accountHoneypot,
+    };
 
     const userFields = this.userFields;
     const destinationUrl = this.get("model.authOptions.destination_url");

app/assets/javascripts/discourse/tests/acceptance/create-account-test.js
@@ -2,6 +2,10 @@ import { click, fillIn, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import sinon from "sinon";
 import LoginMethod from "discourse/models/login-method";
+import pretender, {
+  parsePostData,
+  response,
+} from "discourse/tests/helpers/create-pretender";
 import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import I18n from "discourse-i18n";
 
@@ -32,10 +36,22 @@ acceptance("Create Account", function () {
       .dom("#username-validation.good")
       .exists("the username validation is good");
 
+    pretender.post("/u", (request) => {
+      assert.step("request");
+      const data = parsePostData(request.requestBody);
+      assert.strictEqual(data.name, "Dr. Good Tuna");
+      assert.strictEqual(data.password, "cool password bro");
+      assert.strictEqual(data.email, "good.tuna@test.com");
+      assert.strictEqual(data.username, "good-tuna");
+      return response({ success: true });
+    });
+
     await click(".modal-footer .btn-primary");
     assert
       .dom(".modal-footer .btn-primary:disabled")
       .exists("create account is disabled");
+
+    assert.verifySteps(["request"]);
   });
 
   test("validate username", async function (assert) {
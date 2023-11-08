hanzoai/forum

commit f650ab6f8016713053d604a47923cdc91fc921a4

DEV: Refactor watched words (#24163)
- Ignore only invalid words, not all words if one of them is invalid

- The naming scheme for methods was inconsistent

- Optimize regular expressions

app/services/word_watcher.rb
@@ -18,106 +18,109 @@ class WordWatcher
     @cache_enabled
   end
 
+  def self.cache_key(action)
+    "watched-words-list:v#{CACHE_VERSION}:#{action}"
+  end
+
+  def self.clear_cache!
+    WatchedWord.actions.each { |action, _| Discourse.cache.delete(cache_key(action)) }
+  end
+
   def self.words_for_action(action)
     WatchedWord
       .where(action: WatchedWord.actions[action.to_sym])
       .limit(WatchedWord::MAX_WORDS_PER_ACTION)
       .order(:id)
       .pluck(:word, :replacement, :case_sensitive)
-      .to_h do |w, r, c|
-        [w, { word: word_to_regexp(w, whole: false), replacement: r, case_sensitive: c }.compact]
-      end
+      .to_h { |w, r, c| [w, { word: w, replacement: r, case_sensitive: c }.compact] }
   end
 
-  def self.words_for_action_exists?(action)
+  def self.words_for_action_exist?(action)
     WatchedWord.where(action: WatchedWord.actions[action.to_sym]).exists?
   end
 
-  def self.get_cached_words(action)
+  def self.cached_words_for_action(action)
     if cache_enabled?
       Discourse
         .cache
-        .fetch(word_matcher_regexp_key(action), expires_in: 1.day) do
-          words_for_action(action).presence
-        end
+        .fetch(cache_key(action), expires_in: 1.day) { words_for_action(action).presence }
     else
       words_for_action(action).presence
     end
   end
 
-  def self.serializable_word_matcher_regexp(action, engine: :ruby)
-    word_matcher_regexp_list(action, engine: engine).map do |r|
-      { r.source => { case_sensitive: !r.casefold? } }
+  def self.regexps_for_action(action, engine: :ruby)
+    cached_words_for_action(action)&.to_h do |word, attrs|
+      [word_to_regexp(word, engine: engine), attrs]
     end
   end
 
   # This regexp is run in miniracer, and the client JS app
   # Make sure it is compatible with major browsers when changing
   # hint: non-chrome browsers do not support 'lookbehind'
-  def self.word_matcher_regexp_list(action, engine: :ruby, raise_errors: false)
-    words = get_cached_words(action)
+  def self.compiled_regexps_for_action(action, engine: :ruby, raise_errors: false)
+    words = cached_words_for_action(action)
     return [] if words.blank?
 
-    grouped_words = { case_sensitive: [], case_insensitive: [] }
+    words
+      .values
+      .group_by { |attrs| attrs[:case_sensitive] ? :case_sensitive : :case_insensitive }
+      .map do |group_key, attrs_list|
+        words = attrs_list.map { |attrs| attrs[:word] }
 
-    words.each do |word, attrs|
-      word = word_to_regexp(word, whole: SiteSetting.watched_words_regular_expressions?)
-      group_key = attrs[:case_sensitive] ? :case_sensitive : :case_insensitive
-      grouped_words[group_key] << word
-    end
+        # Compile all watched words into a single regular expression
+        regexp =
+          words
+            .map do |word|
+              r = word_to_regexp(word, match_word: SiteSetting.watched_words_regular_expressions?)
+              begin
+                r if Regexp.new(r)
+              rescue RegexpError
+                raise if raise_errors
+              end
+            end
+            .select { |r| r.present? }
+            .join("|")
 
-    regexps = grouped_words.select { |_, w| w.present? }.transform_values { |w| w.join("|") }
+        # Add word boundaries to the regexp for regular watched words
+        regexp =
+          match_word_regexp(
+            regexp,
+            engine: engine,
+          ) if !SiteSetting.watched_words_regular_expressions?
 
-    if !SiteSetting.watched_words_regular_expressions?
-      regexps.transform_values! { |regexp| wrap_regexp(regexp, engine: engine) }
-    end
-
-    regexps.map { |c, regexp| Regexp.new(regexp, c == :case_sensitive ? nil : Regexp::IGNORECASE) }
-  rescue RegexpError
-    raise if raise_errors
-    [] # Admin will be alerted via admin_dashboard_data.rb
+        # Add case insensitive flag if needed
+        Regexp.new(regexp, group_key == :case_sensitive ? nil : Regexp::IGNORECASE)
+      end
   end
 
-  def self.word_matcher_regexps(action, engine: :ruby)
-    get_cached_words(action)&.to_h { |word, attrs| [word_to_regexp(word, engine: engine), attrs] }
+  def self.serialized_regexps_for_action(action, engine: :ruby)
+    compiled_regexps_for_action(action, engine: engine).map do |r|
+      { r.source => { case_sensitive: !r.casefold? } }
+    end
   end
 
-  def self.word_to_regexp(word, engine: :ruby, whole: true)
+  def self.word_to_regexp(word, engine: :ruby, match_word: true)
     if SiteSetting.watched_words_regular_expressions?
-      # Strip Ruby regexp format if present
-      regexp = word.start_with?("(?-mix:") ? word[7..-2] : word
-      regexp = "(#{regexp})" if whole
-      return regexp
-    end
-
-    # Escape regular expression. Avoid using Regexp.escape because it escapes
-    # more characters than it should (for example, whitespaces)
-    regexp = word.gsub(/([.*+?^${}()|\[\]\\])/, '\\\\\1')
-
-    # Handle wildcards
-    regexp = regexp.gsub("\\*", '\S*')
-
-    regexp = wrap_regexp(regexp, engine: engine) if whole
-
-    regexp
-  end
-
-  def self.wrap_regexp(regexp, engine: :ruby)
-    if engine == :js
-      "(?:\\P{L}|^)(#{regexp})(?=\\P{L}|$)"
-    elsif engine == :ruby
-      "(?:[^[:word:]]|^)(#{regexp})(?=[^[:word:]]|$)"
+      regexp = word
+      regexp = "(#{regexp})" if match_word
+      regexp
     else
-      "(?:\\W|^)(#{regexp})(?=\\W|$)"
-    end
-  end
+      # Convert word to regex by escaping special characters in a regexp.
+      # Avoid using Regexp.escape because it escapes more characters than
+      # it should (for example, whitespaces, dashes, etc)
+      regexp = word.gsub(/([.*+?^${}()|\[\]\\])/, '\\\\\1')
 
-  def self.word_matcher_regexp_key(action)
-    "watched-words-list:v#{CACHE_VERSION}:#{action}"
+      # Convert wildcards to regexp
+      regexp = regexp.gsub("\\*", '\S*')
+
+      regexp = match_word_regexp(regexp, engine: engine) if match_word
+      regexp
+    end
   end
 
   def self.censor(html)
-    regexps = word_matcher_regexp_list(:censor)
+    regexps = compiled_regexps_for_action(:censor)
     return html if regexps.blank?
 
     doc = Nokogiri::HTML5.fragment(html)
@@ -133,7 +136,7 @@ class WordWatcher
   def self.censor_text(text)
     return text if text.blank?
 
-    regexps = word_matcher_regexp_list(:censor)
+    regexps = compiled_regexps_for_action(:censor)
     return text if regexps.blank?
 
     regexps.inject(text) { |txt, regexp| censor_text_with_regexp(txt, regexp) }
@@ -156,10 +159,6 @@ class WordWatcher
     text
   end
 
-  def self.clear_cache!
-    WatchedWord.actions.each { |a, i| Discourse.cache.delete word_matcher_regexp_key(a) }
-  end
-
   def requires_approval?
     word_matches_for_action?(:require_approval)
   end
@@ -177,7 +176,7 @@ class WordWatcher
   end
 
   def word_matches_for_action?(action, all_matches: false)
-    regexps = self.class.word_matcher_regexp_list(action)
+    regexps = self.class.compiled_regexps_for_action(action)
     return if regexps.blank?
 
     match_list = []
@@ -254,14 +253,28 @@ class WordWatcher
 
   private_class_method :censor_text_with_regexp
 
-  private
+  # Returns a regexp that transforms a regular expression into a regular
+  # expression that matches a whole word.
+  def self.match_word_regexp(regexp, engine: :ruby)
+    if engine == :js
+      "(?:\\P{L}|^)(#{regexp})(?=\\P{L}|$)"
+    elsif engine == :ruby
+      "(?:[^[:word:]]|^)(#{regexp})(?=[^[:word:]]|$)"
+    else
+      raise "unknown regexp engine: #{engine}"
+    end
+  end
+
+  private_class_method :match_word_regexp
 
   def self.replace(text, watch_word_type)
-    word_matcher_regexps(watch_word_type)
+    regexps_for_action(watch_word_type)
       .to_a
       .reduce(text) do |t, (word_regexp, attrs)|
         case_flag = attrs[:case_sensitive] ? nil : Regexp::IGNORECASE
         replace_text_with_regexp(t, Regexp.new(word_regexp, case_flag), attrs[:replacement])
       end
   end
+
+  private_class_method :replace
 end

app/controllers/admin/watched_words_controller.rb
@@ -6,7 +6,7 @@ class Admin::WatchedWordsController < Admin::StaffController
   skip_before_action :check_xhr, only: [:download]
 
   def index
-    watched_words = WatchedWord.by_action
+    watched_words = WatchedWord.order(:action, :word)
     watched_words =
       watched_words.where.not(action: WatchedWord.actions[:tag]) if !SiteSetting.tagging_enabled
     render_json_dump WatchedWordListSerializer.new(watched_words, scope: guardian, root: false)

app/models/admin_dashboard_data.rb
@@ -421,7 +421,7 @@ class AdminDashboardData
   def watched_words_check
     WatchedWord.actions.keys.each do |action|
       begin
-        WordWatcher.word_matcher_regexp_list(action, raise_errors: true)
+        WordWatcher.compiled_regexps_for_action(action, raise_errors: true)
       rescue RegexpError => e
         translated_action = I18n.t("admin_js.admin.watched_words.actions.#{action}")
         I18n.t(

app/models/watched_word.rb
@@ -1,6 +1,39 @@
 # frozen_string_literal: true
 
 class WatchedWord < ActiveRecord::Base
+  MAX_WORDS_PER_ACTION = 2000
+
+  before_validation { self.word = WatchedWord.normalize_word(self.word) }
+
+  before_validation do
+    if self.action == WatchedWord.actions[:link] && self.replacement !~ %r{\Ahttps?://}
+      self.replacement =
+        "#{Discourse.base_url}#{self.replacement&.starts_with?("/") ? "" : "/"}#{self.replacement}"
+    end
+  end
+
+  validates :word, presence: true, uniqueness: true, length: { maximum: 100 }
+  validates :action, presence: true
+  validate :replacement_is_url, if: -> { action == WatchedWord.actions[:link] }
+  validate :replacement_is_tag_list, if: -> { action == WatchedWord.actions[:tag] }
+
+  validates_each :word do |record, attr, val|
+    if WatchedWord.where(action: record.action).count >= MAX_WORDS_PER_ACTION
+      record.errors.add(:word, :too_many)
+    end
+  end
+
+  after_save -> { WordWatcher.clear_cache! }
+  after_destroy -> { WordWatcher.clear_cache! }
+
+  scope :for,
+        ->(word:) {
+          where(
+            "(word ILIKE :word AND case_sensitive = 'f') OR (word LIKE :word AND case_sensitive = 't')",
+            word: word,
+          )
+        }
+
   def self.actions
     @actions ||=
       Enum.new(
@@ -15,65 +48,15 @@ class WatchedWord < ActiveRecord::Base
       )
   end
 
-  MAX_WORDS_PER_ACTION = 2000
-
-  before_validation do
-    self.word = self.class.normalize_word(self.word)
-    if self.action == WatchedWord.actions[:link] && !(self.replacement =~ %r{\Ahttps?://})
-      self.replacement =
-        "#{Discourse.base_url}#{self.replacement&.starts_with?("/") ? "" : "/"}#{self.replacement}"
-    end
-  end
-
-  validates :word, presence: true, uniqueness: true, length: { maximum: 100 }
-  validates :action, presence: true
-
-  validate :replacement_is_url, if: -> { action == WatchedWord.actions[:link] }
-  validate :replacement_is_tag_list, if: -> { action == WatchedWord.actions[:tag] }
-
-  validates_each :word do |record, attr, val|
-    if WatchedWord.where(action: record.action).count >= MAX_WORDS_PER_ACTION
-      record.errors.add(:word, :too_many)
-    end
-  end
-
-  after_save :clear_cache
-  after_destroy :clear_cache
-
-  scope :by_action, -> { order("action ASC, word ASC") }
-  scope :for,
-        ->(word:) {
-          where(
-            "(word ILIKE :word AND case_sensitive = 'f') OR (word LIKE :word AND case_sensitive = 't')",
-            word: word,
-          )
-        }
-
-  def self.normalize_word(w)
-    w.strip.squeeze("*")
-  end
-
-  def replacement_is_url
-    errors.add(:base, :invalid_url) if !(replacement =~ URI.regexp)
-  end
-
-  def replacement_is_tag_list
-    tag_list = replacement&.split(",")
-    tags = Tag.where(name: tag_list)
-    if (tag_list.blank? || tags.empty? || tag_list.size != tags.size)
-      errors.add(:base, :invalid_tag_list)
-    end
-  end
-
   def self.create_or_update_word(params)
-    new_word = normalize_word(params[:word])
-    w = self.for(word: new_word).first_or_initialize(word: new_word)
-    w.replacement = params[:replacement] if params[:replacement]
-    w.action_key = params[:action_key] if params[:action_key]
-    w.action = params[:action] if params[:action]
-    w.case_sensitive = params[:case_sensitive] if !params[:case_sensitive].nil?
-    w.save
-    w
+    word = normalize_word(params[:word])
+    word = self.for(word: word).first_or_initialize(word: word)
+    word.replacement = params[:replacement] if params[:replacement]
+    word.action_key = params[:action_key] if params[:action_key]
+    word.action = params[:action] if params[:action]
+    word.case_sensitive = params[:case_sensitive] if !params[:case_sensitive].nil?
+    word.save
+    word
   end
 
   def self.has_replacement?(action)
@@ -81,7 +64,7 @@ class WatchedWord < ActiveRecord::Base
   end
 
   def action_key=(arg)
-    self.action = self.class.actions[arg.to_sym]
+    self.action = WatchedWord.actions[arg.to_sym]
   end
 
   def action_log_details
@@ -92,8 +75,26 @@ class WatchedWord < ActiveRecord::Base
     end
   end
 
-  def clear_cache
-    WordWatcher.clear_cache!
+  private
+
+  def self.normalize_word(word)
+    # When a regular expression is converted to a string, it is wrapped with
+    # '(?-mix:' and ')'
+    word = word[7..-2] if word.start_with?("(?-mix:")
+
+    word.strip.squeeze("*")
+  end
+
+  def replacement_is_url
+    errors.add(:base, :invalid_url) if replacement !~ URI.regexp
+  end
+
+  def replacement_is_tag_list
+    tag_list = replacement&.split(",")
+    tags = Tag.where(name: tag_list)
+    if tag_list.blank? || tags.empty? || tag_list.size != tags.size
+      errors.add(:base, :invalid_tag_list)
+    end
   end
 end
 

app/serializers/site_serializer.rb
@@ -210,7 +210,7 @@ class SiteSerializer < ApplicationSerializer
   end
 
   def censored_regexp
-    WordWatcher.serializable_word_matcher_regexp(:censor, engine: :js)
+    WordWatcher.serialized_regexps_for_action(:censor, engine: :js)
   end
 
   def custom_emoji_translation
@@ -226,11 +226,11 @@ class SiteSerializer < ApplicationSerializer
   end
 
   def watched_words_replace
-    WordWatcher.word_matcher_regexps(:replace, engine: :js)
+    WordWatcher.regexps_for_action(:replace, engine: :js)
   end
 
   def watched_words_link
-    WordWatcher.word_matcher_regexps(:link, engine: :js)
+    WordWatcher.regexps_for_action(:link, engine: :js)
   end
 
   def categories

app/serializers/watched_word_list_serializer.rb
@@ -18,7 +18,7 @@ class WatchedWordListSerializer < ApplicationSerializer
   def compiled_regular_expressions
     expressions = {}
     actions.each do |action|
-      expressions[action] = WordWatcher.serializable_word_matcher_regexp(action, engine: :js)
+      expressions[action] = WordWatcher.serialized_regexps_for_action(action, engine: :js)
     end
     expressions
   end

lib/new_post_manager.rb
@@ -202,8 +202,8 @@ class NewPostManager
   def self.queue_enabled?
     SiteSetting.approve_post_count > 0 || SiteSetting.approve_unless_trust_level.to_i > 0 ||
       SiteSetting.approve_new_topics_unless_trust_level.to_i > 0 ||
-      SiteSetting.approve_unless_staged ||
-      WordWatcher.words_for_action_exists?(:require_approval) || handlers.size > 1
+      SiteSetting.approve_unless_staged || WordWatcher.words_for_action_exist?(:require_approval) ||
+      handlers.size > 1
   end
 
   def initialize(user, args)

lib/pretty_text.rb
@@ -203,9 +203,9 @@ module PrettyText
         __optInput.emojiUnicodeReplacer = __emojiUnicodeReplacer;
         __optInput.emojiDenyList = #{Emoji.denied.to_json};
         __optInput.lookupUploadUrls = __lookupUploadUrls;
-        __optInput.censoredRegexp = #{WordWatcher.serializable_word_matcher_regexp(:censor, engine: :js).to_json};
-        __optInput.watchedWordsReplace = #{WordWatcher.word_matcher_regexps(:replace, engine: :js).to_json};
-        __optInput.watchedWordsLink = #{WordWatcher.word_matcher_regexps(:link, engine: :js).to_json};
+        __optInput.censoredRegexp = #{WordWatcher.serialized_regexps_for_action(:censor, engine: :js).to_json};
+        __optInput.watchedWordsReplace = #{WordWatcher.regexps_for_action(:replace, engine: :js).to_json};
+        __optInput.watchedWordsLink = #{WordWatcher.regexps_for_action(:link, engine: :js).to_json};
         __optInput.additionalOptions = #{Site.markdown_additional_options.to_json};
         __optInput.avatar_sizes = #{SiteSetting.avatar_sizes.to_json};
       JS

commit c46fa261a0b09f302f4fccc35e732fa310fbdab0

FIX: Replace watched words with wildcards (#24279)
These have been broken since f650ab6f8016713053d604a47923cdc91fc921a4
because watched words were not correctly transformed to regexps.
This partially reverts the changes.

app/assets/javascripts/discourse-common/addon/utils/watched-words.js
@@ -5,5 +5,5 @@ export function createWatchedWordRegExp(word) {
 
 export function toWatchedWord(regexp) {
   const [[regexpString, options]] = Object.entries(regexp);
-  return { regexp: regexpString, ...options };
+  return { ...options, regexp: regexpString };
 }

app/assets/javascripts/discourse-markdown-it/src/features/watched-words.js
@@ -61,7 +61,7 @@ export function setup(helper) {
           const word = toWatchedWord({ [regexpString]: options });
 
           matchers.push({
-            word: new RegExp(options.word, options.case_sensitive ? "" : "i"),
+            word: new RegExp(options.regexp, options.case_sensitive ? "" : "i"),
             pattern: createWatchedWordRegExp(word),
             replacement: options.replacement,
             link: false,
@@ -76,7 +76,7 @@ export function setup(helper) {
           const word = toWatchedWord({ [regexpString]: options });
 
           matchers.push({
-            word: new RegExp(options.word, options.case_sensitive ? "" : "i"),
+            word: new RegExp(options.regexp, options.case_sensitive ? "" : "i"),
             pattern: createWatchedWordRegExp(word),
             replacement: options.replacement,
             link: true,

app/services/word_watcher.rb
@@ -32,7 +32,12 @@ class WordWatcher
       .limit(WatchedWord::MAX_WORDS_PER_ACTION)
       .order(:id)
       .pluck(:word, :replacement, :case_sensitive)
-      .to_h { |w, r, c| [w, { word: w, replacement: r, case_sensitive: c }.compact] }
+      .to_h do |w, r, c|
+        [
+          word_to_regexp(w, match_word: false),
+          { word: w, replacement: r, case_sensitive: c }.compact,
+        ]
+      end
   end
 
   def self.words_for_action_exist?(action)
@@ -50,8 +55,8 @@ class WordWatcher
   end
 
   def self.regexps_for_action(action, engine: :ruby)
-    cached_words_for_action(action)&.to_h do |word, attrs|
-      [word_to_regexp(word, engine: engine), attrs]
+    cached_words_for_action(action)&.to_h do |_, attrs|
+      [word_to_regexp(attrs[:word], engine: engine), attrs]
     end
   end
 

lib/topic_creator.rb
@@ -191,8 +191,8 @@ class TopicCreator
     if watched_words.present?
       word_watcher = WordWatcher.new("#{@opts[:title]} #{@opts[:raw]}")
       word_watcher_tags = topic.tags.map(&:name)
-      watched_words.each do |word, opts|
-        if word_watcher.word_matches?(word, case_sensitive: opts[:case_sensitive])
+      watched_words.each do |_, opts|
+        if word_watcher.word_matches?(opts[:word], case_sensitive: opts[:case_sensitive])
           word_watcher_tags += opts[:replacement].split(",")
         end
       end

spec/lib/pretty_text_spec.rb
@@ -1981,6 +1981,19 @@ HTML
       HTML
     end
 
+    it "replaces words with wildcards" do
+      Fabricate(
+        :watched_word,
+        action: WatchedWord.actions[:replace],
+        word: "*dolor*",
+        replacement: "something else",
+      )
+
+      expect(PrettyText.cook("Lorem ipsum xdolorx sit amet")).to match_html(<<~HTML)
+        <p>Lorem ipsum something else sit amet</p>
+      HTML
+    end
+
     it "replaces words with links" do
       Fabricate(
         :watched_word,
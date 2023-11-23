hanzoai/forum

commit 136e5e3c730790435469836c022590395360b2d1

DEV: Don't install plugin gems twice (#16192)
Missing plugin gems are installed when the app is being loaded.

That means when you run `bin/rails plugin:install_all_gems` it first installs missing gems and then reinstalls all gems…

Also, the method these rake tasks were using to install gems was very crude, and the regex there was incorrect which resulted in failures in certain cases. Though that didn't matter since those gems were being installed using a correct method just moments before…

lib/tasks/plugin.rake
@@ -127,7 +127,6 @@ end
 
 desc 'pull a compatible plugin version'
 task 'plugin:pull_compatible', :plugin do |t, args|
-
   plugin = ENV['PLUGIN'] || ENV['plugin'] || args[:plugin]
   plugin_path = plugin
   plugin = File.basename(plugin)
@@ -154,26 +153,18 @@ end
 
 desc 'install all plugin gems'
 task 'plugin:install_all_gems' do |t|
-  plugins = Dir.glob(File.expand_path('plugins/*')).select { |f| File.directory? f }
-  plugins.each do |plugin|
-    Rake::Task['plugin:install_gems'].invoke(plugin)
-    Rake::Task['plugin:install_gems'].reenable
-  end
+  # Left intentionally blank.
+  # When the app is being loaded, all missing gems are installed
+  # See: lib/plugin_gem.rb
+  puts "Done"
 end
 
 desc 'install plugin gems'
 task 'plugin:install_gems', :plugin do |t, args|
-  plugin = ENV['PLUGIN'] || ENV['plugin'] || args[:plugin]
-  plugin_path = plugin + "/plugin.rb"
-
-  if File.file?(plugin_path)
-    File.open(plugin_path).each do |l|
-      next if !l.start_with? "gem"
-      next unless /gem\s['"](.*)['"],\s['"](.*)['"]/.match(l)
-      puts "gem install #{$1} -v #{$2} -i #{plugin}/gems/#{RUBY_VERSION} --no-document --ignore-dependencies --no-user-install"
-      system("gem install #{$1} -v #{$2} -i #{plugin}/gems/#{RUBY_VERSION} --no-document --ignore-dependencies --no-user-install")
-    end
-  end
+  # Left intentionally blank.
+  # When the app is being loaded, all missing gems are installed
+  # See: lib/plugin_gem.rb
+  puts "Done"
 end
 
 desc 'run plugin specs'

commit 3d7a7d7b02fc8781def8cd9c96305066d5c32fd8

FIX: `plugin:install_all_gems` Rake task not installing plugin gem (#24522)
Why this change?

This regressed in 136e5e3c730790435469836c022590395360b2d1 where the
commit assumes that plugin gems are always installed when the
`plugin:install_all_gems` Rake task is ran as it would run the our Rails
initializers which activates plugins and install the gems. However, this
assumption only holds true when the `LOAD_PLUGINS` is present and set to
`1`.

What does this change do?

This commit changes the `plugin:install_all_gems` to load the Rails
environment with `LOAD_PLUGINS` set to `1` such that the plugin gems
will be installed as part of our initialization process for the app.

The commit also removes the `plugin:install_gems` Rake task which is
currently a noop and does not seem to be used anywhere..

lib/tasks/plugin.rake
@@ -170,18 +170,8 @@ end
 
 desc "install all plugin gems"
 task "plugin:install_all_gems" do |t|
-  # Left intentionally blank.
-  # When the app is being loaded, all missing gems are installed
-  # See: lib/plugin_gem.rb
-  puts "Done"
-end
-
-desc "install plugin gems"
-task "plugin:install_gems", :plugin do |t, args|
-  # Left intentionally blank.
-  # When the app is being loaded, all missing gems are installed
-  # See: lib/plugin_gem.rb
-  puts "Done"
+  ENV["LOAD_PLUGINS"] = "1"
+  Rake::Task["environment"].invoke
 end
 
 def spec(plugin, parallel: false, argv: nil)
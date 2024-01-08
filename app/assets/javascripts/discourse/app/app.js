hanzoai/forum

commit d6046e381505e420b091118d0fc5204035d0e874

DEV: Remove JQuery from app.js (#15449)

app/assets/javascripts/discourse/app/app.js
@@ -52,7 +52,7 @@ const Discourse = Application.extend({
 
   // Start up the Discourse application by running all the initializers we've defined.
   start() {
-    $("noscript").remove();
+    document.querySelector("noscript")?.remove();
 
     Object.keys(requirejs._eak_seen).forEach((key) => {
       if (/\/pre\-initializers\//.test(key)) {

commit 74708591ae52c31395ed26221cff9c0250250ef7

FIX: Remove all noscript elements, not just the first one (#25145)
Regressed in d6046e3

app/assets/javascripts/discourse/app/app.js
@@ -30,7 +30,7 @@ const Discourse = Application.extend({
   start() {
     printDebugInfo();
 
-    document.querySelector("noscript")?.remove();
+    document.querySelectorAll("noscript").forEach((el) => el.remove());
 
     // Rewire event handling to eliminate event delegation for better compat
     // between Glimmer and Classic components.
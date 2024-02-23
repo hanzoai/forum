hanzoai/forum

commit eafd0cc948ac674e0d0484f89fcbf03511072791

DEV: Do not process requests initiated by browser in a different example (#25809)
Why this change?

We noticed that running `LOAD_PLUGINS=1 rspec --seed=38855 plugins/chat/spec/system/chat_new_message_spec.rb` locally
results in the system tests randomly failing. When we inspected the
request logs closely, we noticed that a `/presence/get` request from a
previous rspec example was being processed when a new rspec example is
already being run. We know it was from the previous rspec example
because inspecting the auth token showed the request using the auth
token of a user from the previous example. However, when a request using
an auth token from a previous example is used it ends up logging out the
same user on the server side because the user id in the cookie is the same
due to the use of `fab!`.

I did some research and there is apparently no way to wait until all
inflight requests by the browser has completed through capybara or
selenium. Therefore, we will add an identifier by attaching a cookie to all non-xhr requests so that
xhr requests which are triggered subsequently will contain the cookie in the request.

In the `BlockRequestsMiddleware` middleware, we will then reject any
requests when the value of the identifier in the cookie does not match the current rspec's example
location.

To see the problem locally, change `Auth::DefaultCurrentUserProvider.find_v1_auth_cookie` to the following:

```
  def self.find_v1_auth_cookie(env)
    return env[DECRYPTED_AUTH_COOKIE] if env.key?(DECRYPTED_AUTH_COOKIE)

    env[DECRYPTED_AUTH_COOKIE] = begin
      request = ActionDispatch::Request.new(env)
      cookie = request.cookies[TOKEN_COOKIE]

      # don't even initialize a cookie jar if we don't have a cookie at all
      if cookie&.valid_encoding? && cookie.present?
        puts "#{env["REQUEST_PATH"]} #{request.cookie_jar.encrypted[TOKEN_COOKIE]&.with_indifferent_access}"

config/initializers/200-first_middlewares.rb
@@ -33,6 +33,10 @@ if Rails.env.test?
                                          RailsMultisite::DiscoursePatches.config
 
   class BlockRequestsMiddleware
+    RSPEC_CURRENT_EXAMPLE_COOKIE_STRING = "rspec_current_example_location"
+
+    cattr_accessor :current_example_location
+
     @@block_requests = false
 
     def self.block_requests!
@@ -48,7 +52,17 @@ if Rails.env.test?
     end
 
     def call(env)
-      if @@block_requests
+      request = Rack::Request.new(env)
+
+      if request.xhr? &&
+           (
+             @@block_requests ||
+               (
+                 self.class.current_example_location.present? &&
+                   self.class.current_example_location !=
+                     request.cookies[RSPEC_CURRENT_EXAMPLE_COOKIE_STRING]
+               )
+           )
         [503, { "Content-Type" => "text/plain" }, ["Blocked by BlockRequestsMiddleware"]]
       else
         @app.call(env)

plugins/chat/spec/system/chat_new_message_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "Chat New Message from params", type: :system do
       expect(page).to have_current_path("/chat/c/#{user_2.username}/#{Chat::Channel.last.id}")
     end
 
-    xit "redirects to chat channel if recipients param is missing" do
+    it "redirects to chat channel if recipients param is missing" do
       visit("/chat/new-message")
 
       # chat service selects public channel from getIdealFirstChannelId

plugins/chat/spec/system/single_thread_spec.rb
@@ -20,7 +20,7 @@ describe "Single thread in side panel", type: :system do
 
     before { channel.update!(threading_enabled: false) }
 
-    it "does not open the side panel for a single thread", capture_log: true do
+    it "does not open the side panel for a single thread" do
       thread =
         chat_thread_chain_bootstrap(channel: channel, users: [current_user, Fabricate(:user)])
       chat_page.visit_channel(channel)

spec/rails_helper.rb
@@ -152,6 +152,7 @@ module TestSetup
 
     Middleware::AnonymousCache.disable_anon_cache
     BlockRequestsMiddleware.allow_requests!
+    BlockRequestsMiddleware.current_example_location = nil
   end
 end
 
@@ -592,6 +593,14 @@ RSpec.configure do |config|
         ActiveRecord::Base.connection.schema_cache.add(table)
       end
 
+      ApplicationController.before_action do
+        if BlockRequestsMiddleware.current_example_location
+          cookies[
+            BlockRequestsMiddleware::RSPEC_CURRENT_EXAMPLE_COOKIE_STRING
+          ] = BlockRequestsMiddleware.current_example_location
+        end
+      end
+
       system_tests_initialized = true
     end
 
@@ -609,6 +618,8 @@ RSpec.configure do |config|
     driven_by driver.join("_").to_sym
 
     setup_system_test
+
+    BlockRequestsMiddleware.current_example_location = example.location
   end
 
   config.after(:each, type: :system) do |example|

commit b1f9b151fa4d7ab2e5082c47ac713ebf008be055

DEV: Fix connections timeout in system test (#25835)
Why this change?

This regressed in eafd0cc948ac674e0d0484f89fcbf03511072791 because we
had a `request.xhr?` check before we decide to block requests. However,
there could not none-xhr requests which we need to block as well at the
end of each system test when `@@block_requests` is true.

This also reverts commit 0e528ee6118bc4812dacedfba228d234ed0bf91f.

.github/workflows/tests.yml
@@ -254,7 +254,6 @@ jobs:
         if: matrix.build_type == 'system' && matrix.target == 'chat'
         env:
           CHECKOUT_TIMEOUT: 10
-          DEBUG_AR_CONNECTION_QUEUE: 1
         run: LOAD_PLUGINS=1 RAILS_ENABLE_TEST_LOG=1 RAILS_TEST_LOG_LEVEL=error PARALLEL_TEST_PROCESSORS=4 bin/turbo_rspec --use-runtime-info --profile=50 --verbose --format documentation plugins/chat/spec/system
         timeout-minutes: 30
 

config/initializers/200-first_middlewares.rb
@@ -54,15 +54,14 @@ if Rails.env.test?
     def call(env)
       request = Rack::Request.new(env)
 
-      if request.xhr? &&
-           (
-             @@block_requests ||
-               (
-                 self.class.current_example_location.present? &&
-                   self.class.current_example_location !=
-                     request.cookies[RSPEC_CURRENT_EXAMPLE_COOKIE_STRING]
-               )
-           )
+      if (
+           @@block_requests ||
+             (
+               request.xhr? && self.class.current_example_location.present? &&
+                 self.class.current_example_location !=
+                   request.cookies[RSPEC_CURRENT_EXAMPLE_COOKIE_STRING]
+             )
+         )
         [
           503,
           { "Content-Type" => "text/plain" },

lib/freedom_patches/debug_connection_queue.rb
@@ -1,40 +1,0 @@
-# frozen_string_literal: true
-
-if ENV["DEBUG_AR_CONNECTION_QUEUE"] == "1"
-  module QueuePatch
-    # Add +element+ to the queue.  Never blocks.
-    def add(element)
-      puts "::group::##{Process.pid} Adding element to the queue"
-      puts Thread.current.backtrace.first(30).join("\n")
-      puts "::endgroup::"
-      super
-    end
-
-    # If +element+ is in the queue, remove and return it, or +nil+.
-    def delete(element)
-      puts "::group::##{Process.pid} Delete element from the queue"
-      puts Thread.current.backtrace.first(30).join("\n")
-      puts "::endgroup::"
-      super
-    end
-
-    # Remove all elements from the queue.
-    def clear
-      puts "::group::##{Process.pid} Clear all elements from the queue"
-      puts Thread.current.backtrace.first(30).join("\n")
-      puts "::endgroup::"
-      super
-    end
-
-    private
-
-    def remove
-      puts "::group::##{Process.pid} Removing element from the queue"
-      puts Thread.current.backtrace.first(30).join("\n")
-      puts "::endgroup::"
-      super
-    end
-  end
-
-  ActiveRecord::ConnectionAdapters::ConnectionPool::Queue.prepend(QueuePatch)
-end
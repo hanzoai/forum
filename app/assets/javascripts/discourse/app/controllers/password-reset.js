hanzoai/forum

commit c50b7c9266643ff8aeb8b60edf96da227cca14fc

UX: Streamline reset password page (#27341)
This commit includes various UX improvements to the reset
password page:

* Introduce a `hide-application-header-buttons` helper to do the following:
  * Hide Sign Up and Log In buttons, they are not necessary on this flow
  * Hide the sidebar, it is a distraction on this flow
* Improve messaging when a 2FA confirmation is required first
* Improve display of server-side ActiveRecord model validation errors
  in password form, e.g. instead of "is the same as your current password"
  we do "The password is the same as your current password"
* Move password tip to next line below input and move caps lock hint
  inline with Show/Hide password toggle
* Add system specs for 2FA flow on reset password page
* Fixes a computed property conflict issue on the password reset
   page when toggling 2FA methods

app/assets/javascripts/discourse/app/controllers/password-reset.js
@@ -35,6 +35,11 @@ export default Controller.extend(PasswordValidation, {
   redirected: false,
   maskPassword: true,
 
+  init() {
+    this._super(...arguments);
+    this.set("selectedSecondFactorMethod", this.secondFactorMethod);
+  },
+
   @discourseComputed()
   continueButtonText() {
     return I18n.t("password_reset.continue", {
@@ -73,7 +78,7 @@ export default Controller.extend(PasswordValidation, {
           password: this.accountPassword,
           second_factor_token:
             this.securityKeyCredential || this.secondFactorToken,
-          second_factor_method: this.secondFactorMethod,
+          second_factor_method: this.selectedSecondFactorMethod,
           timezone: moment.tz.guess(),
         },
       })
@@ -109,7 +114,7 @@ export default Controller.extend(PasswordValidation, {
               this.rejectedPasswords.pushObject(this.accountPassword);
               this.rejectedPasswordsMessages.set(
                 this.accountPassword,
-                result.errors.password[0]
+                (result.friendly_messages || []).join("\n")
               );
             }
 

spec/system/login_spec.rb
@@ -8,6 +8,19 @@ shared_examples "login scenarios" do
 
   before { Jobs.run_immediately! }
 
+  def wait_for_email_link(user, type)
+    wait_for(timeout: 5) { ActionMailer::Base.deliveries.count != 0 }
+    mail = ActionMailer::Base.deliveries.last
+    expect(mail.to).to contain_exactly(user.email)
+    if type == :reset_password
+      mail.body.to_s[%r{/u/password-reset/\S+}]
+    elsif type == :activation
+      mail.body.to_s[%r{/u/activate-account/\S+}]
+    elsif type == :email_login
+      mail.body.to_s[%r{/session/email-login/\S+}]
+    end
+  end
+
   context "with username and password" do
     it "can login" do
       EmailToken.confirm(Fabricate(:email_token, user: user).token)
@@ -25,11 +38,7 @@ shared_examples "login scenarios" do
       expect(page).to have_css(".not-activated-modal")
       login_modal.click(".activation-controls button.resend")
 
-      wait_for(timeout: 5) { ActionMailer::Base.deliveries.count != 0 }
-
-      mail = ActionMailer::Base.deliveries.last
-      expect(mail.to).to contain_exactly(user.email)
-      activation_link = mail.body.to_s[%r{/u/activate-account/\S+}]
+      activation_link = wait_for_email_link(user, :activation)
       visit activation_link
 
       find("#activate-account-button").click
@@ -53,11 +62,8 @@ shared_examples "login scenarios" do
       login_modal.find("#modal-alert a").click
       find("button.forgot-password-reset").click
 
-      wait_for(timeout: 5) { ActionMailer::Base.deliveries.count != 0 }
-
-      mail = ActionMailer::Base.deliveries.last
-      expect(mail.to).to contain_exactly(user.email)
-      expect(mail.body).to match(%r{/u/password-reset/\S+})
+      reset_password_link = wait_for_email_link(user, :reset_password)
+      expect(reset_password_link).to be_present
     end
 
     it "can reset password" do
@@ -66,15 +72,11 @@ shared_examples "login scenarios" do
       login_modal.forgot_password
       find("button.forgot-password-reset").click
 
-      wait_for(timeout: 5) { ActionMailer::Base.deliveries.count != 0 }
-
-      mail = ActionMailer::Base.deliveries.last
-      expect(mail.to).to contain_exactly(user.email)
-      reset_password_link = mail.body.to_s[%r{/u/password-reset/\S+}]
+      reset_password_link = wait_for_email_link(user, :reset_password)
       visit reset_password_link
 
       find("#new-account-password").fill_in(with: "newsuperpassword")
-      find("form .btn-primary").click
+      find(".change-password-form .btn-primary").click
       expect(page).to have_css(".header-dropdown-toggle.current-user")
     end
   end
@@ -85,11 +87,7 @@ shared_examples "login scenarios" do
       login_modal.fill_username("john")
       login_modal.email_login_link
 
-      wait_for(timeout: 5) { ActionMailer::Base.deliveries.count != 0 }
-
-      mail = ActionMailer::Base.deliveries.last
-      expect(mail.to).to contain_exactly(user.email)
-      login_link = mail.body.to_s[%r{/session/email-login/\S+}]
+      login_link = wait_for_email_link(user, :email_login)
       visit login_link
 
       find(".email-login-form .btn-primary").click
@@ -148,11 +146,7 @@ shared_examples "login scenarios" do
       login_modal.fill_username("john")
       login_modal.email_login_link
 
-      wait_for(timeout: 5) { ActionMailer::Base.deliveries.count != 0 }
-
-      mail = ActionMailer::Base.deliveries.last
-      expect(mail.to).to contain_exactly(user.email)
-      login_link = mail.body.to_s[%r{/session/email-login/\S+}]
+      login_link = wait_for_email_link(user, :email_login)
       visit login_link
 
       totp = ROTP::TOTP.new(user_second_factor.data).now
@@ -166,16 +160,48 @@ shared_examples "login scenarios" do
       login_modal.fill_username("john")
       login_modal.email_login_link
 
-      wait_for(timeout: 5) { ActionMailer::Base.deliveries.count != 0 }
-
-      mail = ActionMailer::Base.deliveries.last
-      expect(mail.to).to contain_exactly(user.email)
-      login_link = mail.body.to_s[%r{/session/email-login/\S+}]
+      login_link = wait_for_email_link(user, :email_login)
       visit login_link
 
       find(".toggle-second-factor-method").click
       find(".second-factor-token-input").fill_in(with: "iAmValidBackupCode")
       find(".email-login-form .btn-primary").click
+      expect(page).to have_css(".header-dropdown-toggle.current-user")
+    end
+
+    it "can reset password with TOTP" do
+      login_modal.open
+      login_modal.fill_username("john")
+      login_modal.forgot_password
+      find("button.forgot-password-reset").click
+
+      reset_password_link = wait_for_email_link(user, :reset_password)
+      visit reset_password_link
+
+      totp = ROTP::TOTP.new(user_second_factor.data).now
+      find(".second-factor-token-input").fill_in(with: totp)
+      find(".password-reset .btn-primary").click
+
+      find("#new-account-password").fill_in(with: "newsuperpassword")
+      find(".change-password-form .btn-primary").click
+      expect(page).to have_css(".header-dropdown-toggle.current-user")
+    end
+
+    it "can reset password with a backup code" do
+      login_modal.open
+      login_modal.fill_username("john")
+      login_modal.forgot_password
+      find("button.forgot-password-reset").click
+
+      reset_password_link = wait_for_email_link(user, :reset_password)
+      visit reset_password_link
+
+      find(".toggle-second-factor-method").click
+      find(".second-factor-token-input").fill_in(with: "iAmValidBackupCode")
+      find(".password-reset .btn-primary").click
+
+      find("#new-account-password").fill_in(with: "newsuperpassword")
+      find(".change-password-form .btn-primary").click
       expect(page).to have_css(".header-dropdown-toggle.current-user")
     end
   end

app/assets/javascripts/discourse/app/components/header/auth-buttons.gjs
@@ -1,14 +1,25 @@
 import Component from "@glimmer/component";
 import { service } from "@ember/service";
-import { and, not } from "truth-helpers";
 import DButton from "discourse/components/d-button";
 
 export default class AuthButtons extends Component {
   @service header;
 
+  get showSignupButton() {
+    return (
+      this.args.canSignUp &&
+      !this.header.headerButtonsHidden.includes("signup") &&
+      !this.header.topic
+    );
+  }
+
+  get showLoginButton() {
+    return !this.header.headerButtonsHidden.includes("login");
+  }
+
   <template>
     <span class="auth-buttons">
-      {{#if (and @canSignUp (not this.header.topic))}}
+      {{#if this.showSignupButton}}
         <DButton
           class="btn-primary btn-small sign-up-button"
           @action={{@showCreateAccount}}
@@ -16,12 +27,14 @@ export default class AuthButtons extends Component {
         />
       {{/if}}
 
-      <DButton
-        class="btn-primary btn-small login-button"
-        @action={{@showLogin}}
-        @label="log_in"
-        @icon="user"
-      />
+      {{#if this.showLoginButton}}
+        <DButton
+          class="btn-primary btn-small login-button"
+          @action={{@showLogin}}
+          @label="log_in"
+          @icon="user"
+        />
+      {{/if}}
     </span>
   </template>
 }

app/assets/javascripts/discourse/app/components/header/icons.gjs
@@ -47,6 +47,10 @@ export default class Icons extends Component {
     return !this.args.sidebarEnabled || this.site.mobileView;
   }
 
+  get hideSearchButton() {
+    return this.header.headerButtonsHidden.includes("search");
+  }
+
   @action
   toggleHamburger() {
     if (this.sidebarState.adminSidebarAllowedWithLegacyNavigationMenu) {
@@ -60,16 +64,18 @@ export default class Icons extends Component {
     <ul class="icons d-header-icons">
       {{#each (headerIcons.resolve) as |entry|}}
         {{#if (eq entry.key "search")}}
-          <Dropdown
-            @title="search.title"
-            @icon="search"
-            @iconId={{@searchButtonId}}
-            @onClick={{@toggleSearchMenu}}
-            @active={{this.search.visible}}
-            @href={{getURL "/search"}}
-            @className="search-dropdown"
-            @targetSelector=".search-menu-panel"
-          />
+          {{#unless this.hideSearchButton}}
+            <Dropdown
+              @title="search.title"
+              @icon="search"
+              @iconId={{@searchButtonId}}
+              @onClick={{@toggleSearchMenu}}
+              @active={{this.search.visible}}
+              @href={{getURL "/search"}}
+              @className="search-dropdown"
+              @targetSelector=".search-menu-panel"
+            />
+          {{/unless}}
         {{else if (eq entry.key "hamburger")}}
           {{#if this.showHamburger}}
             <Dropdown

app/assets/javascripts/discourse/app/helpers/hide-application-header-buttons.js
@@ -1,0 +1,15 @@
+import Helper from "@ember/component/helper";
+import { scheduleOnce } from "@ember/runloop";
+import { service } from "@ember/service";
+
+export default class HideApplicationHeaderButtons extends Helper {
+  @service header;
+
+  registerHider(buttons) {
+    this.header.registerHider(this, buttons);
+  }
+
+  compute([...buttons]) {
+    scheduleOnce("afterRender", this, this.registerHider, buttons);
+  }
+}

app/assets/javascripts/discourse/app/mixins/password-validation.js
@@ -81,7 +81,9 @@ export default Mixin.create({
     if (password.length < passwordMinLength) {
       return EmberObject.create(
         Object.assign(failedAttrs, {
-          reason: I18n.t("user.password.too_short"),
+          reason: I18n.t("user.password.too_short", {
+            password_min_length: passwordMinLength,
+          }),
         })
       );
     }

app/assets/javascripts/discourse/app/services/header.js
@@ -1,5 +1,7 @@
 import { tracked } from "@glimmer/tracking";
+import { registerDestructor } from "@ember/destroyable";
 import Service, { service } from "@ember/service";
+import { TrackedMap } from "@ember-compat/tracked-built-ins";
 import { disableImplicitInjections } from "discourse/lib/implicit-injections";
 
 @disableImplicitInjections
@@ -10,6 +12,8 @@ export default class Header extends Service {
   @tracked hamburgerVisible = false;
   @tracked userVisible = false;
   @tracked anyWidgetHeaderOverrides = false;
+
+  #hiders = new TrackedMap();
 
   get useGlimmerHeader() {
     if (this.siteSettings.glimmer_header_mode === "disabled") {
@@ -28,5 +32,23 @@ export default class Header extends Service {
         return true;
       }
     }
+  }
+
+  registerHider(ref, buttons) {
+    this.#hiders.set(ref, buttons);
+
+    registerDestructor(ref, () => {
+      this.#hiders.delete(ref);
+    });
+  }
+
+  get headerButtonsHidden() {
+    const buttonsToHide = new Set();
+    this.#hiders.forEach((buttons) => {
+      buttons.forEach((button) => {
+        buttonsToHide.add(button);
+      });
+    });
+    return Array.from(buttonsToHide);
   }
 }

app/assets/javascripts/discourse/app/templates/password-reset.hbs
@@ -1,3 +1,6 @@
+{{body-class "password-reset-page"}}
+{{hide-application-sidebar}}
+{{hide-application-header-buttons "search" "login" "signup"}}
 <div class="container password-reset clearfix">
   <div class="pull-left col-image">
     <img src={{this.lockImageUrl}} class="password-reset-img" alt="" />
@@ -19,8 +22,12 @@
         {{/unless}}
       {{/if}}
     {{else}}
-      <form>
+      <form class="change-password-form">
         {{#if this.securityKeyOrSecondFactorRequired}}
+          <h2>{{i18n "user.change_password.title"}}</h2>
+          <p>
+            {{i18n "user.change_password.verify_identity"}}
+          </p>
           {{#if this.errorMessage}}
             <div class="alert alert-error">{{this.errorMessage}}</div>
             <br />
@@ -31,13 +38,13 @@
               @challenge={{this.model.security_key_challenge}}
               @showSecurityKey={{this.model.security_key_required}}
               @showSecondFactor={{false}}
-              @secondFactorMethod={{this.secondFactorMethod}}
+              @secondFactorMethod={{this.selectedSecondFactorMethod}}
               @otherMethodAllowed={{this.otherMethodAllowed}}
               @action={{action "authenticateSecurityKey"}}
             />
           {{else}}
             <SecondFactorForm
-              @secondFactorMethod={{this.secondFactorMethod}}
+              @secondFactorMethod={{this.selectedSecondFactorMethod}}
               @secondFactorToken={{this.secondFactorToken}}
               @backupEnabled={{this.backupEnabled}}
               @isLogin={{false}}
@@ -47,7 +54,7 @@
                   "input"
                   (with-event-value (fn (mut this.secondFactorToken)))
                 }}
-                @secondFactorMethod={{this.secondFactorMethod}}
+                @secondFactorMethod={{this.selectedSecondFactorMethod}}
                 value={{this.secondFactorToken}}
                 id="second-factor"
               />
@@ -77,15 +84,14 @@
               @maskPassword={{this.maskPassword}}
               @togglePasswordMask={{this.togglePasswordMask}}
             />
-            <InputTip @validation={{this.passwordValidation}} />
-          </div>
 
-          <div class="instructions">
             <div class="caps-lock-warning {{unless this.capsLockOn 'hidden'}}">
               {{d-icon "exclamation-triangle"}}
               {{i18n "login.caps_lock_warning"}}
             </div>
           </div>
+
+          <InputTip @validation={{this.passwordValidation}} />
 
           <DButton
             @action={{action "submit"}}

commit 77af98ab660956f7ef90d9bc5ab0499abc791cce

FIX: password reset with security key (#27358)
This regressed in c50b7c9.

Co-authored-by: Penar Musaraj <pmusaraj@gmail.com>
Co-authored-by: Alan Guo Xiang Tan <gxtan1990@gmail.com>

app/assets/javascripts/discourse/app/controllers/password-reset.js
@@ -136,6 +136,10 @@ export default Controller.extend(PasswordValidation, {
     },
 
     authenticateSecurityKey() {
+      this.set(
+        "selectedSecondFactorMethod",
+        SECOND_FACTOR_METHODS.SECURITY_KEY
+      );
       getWebauthnCredential(
         this.model.challenge,
         this.model.allowed_credential_ids,

spec/system/login_spec.rb
@@ -4,7 +4,9 @@ require "rotp"
 
 shared_examples "login scenarios" do
   let(:login_modal) { PageObjects::Modals::Login.new }
+  let(:user_preferences_security_page) { PageObjects::Pages::UserPreferencesSecurity.new }
   fab!(:user) { Fabricate(:user, username: "john", password: "supersecurepassword") }
+  let(:user_menu) { PageObjects::Components::UserMenu.new }
 
   before { Jobs.run_immediately! }
 
@@ -223,6 +225,46 @@ shared_examples "login scenarios" do
       find("#new-account-password").fill_in(with: "newsuperpassword")
       find(".change-password-form .btn-primary").click
       expect(page).to have_css(".header-dropdown-toggle.current-user")
+    end
+
+    it "can reset password with a security key" do
+      # testing the 2FA flow requires a user that was created > 5 minutes ago
+      user.created_at = 6.minutes.ago
+      user.save!
+      sign_in(user)
+      options = ::Selenium::WebDriver::VirtualAuthenticatorOptions.new
+      authenticator = page.driver.browser.add_virtual_authenticator(options)
+
+      user_preferences_security_page.visit(user)
+      user_preferences_security_page.visit_second_factor("supersecurepassword")
+
+      find(".security-key .new-security-key").click
+      expect(user_preferences_security_page).to have_css("input#security-key-name")
+
+      find(".d-modal__body input#security-key-name").fill_in(with: "First Key")
+      find(".add-security-key").click
+
+      expect(user_preferences_security_page).to have_css(".security-key .second-factor-item")
+
+      user_menu.sign_out
+
+      # reset password flow
+      login_modal.open
+      login_modal.fill_username("john")
+      login_modal.forgot_password
+      find("button.forgot-password-reset").click
+
+      reset_password_link = wait_for_email_link(user, :reset_password)
+      visit reset_password_link
+
+      find("#security-key .btn-primary").click
+
+      find("#new-account-password").fill_in(with: "newsuperpassword")
+      find(".change-password-form .btn-primary").click
+      expect(page).to have_css(".header-dropdown-toggle.current-user")
+    ensure
+      # clear authenticator (otherwise it will interfere with other tests)
+      authenticator&.remove!
     end
   end
 end
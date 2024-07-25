hanzoai/forum

commit 1f74a86e43c36e18675747a2326c5a8b6701b4df

PERF: Improve production JS build in low-memory environments (#26849)
- Use 'cheap-source-map' webpack config on low-memory machines

  This results in worse quality sourcemaps in browser dev tools, but it significantly reduces memory use in our webpack build. In approximate local testing it drops from 1100mb to 590mb. This should make the rebuild process on low-memory machines much faster and less likely to trigger OOM errors.

  In development, and on higher-memory machines, the higher-quality 'source-map' option is maintained.

- Disable Webpack's built-in `minimize` feature. Embroider already applies Terser after the webpack build is complete. There is no need to double-minimize the output.

- Update ember-cli-progress-ci to print to stderr instead of stdout. For some reason, pups (used by discourse_docker) buffers the stdout of commands and only prints when they are finished. stderr does not have this same limitation, so switching will mean sysadmins can see the progress of the ember build in real-time.

Given the number of variables it's hard to promise exact numbers. But, in my tests on a DO droplet with 1GB RAM (+2GB swap), this reduced the `ember build` portion of a `./launcher rebuild app` from ~50 minutes to ~15 minutes.

app/assets/javascripts/discourse/ember-cli-build.js
@@ -136,11 +136,18 @@ module.exports = function (defaults) {
     staticAppPaths: ["static"],
     packagerOptions: {
       webpackConfig: {
-        devtool: "source-map",
+        devtool:
+          process.env.CHEAP_SOURCE_MAPS === "1"
+            ? "cheap-source-map"
+            : "source-map",
         output: {
           publicPath: "auto",
           filename: `assets/chunk.[chunkhash].${cachebusterHash}.js`,
           chunkFilename: `assets/chunk.[chunkhash].${cachebusterHash}.js`,
+        },
+        optimization: {
+          // Disable webpack minimization. Embroider automatically applies terser after webpack.
+          minimize: false,
         },
         cache: isProduction
           ? false

app/assets/javascripts/ember-cli-progress-ci/index.js
@@ -42,7 +42,7 @@ module.exports = {
     ) {
       this._sameOutputCount++;
     } else {
-      this.project.ui.writeInfoLine("..." + (text ? `[${text}]` : "."));
+      process.stderr.write("..." + (text ? `[${text}]` : ".") + "\n");
       this._sameOutputCount = 0;
     }
     this._lastText = text;

lib/tasks/assets.rake
@@ -17,8 +17,9 @@ task "assets:precompile:build" do
     heap_size_limit = check_node_heap_size_limit
 
     if heap_size_limit < 2048
-      STDERR.puts "Node.js heap_size_limit (#{heap_size_limit}) is less than 2048MB. Setting --max-old-space-size=2048."
-      compile_command = "NODE_OPTIONS='--max-old-space-size=2048' #{compile_command}"
+      STDERR.puts "Node.js heap_size_limit (#{heap_size_limit}) is less than 2048MB. Setting --max-old-space-size=2048 and CHEAP_SOURCE_MAPS=1"
+      compile_command =
+        "JOBS=0 CI=1 NODE_OPTIONS='--max-old-space-size=2048' CHEAP_SOURCE_MAPS=1 #{compile_command}"
     end
 
     ember_env = ENV["EMBER_ENV"] || "production"

commit e0b24b14771ee63d970f1f5de0a317f5546ac4e2

PERF: Restore minimization of all JS assets (#28077)
In an attempt to improve build performance, 1f74a86e mistakenly removed minimization for some of our JS assets, leading to a significant increase in the size of some files.

This commit restores minimization to those files. To avoid regressing on the build time improvements, this commit switches to using the `webpack-terser-plugin`'s "swcMinify" option. On an entry-level 1CPU/1GB-ram/2GB-swap DO droplet, this commit increases build time from ~16 minutes to ~18 minutes.

Co-authored-by: Alan Guo Xiang Tan <gxtan1990@gmail.com>

app/assets/javascripts/discourse/ember-cli-build.js
@@ -17,6 +17,7 @@ const withSideWatch = require("./lib/with-side-watch");
 const RawHandlebarsCompiler = require("discourse-hbr/raw-handlebars-compiler");
 const crypto = require("crypto");
 const commonBabelConfig = require("./lib/common-babel-config");
+const TerserPlugin = require("terser-webpack-plugin");
 
 process.env.BROCCOLI_ENABLED_MEMOIZE = true;
 
@@ -140,8 +141,12 @@ module.exports = function (defaults) {
           chunkFilename: `assets/chunk.[chunkhash].${cachebusterHash}.js`,
         },
         optimization: {
-          // TODO: Minimization being disabled is a bug and we are currently working to restore it.
-          minimize: process.env.DISCOURSE_WEBPACK_MINIMIZE === "1",
+          minimize: isProduction,
+          minimizer: [
+            new TerserPlugin({
+              minify: TerserPlugin.swcMinify,
+            }),
+          ],
         },
         cache: isProduction
           ? false
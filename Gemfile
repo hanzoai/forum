hanzoai/forum

commit 4851dbab1dd7bb89ece1798c15c419a744e509ea

DEV: Add converter framework for migrations-tooling (#28540)
* Updates GitHub Actions
* Switches from `bundler/inline` to an optional group in the `Gemfile` because the previous solution didn't work well with rspec
* Adds the converter framework and tests
* Allows loading private converters (see README)
* Switches from multiple CLI tools to a single CLI
* Makes DB connections reusable and adds a new abstraction for the `IntermediateDB`
* `IntermediateDB` acts as an interface for IPC calls when a converter steps runs in parallel (forks). Only the main process writes to the DB.
* Includes a simple example implementation of a converter for now.

Gemfile
@@ -274,3 +274,19 @@ gem "csv", require: false
 # dependencies for the automation plugin
 gem "iso8601"
 gem "rrule"
+
+group :migrations, optional: true do
+  gem "extralite-bundle", require: "extralite"
+
+  # auto-loading
+  gem "zeitwerk"
+
+  # databases
+  gem "trilogy"
+
+  # CLI
+  gem "ruby-progressbar"
+
+  # additional Gemfiles from converters
+  Dir[File.expand_path("migrations/**/Gemfile", __dir__)].each { |path| eval_gemfile(path) }
+end

migrations/config/gemfiles/common/Gemfile
@@ -1,30 +1,0 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
-
-# the minimal Ruby version required by migration-tooling
-ruby ">= 3.2.2"
-
-# `activesupport` gem needs to be in sync with the Rails version of Discourse, see `/Gemfile`
-lock_file = %w[Gemfile.lock ../Gemfile.lock].detect { File.exist?(_1) }
-activesupport_version =
-  Bundler::LockfileParser
-    .new(Bundler.read_file(lock_file))
-    .specs
-    .detect { _1.name == "activesupport" }
-    .version
-gem "activesupport", "= #{activesupport_version}", require: "active_support"
-
-# for SQLite
-gem "extralite-bundle", "~> 2.8", require: "extralite", github: "digital-fabric/extralite"
-gem "lru_redux", "~> 1.1", require: false
-
-# for communication between process forks
-gem "msgpack", "~> 1.7"
-
-# for CLI
-gem "colored2", "~> 4.0"
-gem "thor", "~> 1.3"
-
-# auto-loading
-gem "zeitwerk", "~> 2.6"

commit a96045efccdc1d80e06d401550a0e2f993ba916f

DEV: Remove dynamic inclusions from Gemfile (#28847)
Added in 4851dba, broke Dependabot parsing.

Gemfile
@@ -286,7 +286,4 @@ group :migrations, optional: true do
 
   # CLI
   gem "ruby-progressbar"
-
-  # additional Gemfiles from converters
-  Dir[File.expand_path("migrations/**/Gemfile", __dir__)].each { |path| eval_gemfile(path) }
 end